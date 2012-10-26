890a904b44e683bddf3f592cd13f4b625c9de20d
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2012-10-26 15:44:25 +0200

    [IMP] manifest description (lp:c2c-addons/6.1  rev 89.1.31)

--- account_credit_control/__openerp__.py
+++ account_credit_control/__openerp__.py
@@ -36,8 +36,10 @@ Configure the policies and policy levels in ``Accounting  > Configuration >
 Credit Control > Credit Policies``.
 You can define as many policy levels as you need.
 
-Configure a tolerance for the Credit control in each company, under the
-Accounting tab.
+Configure a tolerance for the Credit control and a default policy
+applied on all partners in each company, under the Accounting tab.
+
+You are able to specify a particular policy for one partner or one invoice.
 
 Usage
 -----
