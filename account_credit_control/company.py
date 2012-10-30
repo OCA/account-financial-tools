377c6c63bac446b1c20377867e172e66066ab799
Guewen Baconnier @ Camptocamp <guewen.baconnier@camptocamp.com>
2012-10-30 09:54:45 +0100

    [ADD] add myself as author (lp:c2c-addons/6.1  rev 89.1.48)

--- account_credit_control/company.py
+++ account_credit_control/company.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Nicolas Bessi
+#    Author: Nicolas Bessi, Guewen Baconnier
 #    Copyright 2012 Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
