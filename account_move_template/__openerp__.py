59f4c0b61dd51a53d7653f1d93d40dd390a850ae
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2012-11-17 00:44:13 +0100

    account_move_template moved from https://code.launchpad.net/~domsense/domsense-agilebg-addons/6.1

---
+++ account_move_template/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    Copyright (C) 2011 Agile Business Group sagl (<http://www.agilebg.com>)
+#    Copyright (C) 2011 Domsense srl (<http://www.domsense.com>)
+#    All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': "Account Move Template",
+    'version': '0.1',
+    'category': 'Generic Modules/Accounting',
+    'description': """
+Templates for Journal Entries
+
+User can configure journal entries templates, useful for recurring entries.
+The amount of each template line can be computed (through python code) or kept as user input. If user input, when using the template, user has to fill the amount of every input lines.
+The journal entry form allows lo load, through a wizard, the template to use and the amounts to fill.
+""",
+    'author': 'Agile Business Group & Domsense',
+    'website': 'http://www.agilebg.com',
+    'license': 'AGPL-3',
+    "depends" : ['account_accountant', 'analytic'],
+    "init_xml" : [],
+    "update_xml" : [
+        'move_template.xml',
+        'wizard/select_template.xml',
+        'security/ir.model.access.csv',
+        ],
+    "demo_xml" : [],
+    'test': [
+        'test/generate_move.yml',
+        ],
+    "active": False,
+    "installable": True
+}
