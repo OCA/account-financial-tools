57e73c7da0ac31f8eca456f2f108ea40df1c494a
Vincent Renaville <vincent.renaville@camptocamp.com>
2013-01-14 11:57:17 +0100

    [FIX] regarding comment from Alexandre Fayolle @ camptocamp

--- account_default_draft_move/account.py
+++ account_default_draft_move/account.py
@@ -6,64 +6,60 @@
 #
 # Author : Vincent Renaville (Camptocamp)
 #
-
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
-from osv import osv, fields
+
+from openerp.osv.orm import  TransientModel, fields
 from tools.translate import _
 
-class account_move(osv.osv):
+class account_move(TransientModel):
     _inherit = "account.move"
     
     def post(self, cr, uid, ids, context=None):
         return_value = super(account_move,self).post(cr, uid, ids, context)
         if return_value:
             invoice = context.get('invoice', False)
-            ## We test that we have an invoice in the context, in this case move net to be set to draft
-            ## in this condition we can cancel a invoice
+            ## We test if the move is related to an invoice_id in order to post it with draft status
             if invoice:
                 valid_moves = self.validate(cr, uid, ids, context)
                 if not valid_moves:
-                    raise osv.except_osv(_('Integrity Error !'), _('You can not validate a non-balanced entry !\nMake sure you have configured payment terms properly !\nThe latest payment term line should be of the type "Balance" !'))
+                    raise osv.except_osv(_('Integrity Error !'), _('''You can not validate a non-balanced entry!\n \
+                    Make sure you have configured payment terms properly!\n \
+                    The latest payment term line should be of the type "Balance" !'''))
+                move_obj = self.pool.get('account.move')
+                move_obj.write(cr, uid, valid_moves, {'state': 'draft'}, context=context)
+
                 cr.execute('UPDATE account_move '\
                            'SET state=%s '\
                            'WHERE id IN %s',
                            ('draft', tuple(valid_moves),))
-        return True
+        return return_value
 
     def button_cancel(self, cr, uid, ids, context=None):
         for line in self.browse(cr, uid, ids, context=context):
             if line.period_id.state == 'done':
-                raise osv.except_osv(_('Error !'), _('You can not modify a posted entry of closed periods'))
+                raise osv.except_osv(_('Error'), _('You can not modify a posted entry of closed periods'))
             ## Change the test of line state instead of journal type
             elif line.state == 'posted':
-                raise osv.except_osv(_('Error !'), _('You can not modify a posted entry of this journal !\nYou should set the journal to allow cancelling entries if you want to do that.'))
+                raise osv.except_osv(_('Error'), _('''You can not modify a posted entry of this journal!\n \
+                You should set the journal to allow cancelling entries if you want to do that.'''))
         if ids:
-            cr.execute('UPDATE account_move '\
-                       'SET state=%s '\
-                       'WHERE id IN %s', ('draft', tuple(ids),))
+            move_obj = self.pool.get('account.move')
+            move_obj.write(cr, uid, ids, {'state': 'draft'}, context=context)
         return True
 
     
-account_move()
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
