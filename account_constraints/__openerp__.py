fc317a27b6ed19b8e0e59b6f0d3dee3a7b426250
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-01-14 09:42:08 +0100

    [FIX] manifest: remove deprecated keys, ReSTructure the description

--- account_constraints/__openerp__.py
+++ account_constraints/__openerp__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name' : 'Account Constraints',
-    'version' : '1',
+    'version' : '1.0',
     'depends' : [
                  'account',
                  ],
@@ -27,25 +27,30 @@
     'license': 'AGPL-3',
     'category': 'Generic Modules/Accounting',
     'description': """
-    Add constraints in the accounting module of OpenERP to avoid bad usage by users that lead
-    to corrupted datas. This is based on our experiences and legal state of the art in other
-    software.
-    
-    Summary of constraints are:
-      
-    * Add a constraint on account move: you cannot pickup a date that is not in the fiscal year of the concerned period
-    
-    * For manual entries when multicurrency: 
-      a - Validation on the use of the 'Currency' and 'Currency Amount' fields as it is possible to enter one without the other
-      b - Validation to prevent a Credit amount with a positive 'Currency Amount', or a Debit with a negative 'Currency Amount'
-    
-    * Add a check on entries that user cannot provide a secondary currency if the same than the company one.
-     
+Account Constraints
+===================
+
+Add constraints in the accounting module of OpenERP to avoid bad usage
+by users that lead to corrupted datas. This is based on our experiences
+and legal state of the art in other software.
+
+Summary of constraints are:
+
+* Add a constraint on account move: you cannot pickup a date that is not
+  in the fiscal year of the concerned period
+
+* For manual entries when multicurrency:
+
+  a. Validation on the use of the 'Currency' and 'Currency Amount'
+     fields as it is possible to enter one without the other
+  b. Validation to prevent a Credit amount with a positive
+     'Currency Amount', or a Debit with a negative 'Currency Amount'
+
+* Add a check on entries that user cannot provide a secondary currency
+  if the same than the company one.
+
     """,
     'website': 'http://www.camptocamp.com',
-    'init_xml': [],
-    'update_xml': [],
-    'demo_xml': [],
+    'data': [],
     'installable': True,
-    'active': False,
 }
