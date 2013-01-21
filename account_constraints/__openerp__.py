f35e1ba6c7b50647e2f5bb0476488364dd9ac115
Joel Grand-Guillaume <joel.grandguillaume@camptocamp.com>
2013-01-21 17:07:58 +0100

    [FIX] Typo regarding MP comments

--- account_constraints/__openerp__.py
+++ account_constraints/__openerp__.py
@@ -51,9 +51,9 @@ Summary of constraints are:
   
 * Remove the possibility to modify or delete a move line related to an
   invoice or a bank statement, no matter what the status of the move
-  (draft, validated or posted). This is usefule in standard context but
-  moreover if you're using : account_default_draft_move. This way you ensure
-  user cannot make mistake even in draft, he must pass through the 
+  (draft, validated or posted). This is useful in a standard context but
+  even more if you're using `account_default_draft_move`. This way you ensure
+  that the user cannot make mistakes even in draft state, he must pass through the 
   parent object to make his modification.
 
     """,
