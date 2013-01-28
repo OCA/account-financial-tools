f782c75279c783d723bdd1c834203d23e00e59d2
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-01-28 10:33:17 +0100

    [FIX] manifest's description

--- account_default_draft_move/__openerp__.py
+++ account_default_draft_move/__openerp__.py
@@ -23,30 +23,32 @@
     "author" : "Camptocamp",
     'license': 'AGPL-3',
     "description": """
-    
-    Let the generated move in draft on invoice and bank statement validation. The main reason
-    is to ease the user work day-to-day. At first we used account_cancel, but this module allow
-    to cancel posted move and that's not allowed.
-    
-    Two needs here:
-    
-    - We want to be able to cancel an invoice (as soon as move are not validated) without
-      making a refund. Posted move can't be canceled.
-    
-    - We want to ensure all move generated from bank statement and invoice are generated in 
-      draft so we can still change them if needed.
-    
-    Use this module with account_constraints (find it here : https://launchpad.net/account-financial-tools
-    or in apps.openerp.com) and you'll get closely the same feature as account_cancel, 
-    but with th insurance that user won't change posted move.
-    
-    The new framework will then be: always work with draft moves, allowing people to change what 
-    they want. At the end of the period, validate all moves. Till there, you ensure no-one can change
-    something (or they'll need to make a refund).
-    
+Let the generated move in draft on invoice and bank statement
+validation. The main reason is to ease the user work day-to-day. At
+first we used account_cancel, but this module allow to cancel posted
+move and that's not allowed.
+
+Two needs here:
+
+- We want to be able to cancel an invoice (as soon as move are not
+  validated) without making a refund. Posted move can't be canceled.
+- We want to ensure all move generated from bank statement and invoice
+  are generated in draft so we can still change them if needed.
+
+Use this module with account_constraints (find it here:
+https://launchpad.net/account-financial-tools or in
+http://apps.openerp.com) and you'll get closely the same feature as
+account_cancel, but with the insurance that user won't change posted
+move.
+
+The new framework will then be: always work with draft moves, allowing
+people to change what they want. At the end of the period, validate all
+moves. Till there, you ensure no-one can change something (or they'll
+need to make a refund).
+
     """,
     'website': 'http://www.camptocamp.com',
-    'data' : [], 
+    'data' : [],
     'installable': True,
     'active': False,
 }
