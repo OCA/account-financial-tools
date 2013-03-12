8302dddcb0b32199b7177011c8fff83b0d951508
Vincent Renaville <vincent.renaville@camptocamp.com>
2013-03-12 13:33:24 +0100

    [ADD] tax analysis

---
+++ account_tax_analysis/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author Vincent Renaville. Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name" : "Tax analysis",
+    "version" : "1.0",
+    "depends" : ["base", "account"],
+    "author" : "CamptoCamp SA",
+    "category": 'Accounting & Finance',
+    "description": """Provide with details of your tax form (Allows you to group by move lines by tax account)
+    """,
+    'website': 'http://www.openerp.com',
+    'init_xml': [],
+    'update_xml': [
+        'account_tax_analysis_view.xml',
+    ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
