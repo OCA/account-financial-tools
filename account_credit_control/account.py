59cd274f802b1265809d0cbb7b6447f43bb73778
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-09-04 14:29:15 +0200

    [FIX] account_credit_control: Do not copy lines credit control lines when partners or accounts are duplicated

--- account_credit_control/account.py
+++ account_credit_control/account.py
@@ -20,6 +20,7 @@
 ##############################################################################
 from openerp.osv import orm, fields
 
+
 class AccountAccount(orm.Model):
     """Add a link to a credit control policy on account.account"""
 
@@ -32,6 +33,15 @@ class AccountAccount(orm.Model):
                             string='Credit Lines',
                             readonly=True),
         }
+
+    def copy_data(self, cr, uid, id, default=None, context=None):
+        if default is None:
+            default = {}
+        else:
+            default = default.copy()
+        default['credit_control_line_ids'] = False
+        return super(AccountAccount, self).copy_data(
+            cr, uid, id, default=default, context=context)
 
 
 class AccountInvoice(orm.Model):
@@ -54,24 +64,12 @@ class AccountInvoice(orm.Model):
                             readonly=True),
         }
 
-    def action_move_create(self, cr, uid, ids, context=None):
-        """ Write the id of the invoice in the generated moves. """
-        if context is None:
-            context = {}
-        # add from_parent_object to the conxtext to let the line.write
-        # call pass through account_constraints
-        ctxt = context.copy()
-        ctxt['from_parent_object'] = True
-        res = super(AccountInvoice, self).action_move_create(cr, uid, ids, context=ctxt)
-        for inv in self.browse(cr, uid, ids, context=ctxt):
-            if inv.move_id:
-                for line in inv.move_id.line_id:
-                    line.write({'invoice_id': inv.id}, context=ctxt)
-        return res
-
-
-class AccountMoveLine(orm.Model):
-
-    _inherit = "account.move.line"
-
-    _columns = {'invoice_id': fields.many2one('account.invoice', 'Invoice')}
+    def copy_data(self, cr, uid, id, default=None, context=None):
+        if default is None:
+            default = {}
+        else:
+            default = default.copy()
+        default = default.copy()
+        default['credit_control_line_ids'] = False
+        return super(AccountInvoice, self).copy_data(
+            cr, uid, id, default=default, context=context)
