965f3f50b538a2a4631cf8e6e9e3aff2add8ddee
Yann Papouin <yann.papouin@gmail.com>
2013-11-27 15:21:51 +0100

    FIX: wrong dict passed in _update_accounts

--- account_chart_update/account.py
+++ account_chart_update/account.py
@@ -1193,7 +1193,7 @@ class wizard_update_charts_accounts(osv.osv_memory):
         taxes_res = self._update_taxes(
             cr, uid, wizard, log, tax_codes_res['mapping'], context=context)
         accounts_res = self._update_accounts(
-            cr, uid, wizard, log, taxes_res['pending'], context=context)
+            cr, uid, wizard, log, taxes_res['mapping'], context=context)
         self._update_taxes_pending_for_accounts(cr, uid, wizard, log, taxes_res['pending'], accounts_res['mapping'], context=context)
         fps_res = self._update_fiscal_positions(cr, uid, wizard, log, taxes_res['mapping'], accounts_res['mapping'], context=context)
 
