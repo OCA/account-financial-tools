941f0f973cce192fd59cb7adf1f80cd17b74ef3d
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-01-07 13:02:41 +0100

    updated the contributors section

--- account_chart_update/__openerp__.py
+++ account_chart_update/__openerp__.py
@@ -31,7 +31,12 @@
     'website': "www.zikzakmedia.com",
     'depends': ["account"],
     'category': "Generic Modules/Accounting",
-    'contributors': ['Joaquín Gutierrez', 'Pedro M. Baeza', 'invitu'],
+    'contributors': [
+        'Joaquín Gutierrez',
+        'Pedro M. Baeza',
+        'invitu',
+        'Stéphane Bidoul',
+    ],
     'description': """
 Adds a wizard to update a company account chart from a chart template.
 ======================================================================
