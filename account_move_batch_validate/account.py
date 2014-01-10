7b28343e8c3583d9bd77e8f2c32765d0a19cf84d
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-01-10 15:05:48 +0100

    [add] new module account_move_batch_validate: initial work, unfinished

---
+++ account_move_batch_validate/account.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+#   Author: Leonardo Pistone
+#   Copyright 2014 Camptocamp SA
+#                                                                             #
+#   This program is free software: you can redistribute it and/or modify      #
+#   it under the terms of the GNU Affero General Public License as            #
+#   published by the Free Software Foundation, either version 3 of the        #
+#   License, or (at your option) any later version.                           #
+#                                                                             #
+#   This program is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU Affero General Public License for more details.                       #
+#                                                                             #
+#   You should have received a copy of the GNU Affero General Public License  #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+"""Accounting customisation for delayed posting."""
+
+from openerp.osv import fields, orm
+
+
+class account_move(orm.Model):
+
+    """We add a field to mark a move for delayed posting."""
+
+    _name = 'account.move'
+    _inherit = 'account.move'
+
+    _columns = {
+        'to_post': fields.boolean(
+            'To Post',
+            help='Check this box to mark the move for batch posting'
+        ),
+    }
