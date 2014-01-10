8886335db9fc488f8ca967d68ada07ee942ae0f6
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-01-10 15:05:48 +0100

    [add] new module account_move_batch_validate: initial work, unfinished

---
+++ account_move_batch_validate/__openerp__.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+#   Author: Leonardo Pistone
+#   Copyright 2014 Camptocamp SA
+#                                                                             #
+#   This program is free software: you can redistribute it and/or modify      #
+#   it under the terms of the GNU Affero General Public License as            #
+#   published by the Free Software Foundation, either version 3 of the        #
+#   License, or (at your option) any later version.                           #
+#                                                                             #
+#   This program is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU Affero General Public License for more details.                       #
+#                                                                             #
+#   You should have received a copy of the GNU Affero General Public License  #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+{
+    'name': "Account Move Batch Validate",
+    'version': '0.1',
+    'author': 'Camptocamp',
+    'maintainer': 'Camptocamp',
+    'category': 'Finance',
+    'complexity': 'normal',
+    'depends': [
+        'account',
+        'account_default_draft_move',
+        'connector',
+    ],
+    'description': """
+        Account Move Batch Validate
+
+        This module provides a wizard to post many Journal Entries in batch. it
+        uses the queue system introduces by the OpenERP Connector to handle a
+        big quantity of moves in batch.
+    """,
+    'website': 'http://www.camptocamp.com',
+    'init_xml': [],
+    'update_xml': [
+        'account_view.xml',
+        'wizard/move_marker_view.xml',
+    ],
+    'demo_xml': [],
+    'test': [],
+    'installable': True,
+    'images': [],
+    'license': 'AGPL-3',
+}
---
+++ account_move_batch_validate/wizard/move_marker.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+#   Author: Leonardo Pistone
+#   Copyright 2014 Camptocamp SA
+#                                                                             #
+#   This program is free software: you can redistribute it and/or modify      #
+#   it under the terms of the GNU Affero General Public License as            #
+#   published by the Free Software Foundation, either version 3 of the        #
+#   License, or (at your option) any later version.                           #
+#                                                                             #
+#   This program is distributed in the hope that it will be useful,           #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#   GNU Affero General Public License for more details.                       #
+#                                                                             #
+#   You should have received a copy of the GNU Affero General Public License  #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.     #
+#                                                                             #
+###############################################################################
+"""Wizards for batch posting."""
+
+from openerp.osv import fields, orm
+
+
+class AccountMoveMarker(orm.TransientModel):
+
+    """Wizard to mark account moves for batch posting."""
+
+    _name = "account.move.marker"
+    _inherit = "account.common.report"
+    _description = "Mark Journal Items for batch posting"
+
+    _columns = {
+        'action': fields.selection([
+            ('mark', 'Mark for posting'),
+            ('unmark', 'Unmark for posting'),
+        ], "Action", required=True),
+
+    }
+
+    _defaults = {
+        'action': 'mark',
+    }
+
+    def button_mark(self, cr, uid, ids, context=None):
+        """Mark/unmark lines and update the queue. Return action."""
+
+        for wiz in self.browse(cr, uid, ids, context=context):
+
+            move_obj = self.pool['account.move']
+
+            if wiz.action == 'mark':
+                import pdb;pdb.set_trace()
+            elif wiz.action == 'unmark':
+                # TODO
+                raise NotImplementedError(
+                    'The Unmark action is not implemented yet'
+                )
+            if wiz.filter != 'filter_no':
+                # TODO
+                raise NotImplementedError(
+                    'Date and period filter are not implemented yet'
+                )
+
+            domain = []
+            if wiz.journal_ids:
+                domain.append((
+                    'journal_id',
+                    'in',
+                    [journal.id for journal in wiz.journal_ids]
+                ))
+
+            move_ids = move_obj.search(cr, uid, domain, context=context)
+            move_obj.write 
+            for move in move_ids:
+                import pdb;pdb.set_trace()
