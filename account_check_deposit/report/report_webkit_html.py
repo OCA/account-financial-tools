61a64402185851b3b7c3c3ba456285ecc27dc92e
Sebastien Beau <sebastien.beau@akretion.com>
2014-02-03 08:22:31 +0100

    [FIX+REF+IMP] account_check_deposit:
    * [FIX] fix datamodel structure, indeed check_payment_ids should be a m2o. In the view we still keep the widget
    * [REF] remove useless state cancel, only draft/done is sufficent for now, forbid to delete a done deposit, fix move_line name need to be the equal to the line.ref for the reconciliation
    * [IMP] improve reconciliation between the check and the check deposit
    * [FIX] set correctly the date on the move line generated
    * [FIX] fix reconciliation of check
    * [IMP] check deposit improve view and add tab for move line generated
    * [IMP] add total amount and flag to know if the deposit have been reconcile, the best will be to add a workflow
    * [FIX] fix error when replaying commit
    * [REF] remove dependency on sale_quick_payment as check can be registred in various way like native voucher
    * [CLEAN] pep8 cleanup no code change
    * [REF] clean remove useless code, default value are called during the create
    * [FIX] fix wrong spelling
    * [REF] V7 clean up

--- account_check_deposit/report/report_webkit_html.py
+++ account_check_deposit/report/report_webkit_html.py
@@ -23,18 +23,19 @@
 
 import time
 from report import report_sxw
-from osv import osv
+
 
 class report_webkit_html(report_sxw.rml_parse):
+    
     def __init__(self, cr, uid, name, context):
         super(report_webkit_html, self).__init__(cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
-            'cr':cr,
+            'cr': cr,
             'uid': uid,
         })
 
 report_sxw.report_sxw('report.account.check.deposit',
-                       'account.check.deposit',
-                       'addons/account_check_deposit/report/check_deposit.mako',
-                       parser=report_webkit_html)
+                      'account.check.deposit',
+                      'addons/account_check_deposit/report/check_deposit.mako',
+                      parser=report_webkit_html)
