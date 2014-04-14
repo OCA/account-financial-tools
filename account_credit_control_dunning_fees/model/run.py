089b4846b426e4850ee35667e028c1963071f2c9
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-04-14 13:57:02 +0200

    [ADD] credit control dunning fees module
    [ADD] docstring
    
    [FIX] FeesComputer model specification to act as a abstract class without needing overriding
    
    [IMP] fees column in report and mail
    
    [FIX] dunning fees computer to register itself
    
    [RM] unused import
    
    [PEP8]
    
    [IMP] module documentation
    
    [ADD] dunniing fees translations
    
    [FIX] name of level in tests
    
    [TYPOS]
    
    Set all addons to uninstallable
    
    [ADD] credit control dunning fees module
    
    [ADD] docstring
    
    [FIX] FeesComputer model specification to act as a abstract class without needing overriding
    
    [IMP] fees column in report and mail
    
    [FIX] dunning fees computer to register itself
    
    [RM] unused import
    
    [PEP8]
    
    [IMP] module documentation
    
    [ADD] dunniing fees translations
    
    [FIX] name of level in tests
    
    [TYPOS]
    
    Set all addons to uninstallable
    
    Rename all addons to xxx_unported to be travis compliant
    
    [ADD] credit control dunning fees module
    
    [ADD] docstring
    
    [FIX] FeesComputer model specification to act as a abstract class without needing overriding
    
    [IMP] fees column in report and mail
    
    [FIX] dunning fees computer to register itself
    
    [RM] unused import
    
    [PEP8]
    
    [IMP] module documentation
    
    [ADD] dunniing fees translations
    
    [FIX] name of level in tests
    
    [TYPOS]
    
    Set all addons to uninstallable
    
    Rename all addons to xxx_unported to be travis compliant
    
    Revert "Rename all addons to xxx_unported to be travis compliant"
    
    This reverts commit 7eb182744f3057dd5cb9e35faf230412aa7a2331.
    
    [ADD] credit control dunning fees module
    
    [ADD] docstring
    
    [FIX] FeesComputer model specification to act as a abstract class without needing overriding
    
    [IMP] fees column in report and mail
    
    [FIX] dunning fees computer to register itself
    
    [RM] unused import
    
    [PEP8]
    
    [IMP] module documentation
    
    [ADD] dunniing fees translations
    
    [FIX] name of level in tests
    
    [TYPOS]
    
    Set all addons to uninstallable
    
    Rename all addons to xxx_unported to be travis compliant
    
    Revert "Rename all addons to xxx_unported to be travis compliant"
    
    This reverts commit 7eb182744f3057dd5cb9e35faf230412aa7a2331.
    
    [FIX] move all unported addons into __unported__ folder instead of adding _unported suffix
    
    [FIX] flake8 PEP8 for module account_credit_control_dunning_fees
    
    [FIX] flake8 PEP8 for module account_credit_control_dunning_fees
    
    [FIX] pep8 line length to 80, activate pep8 check in travis, and add noqa on scenario while waiting improvement of quality-tools
    
    Add missing security on `credit.control.dunning.fees.computer`

---
+++ account_credit_control_dunning_fees/model/run.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import orm
+
+
+class credit_control_run(orm.Model):
+    """Add computation of fees"""
+
+    _inherit = "credit.control.run"
+
+    def _generate_credit_lines(self, cr, uid, run_id, context=None):
+        """Override method to add fees computation"""
+        credit_line_ids = super(credit_control_run,
+                                self)._generate_credit_lines(
+                                    cr,
+                                    uid,
+                                    run_id,
+                                    context=context)
+        fees_model = self.pool['credit.control.dunning.fees.computer']
+        fees_model._compute_fees(cr, uid, credit_line_ids, context=context)
+        return credit_line_ids
