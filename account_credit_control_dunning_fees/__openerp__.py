6c026d2e335fa2c5a7a0f62cf6f9fb95c9397ae9
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-04-14 13:57:02 +0200

    # This is a combination of 12 commits. # The first commit's message is: [ADD] credit control dunning fees module
    # This is the 2nd commit message:
    
    [ADD] docstring
    
    # This is the 3rd commit message:
    
    [FIX] FeesComputer model specification to act as a abstract class without needing overriding
    
    # This is the 4th commit message:
    
    [IMP] fees column in report and mail
    
    # This is the 5th commit message:
    
    [FIX] dunning fees computer to register itself
    
    # This is the 6th commit message:
    
    [RM] unused import
    
    # This is the 7th commit message:
    
    [PEP8]
    
    # This is the 8th commit message:
    
    [IMP] module documentation
    
    # This is the 9th commit message:
    
    [ADD] dunniing fees translations
    
    # This is the 10th commit message:
    
    [FIX] name of level in tests
    
    # This is the 11th commit message:
    
    [TYPOS]
    
    # This is the 12th commit message:
    
    Set all addons to uninstallable

---
+++ account_credit_control_dunning_fees/__openerp__.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name': 'Credit control dunning fees',
+ 'version': '0.1.0',
+ 'author': 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'category': 'Accounting',
+ 'complexity': 'normal',
+ 'depends': ['account_credit_control'],
+ 'description': """
+Dunning Fees for Credit Control
+===============================
+
+This extention of credit control adds the notion of dunning fees
+on credit control lines.
+
+Configuration
+-------------
+For release 0.1 only fixed fees are supported.
+
+You can specifiy a fixed fees amount, a product and a currency
+on the credit control level form.
+
+The amount will be used as fees values the currency will determine
+the currency of the fee. If the credit control line has not the
+same currency as the fees currency, fees will be converted to
+the credit control line currency.
+
+The product is used to compute taxes in reconciliation process.
+
+Run
+---
+Fees are automatically computed on credit run and saved
+on the generated credit lines.
+
+Fees can be manually edited as long credit line is draft
+
+Credit control Summary report includes a new fees column.
+-------
+Support of fees price list
+
+""",
+ 'website': 'http://www.camptocamp.com',
+ 'data': ['view/policy_view.xml',
+          'view/line_view.xml',
+          'report/report.xml'],
+ 'demo': [],
+ 'test': [],
+ 'installable': False,
+ 'auto_install': False,
+ 'license': 'AGPL-3',
+ 'application': False,
+}
