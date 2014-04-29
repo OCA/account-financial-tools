f07b3f4ed6ecae56544b5596e598f8378ee90a31
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-04-29 13:48:48 +0200

    [FIX] missing import

--- account_credit_control/invoice.py
+++ account_credit_control/invoice.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.osv import orm
+from openerp.osv import orm, fields
 from openerp.tools.translate import _
 
 
