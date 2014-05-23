5de3a926c9fa32134a4d82fab103c3e5a0943b6d
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-05-23 14:57:13 +0200

    [ADD] constraint to ensure that only highest level credit policy can trigger legal claim

--- account_credit_control_legal_claim/model/policy.py
+++ account_credit_control_legal_claim/model/policy.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 from openerp.osv import orm, fields
-
+from openerp.tools.translate import _
 
 class credit_control_policy_level(orm.Model):
     """Add claim type to policy level"""
@@ -28,3 +28,19 @@ class credit_control_policy_level(orm.Model):
     _columns = {
         'is_legal_claim': fields.boolean('Legal Claim Action')
     }
+
+    def _check_legal_claim_level(self, cr, uid, ids, context=None):
+        for current_level in self.browse(cr, uid, ids, context=context):
+            levels = current_level.policy_id.level_ids
+            highest = max(levels, key= lambda x: x.level)
+            for lvl in levels:
+                if lvl.is_legal_claim and lvl != highest:
+                    raise orm.except_orm(
+                        _('Only highest level can be tickes as legal claim'),
+                        _('The current highest level is %s') % highest.name
+                    )
+        return True
+
+    _constraints = [(_check_legal_claim_level,
+                     'Only highest level can be legal claim',
+                     ['is_legal_claim'])]
