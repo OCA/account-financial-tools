7f1c747589ed1a2312e6dfbab284c0a3e4c90a2a
Nicolas Bessi <nbessi@gmail.com>
2014-07-11 18:25:11 +0200

    [FIX] flake8 PEP8 for module account_credit_control_dunning_fees

--- account_credit_control_dunning_fees/model/run.py
+++ account_credit_control_dunning_fees/model/run.py
@@ -18,7 +18,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp.osv import orm, fields
+from openerp.osv import orm
 
 
 class credit_control_run(orm.Model):
