86886cad44ebc9eac5e6c96fd33439ede60519f2
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2014-07-31 13:29:50 +0200

    remove noqa

--- account_balance_line/__init__.py
+++ account_balance_line/__init__.py
@@ -18,4 +18,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from . import account_move_line  # noqa
+from . import account_move_line
