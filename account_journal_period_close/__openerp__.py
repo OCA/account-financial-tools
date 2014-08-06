99c5cedcb910a8fe7919bb773f39da76da51332b
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-08-06 14:25:22 +0200

    [DOC] account_journal_period_close: improve module description

--- account_journal_period_close/__openerp__.py
+++ account_journal_period_close/__openerp__.py
@@ -38,10 +38,19 @@
     "depends": [
                 "account"],
     "description": """
-Account Journal Period Close
-==============================
-this module allows to add some account move in a close period.
-To do this, you have to specify account journals on which will allow writing of account move on the period form view
+Close period per journal
+========================
+
+This module allows fine grained control of period closing.
+Each journal can be closed independently for any period
+(using buttons on the fiscal period view).
+
+A common use case is letting accountants close the sale
+and purchase journals when the VAT declaration is done for
+a given period, while leaving the miscellaneous journal open.
+
+From a technical standpoint, the module leverages the
+account.journal.period model that is present in Odoo core.
 """,
     "data": ['view/account_view.xml'],
     "demo": [],
