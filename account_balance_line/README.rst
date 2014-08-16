6e10248b6bf5cd698f445c216b45667c7841e300
cubells <vicent@vcubells.net>
2014-08-16 15:46:23 +0200

    account_balance_line module

--- account_balance_line/README.rst
+++ account_balance_line/README.rst
@@ -6,7 +6,7 @@
 Account Balance Line
 ====================
 
-Add a balance total for grouped lines in move line view.
+This module adds a balance total for grouped lines in move line view.
 
 Balance field will only be shown when move lines are grouped by account
 or filtered by account.
@@ -14,21 +14,23 @@ or filtered by account.
 Usage
 =====
 
+#. If you're an accounting adviser, enable developer mode and go to
+   Accounting > Adviser > Journal items.
+#. Or open an account or a partner, and click on Action > Journal items.
+#. Filter or group by account.
+#. You will see a column named "Balance" with the balance of the line.
+
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/92/8.0
-
-For further information, please visit:
-
- * https://www.odoo.com/forum/help-1
+   :target: https://runbot.odoo-community.org/runbot/92/9.0
 
 Bug Tracker
 ===========
 
-Bugs are tracked on `GitHub Issues <https://github.com/OCA/account-financial-tools/issues>`_.
-In case of trouble, please check there if your issue has already been reported.
-If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/account-financial-tools/issues/new?body=module:%20account_balance_line%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/account-financial-tools/issues>`_. In case of trouble,
+please check there if your issue has already been reported. If you spotted it
+first, help us smashing it by providing a detailed and welcomed feedback.
 
 Credits
 =======
@@ -38,6 +40,7 @@ Contributors
 
 * Vincent revaville <vincent.renaville@camptocamp.com>
 * Yannick Vaucher <yannick.vaucher@camptocamp.com>
+* Vicent Cubells <vicent.cubells@tecnativa.com>
 
 Maintainer
 ----------
--- account_balance_line/__openerp__.py
+++ account_balance_line/__openerp__.py
@@ -1,34 +1,23 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Vincent Renaville (Camptocamp)
-#    Copyright 2010-2014 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-{'name': 'Balance on lines',
- 'summary': 'Display balance totals in move line view',
- 'version': '8.0.1.1.1',
- 'author': "Camptocamp,Odoo Community Association (OCA)",
- 'maintainter': 'Camptocamp',
- 'category': 'Accounting',
- 'depends': ['account'],
- 'website': 'http://www.camptocamp.com',
- 'data': ['account_move_line_view.xml'],
- 'tests': [],
- 'installable': True,
- 'license': 'AGPL-3',
- }
+# Copyright 2010-2014 Camptocamp - Vincent Renaville
+# Copyright 2016 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Balance on lines',
+    'summary': 'Display balance totals in move line view',
+    'version': '9.0.1.0.0',
+    'category': 'Accounting',
+    'website': 'http://www.tecnativa.com,',
+    'author': "Camptocamp,"
+              "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'account'
+    ],
+    'data': [
+        'views/account_move_line_view.xml'
+    ],
+}
