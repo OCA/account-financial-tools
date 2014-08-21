3ba7ffad606045ae80feda6f90b58accf7b9e06e
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-08-21 15:46:23 +0200

    account_balance_line: Use existing field

--- account_balance_line/__openerp__.py
+++ account_balance_line/__openerp__.py
@@ -3,9 +3,9 @@
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Balance on lines',
+    'name': 'Balance on journal items',
     'summary': 'Display balance totals in move line view',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'category': 'Accounting',
     'website': 'http://www.tecnativa.com,',
     'author': "Camptocamp,"
