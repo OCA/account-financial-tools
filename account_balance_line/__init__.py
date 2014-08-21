2fe20b172bb3abcab03b71c9a3629b6c1539fc75
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-08-21 15:46:23 +0200

    account_balance_line: Use existing field

--- account_balance_line/__init__.py
+++ account_balance_line/__init__.py
@@ -1,6 +1,2 @@
 # -*- coding: utf-8 -*-
-# Copyright 2010-2014 Camptocamp - Vincent Renaville
-# Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import models
