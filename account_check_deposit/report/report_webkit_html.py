8a3458b7bfdbb7c80da05093360f78dd61b246ea
Alexis de Lattre <alexis@via.ecp.fr>
2014-10-08 13:47:28 +0200

    Add support for multi-currency Works with a single account journal Usability and many other enhancements

--- account_check_deposit/report/report_webkit_html.py
+++ account_check_deposit/report/report_webkit_html.py
@@ -1,34 +1,35 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2010-2012 Camptocamp Austria (<http://www.camptocamp.at>)
-#    Copyright (C) 2013 AKRETION (<http://www.akretion.com>)
+#   account_check_deposit for Odoo/OpenERP
+#   Copyright (C) 2012-2014 Akretion (http://www.akretion.com/)
+#   @author: Benoît GUILLOT <benoit.guillot@akretion.com>
+#   @author: Chafique DELLI <chafique.delli@akretion.com>
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-##############################################################################
+###############################################################################
 
 import time
 from report import report_sxw
 
 
 class report_webkit_html(report_sxw.rml_parse):
-    
+
     def __init__(self, cr, uid, name, context):
-        super(report_webkit_html, self).__init__(cr, uid, name, context=context)
+        super(report_webkit_html, self).__init__(
+            cr, uid, name, context=context)
         self.localcontext.update({
             'time': time,
             'cr': cr,
