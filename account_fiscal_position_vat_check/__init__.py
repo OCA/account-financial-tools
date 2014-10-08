5788aefca6b09ed293f1256794641066ae25d804
Alexis de Lattre <alexis@via.ecp.fr>
2014-10-08 21:02:35 +0200

    OpenERP -> Odoo Remove application=True in __openerp__.py

--- account_fiscal_position_vat_check/__init__.py
+++ account_fiscal_position_vat_check/__init__.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Account Fiscal Position VAT Check module for OpenERP
+#    Account Fiscal Position VAT Check module for Odoo
 #    Copyright (C) 2013-2014 Akretion (http://www.akretion.com)
 #    @author Alexis de Lattre <alexis.delattre@akretion.com>
 #
