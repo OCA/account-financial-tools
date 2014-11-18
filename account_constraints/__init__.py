0da5dd50787a7d916f9509150c284ee71799e6f1
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2014-11-18 16:33:51 +0100

    [FIX] pylint relative imports

--- account_constraints/__init__.py
+++ account_constraints/__init__.py
@@ -17,4 +17,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import account_constraints
+from . import account_constraints
