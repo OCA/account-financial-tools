8155bd58afe13bb7a3ef9ae867e8e1b30449c40d
Alex Comba <alex.comba@agilebg.com>
2014-11-21 15:23:55 +0100

    Move the description to README.rst

--- account_move_template/__openerp__.py
+++ account_move_template/__openerp__.py
@@ -23,21 +23,6 @@
     'version': '0.1',
     'category': 'Generic Modules/Accounting',
     'summary': "Templates for recurring Journal Entries",
-    'description': """
-Templates for Journal Entries
-
-User can configure journal entries templates, useful for recurring entries.
-
-The amount of each template line can be computed (through python code)
-or kept as user input.
-
-If user input, when using the template, user has to fill
-the amount of every input lines.
-
-The journal entry form allows lo load, through a wizard,
-the template to use and the amounts to fill.
-
-""",
     'author': 'Agile Business Group',
     'website': 'http://www.agilebg.com',
     'license': 'AGPL-3',
