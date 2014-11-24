82d3efcc93a1078959a065f145c27e08d9fe84f3
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2014-11-24 15:40:51 +0100

    Unused imports

--- account_tax_analysis/account_tax_analysis.py
+++ account_tax_analysis/account_tax_analysis.py
@@ -17,7 +17,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields, api, exceptions, _
+from openerp import models, fields, api
 
 
 class AccountTaxDeclarationAnalysis(models.TransientModel):
