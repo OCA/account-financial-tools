1674f95e5ab5facb8e8cc1e99a8cf1afc7487d12
luc-demeyer <luc.demeyer@noviat.com>
2014-12-24 15:18:09 +0100

    move description to README.rst

---
+++ account_move_line_search_extension/README.rst
@@ -0,0 +1,15 @@
+Journal Items Search Extension
+==============================
+
+This module adds the 'Journal Items Search All' menu entry.
+
+This menu entry adds a number of search fields on top of the List View rows.
+These fields can be used in combination with the Search window.
+
+The purpose of this view is to offer a fast drill down capability
+when searching through large number of accounting entries.
+
+The drill down is facilitated further by opening the Form View when clicking on
+the sought-after entry.
+This allows an intuitive click-through to the related accounting documents
+such as the originating Bank Statement, Invoice, Asset, ...
--- account_move_line_search_extension/__openerp__.py
+++ account_move_line_search_extension/__openerp__.py
@@ -25,25 +25,7 @@
     'version': '0.2',
     'license': 'AGPL-3',
     'author': 'Noviat',
-    'category': 'Generic Modules',
-    'description': """
-Journal Items Search Extension
-==============================
-
-This module adds the 'Journal Items Search All' menu entry.
-
-This menu entry adds a number of search fields on top of the List View rows.
-These fields can be used in combination with the Search window.
-
-The purpose of this view is to offer a fast drill down capability
-when searching through large number of accounting entries.
-
-The drill down is facilitated further by opening the Form View when clicking on
-the sought-after entry.
-This allows an intuitive click-through to the related accounting documents
-such as the originating Bank Statement, Invoice, Asset, ...
-
-    """,
+    'category': 'Accounting & Finance',
     'depends': ['account'],
     'data': [
         'account_view.xml',
