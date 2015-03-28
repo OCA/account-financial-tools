b7530f280cb38a60d873ead1ae77aee2256ff276
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-28 14:56:47 +0100

    [IMP] account_chart_update: Whole refactoring
    * Fix calculated field when showing inactives + add field to view
    * Migrated to new API.
    * Provided hooks for detecting changes and for adding update/create values.
    * Purge dead error detection code (not applying).
    * Use global mapping dictionaries stored on class instead of passing arguments down to top and viceversa.
    * Number fields computed by a function field instead of being computed by hand.
    * Use recordsets as indexes instead of ids to avoid repetitive browses.
    * Re-enable local cache variables
    * README + authors

--- account_chart_update/__openerp__.py
+++ account_chart_update/__openerp__.py
@@ -6,7 +6,6 @@
 #    Copyright (c) 2010 Pexego Sistemas Informáticos S.L.(http://www.pexego.es)
 #    Copyright (c) 2013 Joaquin Gutierrez (http://www.gutierrezweb.es)
 #                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
-#    $Id$
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
 #    by the Free Software Foundation, either version 3 of the License, or
@@ -27,8 +26,12 @@
 {
     'name': "Detect changes and update the Account Chart from a template",
     'version': "1.2",
-    'author': "Zikzakmedia SL,Odoo Community Association (OCA)",
-    'website': "www.zikzakmedia.com",
+    'author': "Zikzakmedia SL, "
+              "Pexego, "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "ACSONE A/V, "
+              "Odoo Community Association (OCA)",
+    'website': "http://odoo-community.org",
     'depends': ["account"],
     'category': "Generic Modules/Accounting",
     'contributors': [
@@ -37,31 +40,6 @@
         'invitu',
         'Stéphane Bidoul',
     ],
-    'description': """
-Adds a wizard to update a company account chart from a chart template.
-======================================================================
-
-This is a pretty useful tool to update OpenERP installations after tax reforms
-on the official charts of accounts, or to apply fixes performed on the chart
-template.
-
-The wizard:
-
-- Allows the user to compare a chart and a template showing differences
-  on accounts, taxes, tax codes and fiscal positions.
-- It may create the new account, taxes, tax codes and fiscal positions detected
-  on the template.
-- It can also update (overwrite) the accounts, taxes, tax codes and fiscal
-  positions that got modified on the template.
-
-The wizard lets the user select what kind of objects must be checked/updated,
-and whether old records must be checked for changes and updates.
-It will display all the accounts to be created / updated with some information
-about the detected differences, and allow the user to exclude records
-individually.
-
-Any problem found while updating will be shown on the last step.
-""",
     'license': "AGPL-3",
     "depends": [
         "account",
@@ -70,6 +48,7 @@ Any problem found while updating will be shown on the last step.
     "demo": [],
     "data": [
         'wizard/wizard_chart_update_view.xml',
+        'views/account_tax_code_view.xml',
     ],
     "active": False,
     'installable': True
