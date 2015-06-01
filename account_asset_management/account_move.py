426c06bab59dda732176976fc74ad45466ca5d9d
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2015-06-01 17:36:56 +0200

    [IMP][account_asset_management] Define FIELDS_AFFETCS_ASSET_MOVE as a set directly

--- account_asset_management/account_move.py
+++ account_asset_management/account_move.py
@@ -28,7 +28,7 @@ _logger = logging.getLogger(__name__)
 
 # List of move's fields that can't be modified if move is linked
 # with a depreciation line
-FIELDS_AFFETCS_ASSET_MOVE = ['period_id', 'journal_id', 'date']
+FIELDS_AFFETCS_ASSET_MOVE = set(['period_id', 'journal_id', 'date'])
 
 
 class account_move(orm.Model):
@@ -55,7 +55,7 @@ class account_move(orm.Model):
             cr, uid, ids, context=context, check=check)
 
     def write(self, cr, uid, ids, vals, context=None):
-        if set(vals).intersection(set(FIELDS_AFFETCS_ASSET_MOVE)):
+        if set(vals).intersection(FIELDS_AFFETCS_ASSET_MOVE):
             if isinstance(ids, (int, long)):
                 ids = [ids]
             depr_obj = self.pool.get('account.asset.depreciation.line')
