a48fc546f18194cd6197c8fb3a65c66dcdf12de2
Alexis de Lattre <alexis.delattre@akretion.com>
2015-09-23 08:49:16 +0200

    Fix bug #301

--- currency_rate_update/model/currency_rate_update.py
+++ currency_rate_update/model/currency_rate_update.py
@@ -326,7 +326,7 @@ class Currency_rate_update_service(models.Model):
     @api.multi
     def run_currency_update(self):
         # Update currency at the given frequence
-        services = self.search([('next_run', '=', fields.Date.today())])
+        services = self.search([('next_run', '<=', fields.Date.today())])
         services.with_context(cron=True).refresh_currency()
 
     @api.model
