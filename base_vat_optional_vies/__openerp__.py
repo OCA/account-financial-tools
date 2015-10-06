7f45b664dcd7fd7c56420a2b3ece0ca42914a559
Antonio Espinosa <antespi@gmail.com>
2015-10-06 11:10:40 +0200

    base_vat_optional_vies addon

---
+++ base_vat_optional_vies/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
+# See README.rst file on addon root folder for more details
+
+{
+    'name': "Optional validation of VAT via VIES",
+    'category': 'Accounting',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'base_vat',
+    ],
+    'external_dependencies': {
+        'python': ['vatnumber'],
+    },
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+    'author': 'Antiun Ingeniería S.L., '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'demo': [],
+    'test': [],
+    'installable': True,
+}
