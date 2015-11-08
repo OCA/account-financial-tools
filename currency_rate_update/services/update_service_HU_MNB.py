431fcb69652783e0af67d61ba1d81cb8f05bbda8
Viktor Nagy <viktor.nagy@gmail.com>
2015-11-08 21:36:25 +0100

    Updated fetching service for Hungarian National Bank (MNB) from html parsing to SOAP. This requires the suds python package.

--- currency_rate_update/services/update_service_HU_MNB.py
+++ currency_rate_update/services/update_service_HU_MNB.py
@@ -22,9 +22,9 @@
 ##############################################################################
 
 from .currency_getter_interface import Currency_getter_interface
-from lxml import html
+from lxml import etree
 from datetime import datetime
-import requests
+from suds.client import Client
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -34,89 +34,42 @@ class HU_MNB_getter(Currency_getter_interface):
     """Implementation of Currency_getter_factory interface
     for the Hungarian National Bank (http://www.mnb.hu)
     """
-
-    months_to_numbers_mapping = {
-        u'január' : 1,
-        u'február' : 2,
-        u'március' : 3,
-        u'április' : 4,
-        u'május' : 5,
-        u'június' : 6,
-        u'július' : 7,
-        u'augusztus' : 8,
-        u'szeptember' : 9,
-        u'október' : 10,
-        u'november' : 11,
-        u'december' : 12
-    }
-
-    def get_date_for_currency(self, site_tree):
-        #define a date which surely will throw an error 
-        #in the later business logic
-        currency_date = datetime(year=1900, month=1, day=1)
-
-        if(site_tree is not None):
-            date_path = '//div[@class="MNBDailyRatesUI_ExchangeRates"]/table/thead/tr[1]/td/span'
-            date_hun_format = unicode(site_tree.xpath(date_path)[0].text)
-            if(date_hun_format is not None):
-                date_hun_format = date_hun_format.replace(' ','|')
-                date_hun_format = date_hun_format.replace('.','')
-                splitted_date = date_hun_format.split('|')
-            if(splitted_date is not None and len(splitted_date) == 3):
-                currency_date = datetime(year=int(splitted_date[0]), 
-                    month=self.months_to_numbers_mapping[splitted_date[1]], 
-                    day=int(splitted_date[2]) )
-                _logger.info("Date for the MNB curreny is:" + str(currency_date))
-            else:
-                _logger.warning('Date could not be paresed properly from MNB Webpage, please check xpath mapping or website response.')            
-        else:
-            _logger.warning('No site_tree was passed to the method, will not return a valid date!')
-
-        return currency_date
-
+    WSDL = "http://www.mnb.hu/arfolyamok.asmx?wsdl"
 
     def get_updated_currency(self, currency_array, main_currency,
                              max_delta_days):
         """implementation of abstract method of curreny_getter_interface"""
         self.validate_cur(main_currency)
         _logger.info("HU_MNB: Updating currencies from Hunagrian National Bank.")
-        site_content = requests.get("http://www.mnb.hu/arfolyamok")
-        site_tree = html.fromstring(site_content.content)
-        path_template = '//div[@class="MNBDailyRatesUI_ExchangeRates"]/table/tbody/tr[{0}]/td[{1}]'
+        client = Client(self.WSDL)
+        results = etree.fromstring(client.service.GetCurrentExchangeRates())
+
+        date_template = lambda element: element.xpath('//Day')[0].get('date')
+        currency_template = lambda element, currency: element.xpath('//Rate[@curr="{0}"]'.format(currency))[0].text
 
         if main_currency in currency_array:
             _logger.info("HU_MNB: Removing main currency from currency array." + main_currency)
             currency_array.remove(main_currency)
 
-        loaded_currency_date = self.get_date_for_currency(site_tree)
+        loaded_currency_date = datetime.strptime(date_template(results), '%Y-%m-%d')
 
         #this will throw an error if the difference between the 
         #loaded currency date and today is bigger then max_delta_days
         self.check_rate_date(loaded_currency_date, max_delta_days)
 
-        currencies_and_values = {}
+        for currency in currency_array:
+            self.validate_cur(currency)
+            try:
+                value = currency_template(results, currency)
+            except:
+                _logger.error('HU_MNB - Currency Updater - Could not fetch value for {0}'.format(currency))
+                continue
 
-        # there are 17 columns on the HTML page
-        for i in range(1, 18):
-            currency1 = site_tree.xpath(path_template.format(i,1))[0].text
-            val1 = site_tree.xpath(path_template.format(i,4))[0].text
-            currency2 = site_tree.xpath(path_template.format(i,6))[0].text
-            val2 = site_tree.xpath(path_template.format(i,9))[0].text
-        
-            currencies_and_values[currency1] = float(val1.replace(',','.'))
-            currencies_and_values[currency2] = float(val2.replace(',','.'))
+            if value:
+                _logger.info("Currency: {0} = {1}".format(currency, value))
+                self.updated_currency[currency] = float(value.replace(',', '.'))
+            else:
+                raise Exception('HU_MNB - Currency Updater - Could not update the %s' % (currency))
 
         _logger.info("HU_MNB: Currency values loaded:")
-        _logger.info(currencies_and_values)
-
-        for curr in currency_array:
-            self.validate_cur(curr)
-            
-            val = currencies_and_values[curr];
-            if val:
-                _logger.info("Currency: {0} = {1}".format(curr, val))
-                self.updated_currency[curr] = val
-            else:
-                raise Exception('HU_MNB - Currency Updater - Could not update the %s' % (curr))
-
         return self.updated_currency, self.log_info
