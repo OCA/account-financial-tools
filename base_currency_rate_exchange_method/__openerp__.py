d2df63ce1a69b4a43e1ca61bd41d9aa4606be177
Andhitia Rama <andhitia.r@gmail.com>
2015-12-04 21:39:19 +0700

    [ADD] base_currency_rate_exchange_method

---
+++ base_currency_rate_exchange_method/__openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2009 CamptoCamp. All rights reserved.
+#    @author Nicolas Bessi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "Currency Rate Exchange Method",
+    "version": "8.0.1.0.0",
+    "author": "Andhitia Rama,Odoo Community Association (OCA)",
+    "website": "http://andhitiarama.wordpress.com",
+    "license": "AGPL-3",
+    "category": "Financial Management/Configuration",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "views/res_company_view.xml",
+    ],
+    "demo": [],
+    "active": False,
+    'installable': True
+}
---
+++ base_currency_rate_exchange_method/models/res_company.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2015 Andhitia Rama. All rights reserved.
+#    @author Andhitia Rama
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class res_company(models.Model):
+    """override company to add exchange rate method"""
+    _inherit = 'res.company'
+    _name = 'res.company'
+
+    exchange_rate_method = fields.Selection(
+        string='Exchange Rate Method',
+        selection=[
+            ('indirect', 'Indirect'),
+            ('direct', 'Direct'),
+            ],
+        required=True,
+        default='indirect',
+        )
