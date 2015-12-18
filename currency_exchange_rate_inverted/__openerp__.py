7a0319f0305e2453b7ff34630ad57e93bf42a7b7
jbeficent <jordi.ballester@eficent.com>
2015-12-18 14:36:55 +0100

    [ADD] module 'currency_exchange_rate_inverted'

---
+++ currency_exchange_rate_inverted/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent
+# © 2015 Techrifiv Solutions Pte Ltd
+# © 2015 Statecraft Systems Pte Ltd
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Currency Exchange Rate Inverted',
+    'version': '8.0.0.1.0',
+    'category': 'Accounting & Finance',
+    'summary': 'Correct the cost of move lines in purchase flows',
+    'author': 'Eficent',
+    'website': 'http://www.eficent.com',
+    'license': 'AGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/res_currency_view.xml'
+    ],
+    'test': [],
+    "installable": True
+}
