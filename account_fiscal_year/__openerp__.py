5ce9df8713d0ec261acabf62fdd915b1db6566b3
Damien Crier <damien.crier@camptocamp.com>
2016-04-27 22:30:28 +0200

    [ADD] readme.txt on modules

--- account_fiscal_year/__openerp__.py
+++ account_fiscal_year/__openerp__.py
@@ -6,17 +6,6 @@
     'name': 'Account Fiscal Year',
     'version': '9.0.0.1.0',
     'category': 'Accounting',
-    'description': """
-    Extend date.range.type to add fiscal_year flag.
-
-    Override official res_company.compute_fiscal_year_dates to get the
-    fiscal year date start / date end for any given date.
-    That methods first looks for a date range of type fiscal year that
-    encloses the give date.
-    If it does not find it, it falls back on the standard Odoo
-    technique based on the day/month of end of fiscal year.
-
-    """,
     'author': 'Camptocamp SA,'
               'Odoo Community Association (OCA)',
     'website': 'http://www.camptocamp.com',
