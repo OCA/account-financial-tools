309eb987d1123436cbe53d1b91ceb271acea1ccb
Damien Crier <damien.crier@camptocamp.com>
2016-04-27 15:32:36 +0200

    [ADD] add module "account_fiscal_year"

---
+++ account_fiscal_year/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Author: Damien Crier
+# Copyright 2016 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import date_range
+from . import date_range_type
+from . import res_company
