9d84ea313e476403e9bb4f81aa82d0fd6ef4bbe1
Damien Crier <damien.crier@camptocamp.com>
2016-04-27 17:32:40 +0200

    [IMP] add details on summary

--- account_fiscal_year/__openerp__.py
+++ account_fiscal_year/__openerp__.py
@@ -7,7 +7,15 @@
     'version': '9.0.0.1.0',
     'category': 'Accounting',
     'summary': """
-        Fiscal Year based on 'date_range' module
+    Extend date.range.type to add fiscal_year flag.
+
+    Override official res_company.compute_fiscal_year_dates to get the
+    fiscal year date start / date end for any given date.
+    That methods first looks for a date range of type fiscal year that
+    encloses the give date.
+    If it does not find it, it falls back on the standard Odoo
+    technique based on the day/month of end of fiscal year.
+
     """,
     'author': 'Camptocamp SA,'
               'Odoo Community Association (OCA)',
