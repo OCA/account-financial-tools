0de95e1110dcb8be0a2ef1ae425bf749781780d3
Damien Crier <damien.crier@camptocamp.com>
2016-04-28 22:59:54 +0200

    [FIX*IMP] account_fiscal_year
    * Add contributors to README
    * try to find a FY start date according to the start date from choosen period
    * Unable to unlink a date_range with type fiscal_year
    * `fiscal_year` flag readonly
    * add menu to date_range under accounting section
    * remove method on  object because it's the same as in  file
    * unable to delete  with flag 'fiscal_year' but can delete
    * clean __openerp__.py
    * account_fiscal_year version number

--- account_fiscal_year/models/__init__.py
+++ account_fiscal_year/models/__init__.py
@@ -3,6 +3,5 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import date_range
 from . import date_range_type
 from . import res_company
