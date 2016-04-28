f577f3f8da4f9251b573fb4f4928b0a504b2e9e6
Damien Crier <damien.crier@camptocamp.com>
2016-04-28 22:59:54 +0200

    [FIX] try to find a FY start date according to the start date from choosen period [IMP] date fields required in ledger wizard

--- account_fiscal_year/models/date_range.py
+++ account_fiscal_year/models/date_range.py
@@ -3,7 +3,7 @@
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp import models
-from openerp.tools import (DEFAULT_SERVER_DATETIME_FORMAT)
+from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
 
 
 class DateRange(models.Model):
@@ -15,7 +15,7 @@ class DateRange(models.Model):
         with @param:date <= date_start
         """
         fy_id = self.env.ref('account_fiscal_year.fiscalyear')
-        date_str = date.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        date_str = date.strftime(DEFAULT_SERVER_DATE_FORMAT)
         s_args = [
             ('type_id', '=', fy_id.id),
             ('date_start', '<=', date_str),
