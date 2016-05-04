3caaaf5a38aa8709fa503faeecf68d8e7380bff2
Yannick Vaucher <yannick.vaucher@camptocamp.com>
2016-05-04 17:11:22 +0200

    Fix contributors, short license headers and copyrights There was missing a bunch of people there!

--- currency_rate_update/services/update_service_PL_NBP.py
+++ currency_rate_update/services/update_service_PL_NBP.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c) 2009 CamptoCamp. All rights reserved.
-#    @author Nicolas Bessi
-#
-#    Abstract class to fetch rates from National Bank of Poland
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2009 Camptocamp
+# © 2009 Grzegorz Grzelak
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from .currency_getter_interface import Currency_getter_interface
 
 from datetime import datetime
