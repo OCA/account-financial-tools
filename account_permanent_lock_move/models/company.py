f3a41da557ef2db08a4a3574deb594808d1063df
Matthieu Dietrich <matthieu.dietrich@camptocamp.com>
2016-05-24 11:42:40 +0200

    New module account_permanent_lock_move

---
+++ account_permanent_lock_move/models/company.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# © 2016 Camptocamp SA (Matthieu Dietrich)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from openerp import _, api, fields, models
+from openerp.exceptions import UserError
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    permanent_lock_date = fields.Date(
+        string="Permanent Lock Date",
+        help="Non-revertible closing of accounts prior to and inclusive of "
+        "this date. Use it for fiscal year locking instead of ""Lock Date"".")
