aaa2d1a227018450f83be8554945cc81393d998a
Angel Moya <amoyapardo@gmail.com>
2016-09-29 19:27:23 +0200

    MIG account_invoice_tax_required
    FIX warning message
    
    (cherry picked from commit 2a76d45b0afb2ae1b9891ea2f4178acbc0e84586)

--- account_invoice_tax_required/__manifest__.py
+++ account_invoice_tax_required/__manifest__.py
@@ -1,31 +1,16 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author Vincent Renaville. Copyright 2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 - Camptocamp SA - Author Vincent Renaville
+# Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Tax required in invoice",
-    'version': "8.0.1.0.0",
-    "author": "Camptocamp,Odoo Community Association (OCA)",
+    'version': "9.0.1.0.0",
+    "author": "Camptocamp,Tecnativa,Odoo Community Association (OCA)",
     'website': "http://www.camptocamp.com",
     'category': "Localisation / Accounting",
     'license': "AGPL-3",
     'depends': ["account"],
     'data': [
     ],
-    'installable': False,
+    'installable': True,
 }
