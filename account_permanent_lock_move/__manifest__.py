67c47edcdaa0818bc2145790f38a18775f703e1f
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-10-12 22:29:02 +0200

    Migration of account_permanent_lock_move to 10.0

--- account_permanent_lock_move/__manifest__.py
+++ account_permanent_lock_move/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Permanent Lock Move",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "depends": ["account"],
     "author": "Camptocamp,Odoo Community Association (OCA)",
     "website": "http://www.camptocamp.com/",
@@ -14,5 +14,5 @@
     ],
     'license': 'AGPL-3',
     "auto_install": False,
-    'installable': False,
+    'installable': True,
 }
