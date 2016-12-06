9f33f9770352206c63a6f98859448d278f810f80
Alexis de Lattre <alexis.delattre@akretion.com>
2016-12-06 08:52:08 +0100

    Port account_check_deposit to v10
    Add option to have the counter-part of the deposit move directly to the bank account
    Use account.config.settings page

--- account_check_deposit/__manifest__.py
+++ account_check_deposit/__manifest__.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-#   account_check_deposit for Odoo
-#   Copyright (C) 2012-2015 Akretion (http://www.akretion.com/)
-#   @author: Benoît GUILLOT <benoit.guillot@akretion.com>
-#   @author: Chafique DELLI <chafique.delli@akretion.com>
-#   @author: Alexis de Lattre <alexis.delattre@akretion.com>
-#
+# © 2012-2015 Akretion (http://www.akretion.com/)
+# @author: Benoît GUILLOT <benoit.guillot@akretion.com>
+# @author: Chafique DELLI <chafique.delli@akretion.com>
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-#
-###############################################################################
 
 {
     'name': 'Account Check Deposit',
-    'version': '8.0.0.1.0',
+    'version': '10.0.1.0.0',
     'category': 'Accounting & Finance',
     'license': 'AGPL-3',
     'summary': 'Manage deposit of checks to the bank',
@@ -26,7 +20,7 @@
         'views/account_deposit_view.xml',
         'views/account_move_line_view.xml',
         'data/account_deposit_sequence.xml',
-        'views/company_view.xml',
+        'views/account_config_settings.xml',
         'security/ir.model.access.csv',
         'security/check_deposit_security.xml',
         'data/account_data.xml',
