b8f9adad7837bc1892b0ce2244cfa3ac568a05ce
Jay Vora(SerpentCS) <jay.vora@serpentcs.com>
2016-12-07 12:02:49 +0530

    [REF]

--- account_credit_control_payment_details/__init__.py
+++ account_credit_control_payment_details/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
-# © 2016 Serpent Consulting Services Pvt. Ltd
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
--- account_credit_control_payment_details/__openerp__.py
+++ account_credit_control_payment_details/__openerp__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Serpent Consulting Services Pvt. Ltd
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
