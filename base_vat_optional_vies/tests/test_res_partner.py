36106a4d8a0cce3516fda48f5757c7f66653c846
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2016-12-13 12:57:06 +0100

    [FIX] Some fixes:   * base_vat_optional_vies: Fix pass lower country code in _split_vat function to find parent function check_vat_'xx' instead of check_vat_'XX'   * base_vat_optional_vies: Convert to upper when write NIF into database

--- base_vat_optional_vies/tests/test_res_partner.py
+++ base_vat_optional_vies/tests/test_res_partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
+# License AGPL-3: Tecnativa S.L. - Antonio Espinosa
 # See README.rst file on addon root folder for more details
 
 from openerp.tests.common import TransactionCase
