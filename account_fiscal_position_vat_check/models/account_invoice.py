e21871e5ec8e9487a1c27d8576e56e90a874652c
Laurent Bélorgey <lb@lalieutenante.com>
2017-01-06 15:28:30 +0100

    [9.0][FIX] account_fiscal_position_vat_check: correct typo

--- account_fiscal_position_vat_check/models/account_invoice.py
+++ account_fiscal_position_vat_check/models/account_invoice.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
-# Copyright 2016 Tecnativa - Vicent Cubells
+# © 2013-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# © 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields, api, _
