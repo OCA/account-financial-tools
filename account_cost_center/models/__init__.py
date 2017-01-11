ab4b708fc766ab18a59dc6e4be0d3763046d6398
Andrea Stirpe <a.stirpe@onestein.nl>
2017-01-11 15:04:57 +0100

    [ADD] account_cost_center

---
+++ account_cost_center/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import account_move_line
+from . import account_invoice
+from . import account_invoice_line
+from . import account_invoice_report
+from . import account_cost_center
---
+++ account_cost_center/models/account_move_line.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    cost_center_id = fields.Many2one(
+        'account.cost.center',
+        index=True,
+        string='Cost Center'
+    )
