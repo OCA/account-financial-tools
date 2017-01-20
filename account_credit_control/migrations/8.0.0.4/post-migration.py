35b8338eb60c13108b045c25a3b49e9cfae84f58
Paulius Sladkevičius <paulius@versada.eu>
2017-01-20 15:03:22 +0200

    account_credit_control: Fix migration script to 8.0.0.4

--- account_credit_control/migrations/8.0.0.4/post-migration.py
+++ account_credit_control/migrations/8.0.0.4/post-migration.py
@@ -7,6 +7,7 @@ def migrate(cr, version):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
         env.cr.execute("""
-            UPDATE credit_control_run SET company_id = (select company_id
-            FROM credit_control_line where policy_id = credit_control_run.id
-            limit 1)""")
+            UPDATE credit_control_run SET company_id = (SELECT ccp.company_id
+            FROM credit_run_policy_rel AS crpr JOIN credit_control_policy AS ccp
+            ON (crpr.policy_id = ccp.id) WHERE crpr.run_id = credit_control_run.id
+            LIMIT 1)""")
