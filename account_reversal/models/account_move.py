170d47ae93122fe49a0a49394f7752a8e5c80aec
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-01-24 11:47:58 +0100

    Fix flake8

--- account_reversal/models/account_move.py
+++ account_reversal/models/account_move.py
@@ -5,8 +5,7 @@
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models, _
-from openerp.exceptions import UserError
+from openerp import api, fields, models
 
 
 class AccountMove(models.Model):
