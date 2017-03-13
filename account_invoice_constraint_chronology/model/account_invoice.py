cda5bc8b9c35acc82755f4b0ff10a208301f9fc9
gilles <meyomesse.gilles@gmail.com>
2017-03-13 14:18:36 +0100

    [MIG] account_invoice_constraint_chronology

--- account_invoice_constraint_chronology/model/account_invoice.py
+++ account_invoice_constraint_chronology/model/account_invoice.py
@@ -1,41 +1,15 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Authors: Adrien Peiffer
-#    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
-#    All Rights Reserved
-#
-#    WARNING: This program as such is intended to be used by professional
-#    programmers who take the whole responsibility of assessing all potential
-#    consequences resulting from its eventual inadequacies and bugs.
-#    End users who are looking for a ready-to-use solution with commercial
-#    guarantees and support are strongly advised to contact a Free Software
-#    Service Company.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, fields
-from openerp.tools.translate import _
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
-from datetime import datetime
-from openerp import exceptions
+from odoo import models, api, fields, _
+from odoo.exceptions import UserError
 
 
 class account_invoice(models.Model):
     _inherit = "account.invoice"
+
+    already_validated = fields.Boolean(readonly=True, copy=False)
 
     @api.multi
     def action_move_create(self):
@@ -51,33 +25,31 @@ class account_invoice(models.Model):
                                  ('journal_id', '=', inv.journal_id.id)],
                                 limit=1)
                 if len(invoices) > 0:
-                    date_invoice_format = datetime\
-                        .strptime(inv.date_invoice,
-                                  DEFAULT_SERVER_DATE_FORMAT)
+                    date_invoice_format = fields.Date.\
+                        from_string(inv.date_invoice)
                     date_invoice_tz = fields\
                         .Date.context_today(self, date_invoice_format)
-                    raise exceptions.Warning(_("Chronology Error."
-                                               " Please confirm older draft"
-                                               " invoices before %s and"
-                                               " try again.") %
-                                             date_invoice_tz)
-
-                if inv.internal_number is False:
+                    raise UserError(_("Chronology Error. "
+                                      "Please confirm older draft "
+                                      "invoices before %s and try again.")
+                                    % date_invoice_tz)
+                if not inv.already_validated:
                     invoices = self.search([('state', 'in', ['open', 'paid']),
                                             ('date_invoice', '>',
                                              inv.date_invoice),
                                             ('journal_id', '=',
                                              inv.journal_id.id)],
                                            limit=1)
+
                     if len(invoices) > 0:
-                        date_invoice_format = datetime\
-                            .strptime(inv.date_invoice,
-                                      DEFAULT_SERVER_DATE_FORMAT)
+                        date_invoice_format = fields.Date.\
+                            from_string(inv.date_invoice)
                         date_invoice_tz = fields\
                             .Date.context_today(self, date_invoice_format)
-                        raise exceptions.Warning(_("Chronology Error. There"
-                                                   " exist at least one"
-                                                   " invoice with a date"
-                                                   " posterior to %s.") %
-                                                 date_invoice_tz)
+                        raise UserError(_("Chronology Error. "
+                                          "There exist at least one invoice "
+                                          "with a date posterior to %s.") %
+                                        date_invoice_tz)
+            if not inv.already_validated:
+                inv.already_validated = True
         return res
