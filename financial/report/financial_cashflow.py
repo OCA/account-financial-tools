8439b2a45ecc21e4faa7a6bc717dee6fbb38070a
vonpupp <vonpupp@gmail.com>
2017-03-14 14:29:32 -0300

    [FIX] Cashflow install problem

--- financial/report/financial_cashflow.py
+++ financial/report/financial_cashflow.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, _, tools
-from ..models.financial_move_model import (
+from ..models.financial_move import (
     FINANCIAL_MOVE,
     FINANCIAL_STATE,
     FINANCIAL_TYPE
