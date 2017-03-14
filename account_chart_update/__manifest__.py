a5cf9fc6ac74aa455af1ef9c3d69b36a7cc7fe50
Sylvain Van Hoof <sylvainvh@gmail.com>
2017-03-14 11:54:08 +0100

    [MIG] account_chart_update: Migration to 10.0

--- account_chart_update/__manifest__.py
+++ account_chart_update/__manifest__.py
@@ -2,14 +2,16 @@
 # Copyright 2015-2017 Pedro Manuel Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2016 Jacques-Etienne Baudoux <je@bcim.be>
+# Copyright 2016 Sylvain Van Hoof <sylvain@okia.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': "Detect changes and update the Account Chart from a template",
     "summary": "Wizard to update a company's account chart from a template",
-    'version': "9.0.1.0.0",
+    'version': "10.0.1.0.0",
     'author': "Tecnativa, "
-              "BCIM,"
+              "BCIM, "
+              "Okia, "
               "Odoo Community Association (OCA)",
     'website': "http://odoo-community.org",
     'depends': ["account"],
@@ -18,6 +20,7 @@
         'Pedro M. Baeza',
         'Jairo Llopis',
         'Jacques-Etienne Baudoux',
+        'Sylvain Van Hoof'
     ],
     'license': "AGPL-3",
     "data": [
