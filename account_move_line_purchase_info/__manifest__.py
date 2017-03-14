5f1e6fda3566f95c4150063d9739d2f4d14529e5
lreficent <lois.rilo@eficent.com>
2017-03-14 11:47:48 +0100

    [MIG] account_move_line_purchase_info

--- account_move_line_purchase_info/__manifest__.py
+++ account_move_line_purchase_info/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Account Move Line Purchase Info",
     "summary": "Introduces the purchase order line to the journal items",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "http://www.github.com/OCA/account-financial-tools",
