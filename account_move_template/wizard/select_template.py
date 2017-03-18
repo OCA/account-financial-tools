4f9ff4f2973754cf1a85b9d296b38ad9b6fbb0a9
zahid <j.zahid@gmail.com>
2017-03-18 11:45:02 +0000

    Update select_template.py

--- account_move_template/wizard/select_template.py
+++ account_move_template/wizard/select_template.py
@@ -26,25 +26,13 @@ import time
 class WizardSelectMoveTemplate(models.TransientModel):
     _name = "wizard.select.move.template"
 
-    template_id = fields.Many2one(
-        comodel_name='account.move.template',
-        string='Move Template',
-        required=True
-    )
-    partner_id = fields.Many2one(
-        comodel_name='res.partner',
-        string='Partner'
-    )
-    line_ids = fields.One2many(
-        comodel_name='wizard.select.move.template.line',
-        inverse_name='template_id',
-        string='Lines'
-    )
-    state = fields.Selection(
-        [('template_selected', 'Template selected')],
-        string='State'
-    )
+    template_id = fields.Many2one(comodel_name='account.move.template', string='Move Template', required=True)
+    partner_id = fields.Many2one(comodel_name='res.partner',string='Partner')
+    line_ids = fields.One2many(comodel_name='wizard.select.move.template.line',inverse_name='template_id',string='Lines')
+    state = fields.Selection([('template_selected', 'Template selected')],string='State')
 
+    # fonction qui verife si le template contient des lignes à montant nulle
+    # remvoi False si le templet contient une ligne à montant nul
     @api.multi
     def check_zero_lines(self):
         if not self.line_ids:
@@ -72,8 +60,7 @@ class WizardSelectMoveTemplate(models.TransientModel):
             return self.load_template()
         self.state = 'template_selected'
 
-        view_rec = self.env['ir.model.data'].get_object_reference(
-            'account_move_template', 'wizard_select_template')
+        view_rec = self.env['ir.model.data'].get_object_reference('account_move_template', 'wizard_select_template')
         view_id = view_rec and view_rec[1] or False
 
         return {
@@ -90,7 +77,8 @@ class WizardSelectMoveTemplate(models.TransientModel):
     @api.multi
     def load_template(self):
         self.ensure_one()
-        account_period_model = self.env['account.period']
+      
+      
         if not self.check_zero_lines():
             raise exceptions.Warning(
                 _('At least one amount has to be non-zero!')
@@ -99,39 +87,17 @@ class WizardSelectMoveTemplate(models.TransientModel):
         for template_line in self.line_ids:
             input_lines[template_line.sequence] = template_line.amount
 
-        period = account_period_model.find()
-        if not period:
-            raise exceptions.Warning(_('Unable to find a valid period !'))
-
         computed_lines = self.template_id.compute_lines(input_lines)
 
         moves = {}
         for line in self.template_id.template_line_ids:
             if line.journal_id.id not in moves:
-                moves[line.journal_id.id] = self._make_move(
-                    self.template_id.name,
-                    period.id,
-                    line.journal_id.id,
-                    self.partner_id.id
-                )
+                moves[line.journal_id.id] = self._make_move(self.template_id.name,line.journal_id.id,self.partner_id.id)
 
-            self._make_move_line(
-                line,
-                computed_lines,
-                moves[line.journal_id.id],
-                period.id,
-                self.partner_id.id
-            )
+            self._make_move_line(line, computed_lines, moves[line.journal_id.id],self.partner_id.id)
             if self.template_id.cross_journals:
                 trans_account_id = self.template_id.transitory_acc_id.id
-                self._make_transitory_move_line(
-                    line,
-                    computed_lines,
-                    moves[line.journal_id.id],
-                    period.id,
-                    trans_account_id,
-                    self.partner_id.id
-                )
+                self._make_transitory_move_line(line,computed_lines,moves[line.journal_id.id],trans_account_id,self.partner_id.id)
 
         return {
             'domain': "[('id','in', " + str(moves.values()) + ")]",
@@ -144,18 +110,12 @@ class WizardSelectMoveTemplate(models.TransientModel):
         }
 
     @api.model
-    def _make_move(self, ref, period_id, journal_id, partner_id):
-        move = self.env['account.move'].create({
-            'ref': ref,
-            'period_id': period_id,
-            'journal_id': journal_id,
-            'partner_id': partner_id,
-        })
+    def _make_move(self, ref, journal_id, partner_id):
+        move = self.env['account.move'].create({'ref': ref,'journal_id': journal_id,'partner_id': partner_id,})
         return move.id
 
     @api.model
-    def _make_move_line(self, line, computed_lines,
-                        move_id, period_id, partner_id):
+    def _make_move_line(self, line, computed_lines,move_id, partner_id):
         account_move_line_model = self.env['account.move.line']
         analytic_account_id = False
         if line.analytic_account_id:
@@ -171,7 +131,6 @@ class WizardSelectMoveTemplate(models.TransientModel):
             'name': line.name,
             'move_id': move_id,
             'journal_id': line.journal_id.id,
-            'period_id': period_id,
             'analytic_account_id': analytic_account_id,
             'account_id': line.account_id.id,
             'date': time.strftime('%Y-%m-%d'),
@@ -188,9 +147,8 @@ class WizardSelectMoveTemplate(models.TransientModel):
         return id_line
 
     @api.model
-    def _make_transitory_move_line(self, line,
-                                   computed_lines, move_id, period_id,
-                                   trans_account_id, partner_id):
+    def _make_transitory_move_line(self, line, computed_lines, move_id, trans_account_id, partner_id):
+    
         account_move_line_model = self.env['account.move.line']
         analytic_account_id = False
         if line.analytic_account_id:
@@ -206,7 +164,6 @@ class WizardSelectMoveTemplate(models.TransientModel):
             'name': 'transitory',
             'move_id': move_id,
             'journal_id': line.journal_id.id,
-            'period_id': period_id,
             'analytic_account_id': analytic_account_id,
             'account_id': trans_account_id,
             'date': time.strftime('%Y-%m-%d'),
@@ -224,22 +181,9 @@ class WizardSelectMoveTemplateLine(models.TransientModel):
     _description = 'Template Lines'
     _name = "wizard.select.move.template.line"
 
-    template_id = fields.Many2one(
-        comodel_name='wizard.select.move.template',
-        string='Template'
-    )
+    template_id = fields.Many2one(comodel_name='wizard.select.move.template',string='Template')
     sequence = fields.Integer(string='Number', required=True)
     name = fields.Char(required=True, readonly=True)
-    account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Account',
-        required=True,
-        readonly=True
-    )
-    move_line_type = fields.Selection(
-        [('cr', 'Credit'), ('dr', 'Debit')],
-        string='Move Line Type',
-        required=True,
-        readonly=True
-    )
+    account_id = fields.Many2one(comodel_name='account.account',string='Account',required=True,readonly=True)
+    move_line_type = fields.Selection([('cr', 'Credit'), ('dr', 'Debit')],string='Move Line Type',required=True,readonly=True)
     amount = fields.Float(required=True)
