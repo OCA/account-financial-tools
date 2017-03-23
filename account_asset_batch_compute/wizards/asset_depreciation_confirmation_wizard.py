86d5c55b06fdd26d4e8f316b805cd48b81a6452b
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2017-03-23 07:52:07 +0100

    [MIG] Migration of account_asset_batch_compute

--- account_asset_batch_compute/wizards/asset_depreciation_confirmation_wizard.py
+++ account_asset_batch_compute/wizards/asset_depreciation_confirmation_wizard.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 ACSONE SA/NV
+# Copyright 2016-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models, _
@@ -8,10 +8,9 @@ import logging
 _logger = logging.getLogger(__name__)
 
 try:
-    from openerp.addons.connector.session import ConnectorSession
-    from openerp.addons.connector.queue.job import job
+    from odoo.addons.queue_job.job import job
 except ImportError:
-    _logger.debug('Can not `import connector`.')
+    _logger.debug('Can not `import queue_job`.')
 
     def empty_decorator(func):
         return func
@@ -25,27 +24,19 @@ class AssetDepreciationConfirmationWizard(models.TransientModel):
     batch_processing = fields.Boolean()
 
     @api.multi
+    @job(default_channel='root.account_asset_batch_compute')
     def asset_compute(self):
         self.ensure_one()
         if not self.batch_processing:
             return super(AssetDepreciationConfirmationWizard, self)\
                 .asset_compute()
-        if self.env.context.get('not_async'):
-            return super(AssetDepreciationConfirmationWizard,
-                         self.with_context(asset_batch_processing=True))\
-                         .asset_compute()
+        if not self.env.context.get('job_uuid'):
+            description = \
+                _("Creating jobs to create moves for assets to %s") % (
+                    self.date_end,)
+            job = self.with_delay(description=description).asset_compute()
+            return u'Job created with uuid %s' % (job.uuid,)
         else:
-            session = ConnectorSession.from_env(self.env)
-            description =\
-                _("Creating jobs to create moves for assets period %s") % (
-                    self.period_id.id,)
-            async_asset_compute.delay(session, self.period_id.id,
-                                      description=description)
-
-
-@job(default_channel='root.account_asset_batch_compute')
-def async_asset_compute(session, period_id):
-    model = session.env['asset.depreciation.confirmation.wizard']
-    obj = model.create({'period_id': period_id,
-                        'batch_processing': True})
-    obj.with_context(not_async=True).asset_compute()
+            self = self.with_context(asset_batch_processing=True)
+            return super(AssetDepreciationConfirmationWizard, self)\
+                .asset_compute()
