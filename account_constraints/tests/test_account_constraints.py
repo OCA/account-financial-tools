bfa721d6dcba7c713926147fc51b2741c173828c
Mourad Elhadj Mimoune <mourad.elhadj.mimoune@akretion.com>
2017-03-27 17:00:34 +0200

    [MIG] account_constraints: Migrated to 10.0

--- account_constraints/tests/test_account_constraints.py
+++ account_constraints/tests/test_account_constraints.py
@@ -1,54 +1,30 @@
 # -*- coding: utf-8 -*-
-#
-#
-#    Authors: Adrien Peiffer
-#    Copyright (c) 2014 Acsone SA/NV (http://www.acsone.eu)
-#    All Rights Reserved
-#
-#    WARNING: This program as such is intended to be used by professional
-#    programmers who take the whole responsibility of assessing all potential
-#    consequences resulting from its eventual inadequacies and bugs.
-#    End users who are looking for a ready-to-use solution with commercial
-#    guarantees and support are strongly advised to contact a Free Software
-#    Service Company.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#
+# © 2014, Adrien Peiffer, Acsone SA/NV (http://www.acsone.eu).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import openerp.tests.common as common
 from datetime import datetime
-from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
-from openerp import workflow, exceptions
+from odoo.tools import DEFAULT_SERVER_DATE_FORMAT
+from odoo import workflow, exceptions
 
 
 def create_simple_invoice(self):
     partner_id = self.ref('base.res_partner_2')
     product_id = self.ref('product.product_product_4')
     today = datetime.now()
-    journal_id = self.ref('account.sales_journal')
+    journal_id = self.env['account.journal'].search([('type','=','sale')])[0]
+    account_id = self.env['account.account'].search([('internal_type','=','receivable')])[0]
+    account_id_other = self.env['account.account'].search([('internal_type','=','other')])[0]
+    
     date = today.strftime(DEFAULT_SERVER_DATE_FORMAT)
     return self.env['account.invoice']\
         .create({'partner_id': partner_id,
-                 'account_id':
-                 self.ref('account.a_recv'),
-                 'journal_id':
-                 journal_id,
+                 'account_id': account_id,
+                 'journal_id': journal_id.id,
                  'date_invoice': date,
                  'invoice_line': [(0, 0, {'name': 'test',
                                           'account_id':
-                                          self.ref('account.a_sale'),
+                                          account_id_other.id,
                                           'price_unit': 2000.00,
                                           'quantity': 1,
                                           'product_id': product_id,
