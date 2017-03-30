fa8bb651d5edf6228207833a3290183c8fef2e25
Angel Moya - PESOL <amoyapardo@gmail.com>
2017-03-30 20:06:15 +0200

    account_invoice_tax_required (#404)

--- account_invoice_tax_required/README.rst
+++ account_invoice_tax_required/README.rst
@@ -1,19 +1,37 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+==================================================
 Account invoice with tax required in invoice line
 ==================================================
 
 This module adds functional a check on invoice
 to force user to set tax on invoice line.
 
+Usage
+=====
+
+To use this module, you need to:
+
+#. Go to Accounting (or Invoicing) > Sales > Customer Invoices.
+#. Create a new invoice.
+#. Insert a new invoice line without taxes.
+#. Press on "Validate".
+#. An error will raise preventing to do it.
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/92/9.0
 
 
 Bug Tracker
 ===========
 
-Bugs are tracked on `GitHub Issues <https://github.com/OCA/account-financial-tools/issues>`_.
-In case of trouble, please check there if your issue has already been reported.
-If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/account-financial-tools/issues/new?body=module:%20account_invoice_tax_required%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
-
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/account-financial-tools/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
 
 Credits
 =======
@@ -22,17 +40,19 @@ Contributors
 ------------
 
 * Vincent Renaville <vincent.renaville@camptocamp.com>
+* Angel Moya <odoo@tecnativa.com>
 
 Maintainer
 ----------
 
-.. image:: http://odoo-community.org/logo.png
+.. image:: https://odoo-community.org/logo.png
    :alt: Odoo Community Association
-   :target: http://odoo-community.org
+   :target: https://odoo-community.org
 
 This module is maintained by the OCA.
 
-OCA, or the Odoo Community Association, is a nonprofit organization whose mission is to support the collaborative development of Odoo features and promote its widespread use.
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
 
-To contribute to this module, please visit http://odoo-community.org.
-
+To contribute to this module, please visit https://odoo-community.org.
---
+++ account_invoice_tax_required/tests/test_account_invoice_tax_required.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp.tests.common import TransactionCase
+from openerp import exceptions
+
+
+class TestAccountInvoiceTaxRequired(TransactionCase):
+
+    def setUp(self):
+        super(TestAccountInvoiceTaxRequired, self).setUp()
+
+        self.account_invoice = self.env['account.invoice']
+        self.account_journal = self.env['account.journal']
+        self.journal = self.account_journal.create({
+            'code': 'test',
+            'name': 'test',
+            'type': 'sale'
+        })
+        self.partner = self.env.ref('base.res_partner_3')
+        account_user_type = self.env.ref(
+            'account.data_account_type_receivable')
+
+        self.account_account = self.env['account.account']
+        self.account_rec1_id = self.account_account.create(dict(
+            code="cust_acc",
+            name="customer account",
+            user_type_id=account_user_type.id,
+            reconcile=True,
+        ))
+        self.product_product = self.env['product.product']
+        self.product = self.product_product.create({
+            'name': 'Test',
+            'categ_id': self.env.ref(
+                "product.product_category_all").id,
+            'standard_price': 50,
+            'list_price': 100,
+            'type': 'service',
+            'uom_id': self.env.ref("product.product_uom_unit").id,
+            'uom_po_id': self.env.ref("product.product_uom_unit").id,
+            'description': 'Test',
+        })
+
+        invoice_line_data = [(0, 0, {
+            'product_id': self.product.id,
+            'quantity': 10.0,
+            'account_id': self.account_account.search(
+                [('user_type_id',
+                  '=',
+                  self.env.ref('account.data_account_type_revenue').id)
+                 ], limit=1).id,
+            'name': 'product test 5',
+            'price_unit': 100.00,
+        })]
+
+        self.invoice = self.account_invoice.create(dict(
+            name="Test Customer Invoice",
+            reference_type="none",
+            journal_id=self.journal.id,
+            partner_id=self.partner.id,
+            account_id=self.account_rec1_id.id,
+            invoice_line_ids=invoice_line_data
+        ))
+
+    def test_exception(self):
+        """Validate invoice without tax must raise exception
+        """
+        self.invoice.action_date_assign()
+        self.invoice.action_move_create()
+        with self.assertRaises(exceptions.Warning):
+            self.invoice.with_context(
+                test_tax_required=True).invoice_validate()
