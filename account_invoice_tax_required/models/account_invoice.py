4e100442b5f1a1d0e6eeb7938f0dad4dc79831e6
Angel Moya - PESOL <amoyapardo@gmail.com>
2017-03-30 20:06:15 +0200

    account_invoice_tax_required (#404)

--- account_invoice_tax_required/models/account_invoice.py
+++ account_invoice_tax_required/models/account_invoice.py
@@ -1,23 +1,10 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author Vincent Renaville. Copyright 2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2015 - Camptocamp SA - Author Vincent Renaville
+# Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from openerp import models, api, exceptions, _
+from openerp.tools import config
 
 
 class AccountInvoice(models.Model):
@@ -27,20 +14,22 @@ class AccountInvoice(models.Model):
     def test_invoice_line_tax(self):
         errors = []
         error_template = _("Invoice has a line with product %s with no taxes")
-        for invoice in self:
-            for invoice_line in invoice.invoice_line:
-                if not invoice_line.invoice_line_tax_id:
-                    error_string = error_template % (invoice_line.name)
-                    errors.append(error_string)
+        for invoice_line in self.mapped('invoice_line_ids'):
+            if not invoice_line.invoice_line_tax_ids:
+                error_string = error_template % (invoice_line.name)
+                errors.append(error_string)
         if errors:
-            errors_full_string = ','.join(x for x in errors)
-            raise exceptions.Warning(_('No Taxes Defined!'),
-                                     errors_full_string)
+            raise exceptions.Warning(
+                _('%s\n%s') % (_('No Taxes Defined!'),
+                               '\n'.join(x for x in errors))
+            )
         else:
             return True
 
     @api.multi
     def invoice_validate(self):
-        self.test_invoice_line_tax()
+        if not (config['test_enable'] and
+                not self.env.context.get('test_tax_required')):
+            self.test_invoice_line_tax()
         res = super(AccountInvoice, self).invoice_validate()
         return res
