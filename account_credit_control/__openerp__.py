26557ddb249003a7a95ebcfbae7e9c5012a95872
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2017-05-02 12:19:24 +0200

    account_credit_control: remove demo file.
    (cherry picked from commit c32d167ac05ebef2256e3cb5919c13e7b9229fdf)

--- account_credit_control/__openerp__.py
+++ account_credit_control/__openerp__.py
@@ -29,6 +29,7 @@
              'email_template',
              ],
  'website': 'http://www.camptocamp.com',
+<<<<<<< c22b2dde9403c91dc4e4f0835f97c737e54bdb43:account_credit_control/__openerp__.py
  'data': ["report/report.xml",
           "report/report_credit_control_summary.xml",
           "data.xml",
@@ -46,6 +47,37 @@
  'demo': ["credit_control_demo.xml"],
  'tests': [],
  'installable': False,
+=======
+ 'data': [
+     "security/res_groups.xml",
+     # Reports
+     "report/report.xml",
+     "report/report_credit_control_summary.xml",
+
+     # Data
+     "data/data.xml",
+
+     # Views
+     "views/account_invoice.xml",
+     "views/credit_control_line.xml",
+     "views/credit_control_policy.xml",
+     "views/credit_control_run.xml",
+     "views/res_company.xml",
+     "views/res_partner.xml",
+
+     # Wizards
+     "wizard/credit_control_emailer_view.xml",
+     "wizard/credit_control_marker_view.xml",
+     "wizard/credit_control_printer_view.xml",
+     "wizard/credit_control_policy_changer_view.xml",
+
+     # Security
+     "security/ir.model.access.csv",
+ ],
+ 'demo': [
+ ],
+ 'installable': True,
+>>>>>>> account_credit_control: remove demo file.:account_credit_control/__manifest__.py
  'license': 'AGPL-3',
  'application': True
  }
