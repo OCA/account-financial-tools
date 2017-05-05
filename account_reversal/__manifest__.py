2c9c1c4ca8b97903da37ecab45fc406778044f86
Cédric Pigeon (ACSONE) <cedric.pigeon@acsone.eu>
2017-05-06 00:57:47 +0200

    [10.0][MIG] account_reversal migration (#445)
    * OCA Transbot updated translations from Transifex
    * Add option to reconcile the reversal move with the original move (#387)
    * [IMP] account_reversal: Several improvements
    * Default date for reverse move is not start_date of the period following the period of the original move (and not the period following today's period)
    * Return form view of account.move if only 1 move is reversed
    * Add option to reconcile with the reversal entry.
    * [10.0][MIG] account_reversal migration
    * [CHG] skid Odoo method to allow better performance on huge volume

--- account_reversal/__manifest__.py
+++ account_reversal/__manifest__.py
@@ -1,40 +1,28 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Account reversal module for OpenERP
-#    Copyright (C) 2011 Akretion (http://www.akretion.com). All Rights Reserved
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#    Copyright 2012-2013 Camptocamp SA
-#    @author Guewen Baconnier (Camptocamp)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# Copyright 2011 Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2012-2013 Guewen Baconnier (Camptocamp)
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Reversal',
-    'version': '8.0.1.0.0',
-    'category': 'Generic Modules/Accounting',
-    'license': 'AGPL-3',
-    'author': "Akretion,Camptocamp,Odoo Community Association (OCA)",
-    'website': 'http://www.akretion.com/',
-    'depends': ['account'],
-    'data': [
-        'account_view.xml',
-        'wizard/account_move_reverse_view.xml'
-        ],
-    'installable': False,
-    'active': False,
+    "name": "Account Reversal",
+    "summary": "Wizard for creating a reversal account move",
+    "version": "10.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://odoo-community.org/",
+    "author": "Akretion,"
+              "Camptocamp,"
+              "ACSONE SA/NV,"
+              "Tecnativa,"
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account"
+    ],
+    "data": [
+        "wizard/account_move_reverse_view.xml",
+        "views/account_move_view.xml",
+    ],
 }
