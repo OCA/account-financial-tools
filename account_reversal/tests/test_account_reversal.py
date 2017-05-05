0044c02bc5aab3f2b72979e162f1446da830aa09
Cédric Pigeon (ACSONE) <cedric.pigeon@acsone.eu>
2017-05-06 00:57:47 +0200

    [10.0][MIG] account_reversal migration (#445)
    * OCA Transbot updated translations from Transifex
    * Add option to reconcile the reversal move with the original move (#387)
    * [IMP] account_reversal: Several improvements
    * Default date for reverse move is not start_date of the period following the period of the original move (and not the period following today's period)
    * Return form view of account.move if only 1 move is reversed
    * Add option to reconcile with the reversal entry.
    * [10.0][MIG] account_reversal migration
    * [CHG] skid Odoo method to allow better performance on huge volume

--- account_reversal/tests/test_account_reversal.py
+++ account_reversal/tests/test_account_reversal.py
@@ -1,120 +1,116 @@
-# -*- encoding: utf-8 -*-
-# #############################################################################
-#
-# Account partner required module for OpenERP
-#    Copyright (C) 2014 Acsone (http://acsone.eu).
-#    @author Stéphane Bidoul <stephane.bidoul@acsone.eu>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2014 Stéphane Bidoul <stephane.bidoul@acsone.eu>
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import time
-from datetime import datetime
-
-from openerp.tests import common
-from openerp import fields
+from odoo.tests.common import TransactionCase
+import random
 
 
-class test_account_reversal(common.TransactionCase):
+class TestAccountReversal(TransactionCase):
 
     def setUp(self):
-        super(test_account_reversal, self).setUp()
+        super(TestAccountReversal, self).setUp()
         self.move_obj = self.env['account.move']
         self.move_line_obj = self.env['account.move.line']
-
-    def _create_move(self, with_partner, amount=100, period=None):
-        date = datetime.now()
-        company_id = self.env.ref('base.main_company').id
-        period = period or self.env.ref('account.period_0')
-
-        journal = self.env['account.journal'].create({
+        self.company_id = self.env.ref('base.main_company').id
+        self.partner = self.env['res.partner'].create({
+            'name': 'Test partner',
+        })
+        self.journal = self.env['account.journal'].create({
             'name': 'Test journal',
             'code': 'COD',
             'type': 'sale',
-            'sequence_id': self.env.ref('account.sequence_sale_journal').id,
-            'company_id': company_id})
+            'company_id': self.company_id
+        })
+        type_revenue = self.env.ref('account.data_account_type_revenue')
+        type_payable = self.env.ref('account.data_account_type_payable')
+        self.account_sale = self.env['account.account'].create({
+            'name': 'Test sale',
+            'code': 'XX_700',
+            'user_type_id': type_revenue.id,
+        })
+        self.account_customer = self.env['account.account'].create({
+            'name': 'Test customer',
+            'code': 'XX_430',
+            'user_type_id': type_payable.id,
+            'reconcile': True,
+        })
 
+    def _create_move(self, with_partner=True, amount=100):
         move_vals = {
-            'journal_id': journal.id,
-            'period_id': period.id,
-            'date': date,
-            'company_id': company_id,
-        }
-
-        # Why this doesn't work I don't know:
-        # acct = self.ref('account.a_sale'
-        account1, account2 = self.env['account.account'].search(
-            [('company_id', '=', company_id), ('type', '=', 'other')])[:2]
-
-        move_id = self.move_obj.create(move_vals)
-        self.move_line_obj.create({
-            'move_id': move_id.id,
-            'name': '/',
-            'debit': 0,
-            'credit': amount,
-            'company_id': company_id,
-            'account_id': account1.id})
-        move_line_id = self.move_line_obj.create(
-            {
-                'move_id': move_id.id,
+            'journal_id': self.journal.id,
+            'company_id': self.company_id,
+            'line_ids': [(0, 0, {
                 'name': '/',
                 'debit': amount,
                 'credit': 0,
-                'account_id': account2.id,
-                'company_id': company_id,
-                'partner_id': self.ref('base.res_partner_1')
-                if with_partner else False
-            }
-        )
-        return move_line_id.move_id
+                'account_id': self.account_customer.id,
+                'company_id': self.company_id,
+                'partner_id': with_partner and self.partner.id
+            }), (0, 0, {
+                'name': '/',
+                'debit': 0,
+                'credit': amount,
+                'company_id': self.company_id,
+                'account_id': self.account_sale.id,
+            })]
+        }
+        return self.move_obj.create(move_vals)
 
-    def _close_period(self, period_id):
-        self.env.cr.execute('update account_journal_period '
-                            'set state=%s where period_id=%s',
-                            ('done', period_id))
-        self.env.cr.execute('update account_period '
-                            'set state=%s where id=%s',
-                            ('done', period_id))
-        self.env.invalidate_all()
+    def _move_str(self, move):
+        return ''.join(['%.2f%.2f%s' % (
+            x.debit, x.credit, x.account_id == self.account_sale and
+            ':SALE_' or ':CUSTOMER_')
+            for x in move.line_ids.sorted(key=lambda r: r.account_id.id)])
 
     def test_reverse(self):
-        move = self._create_move(with_partner=False)
-        company_id = self.env.ref('base.main_company').id
-        account1 = self.env['account.account'].search(
-            [('company_id', '=', company_id), ('type', '=', 'other')])[0]
-        movestr = ''.join(['%.2f%.2f%s' % (x.debit, x.credit,
-                                           x.account_id == account1 and
-                                           'aaaa' or 'bbbb')
-                           for x in move.line_id])
-        self.assertEqual(movestr, '100.000.00bbbb0.00100.00aaaa')
-        yesterday_date = datetime(
-            year=time.localtime().tm_year, month=3, day=3
-        )
-        yesterday = fields.Date.to_string(yesterday_date)
-        reversed_move_ids = move.create_reversals(yesterday)
-        reversed_moves = self.env['account.move'].browse(reversed_move_ids)
-        movestr_reversed = ''.join(
-            ['%.2f%.2f%s' % (x.debit, x.credit,
-                             x.account_id == account1 and 'aaaa' or 'bbbb')
-             for x in reversed_moves.line_id])
-        self.assertEqual(movestr_reversed, '0.00100.00bbbb100.000.00aaaa')
+        move = self._create_move()
+        self.assertEqual(
+            self._move_str(move), '0.00100.00:SALE_100.000.00:CUSTOMER_')
+        move_prefix = 'REV_TEST_MOVE:'
+        line_prefix = 'REV_TEST_LINE:'
+        rev = move.create_reversals(move_prefix=move_prefix,
+                                    line_prefix=line_prefix, reconcile=True)
+        self.assertEqual(len(rev), 1)
+        self.assertEqual(rev.state, 'posted')
+        self.assertEqual(
+            self._move_str(rev), '100.000.00:SALE_0.00100.00:CUSTOMER_')
+        self.assertEqual(rev.ref[0:len(move_prefix)], move_prefix)
+        for line in rev.line_ids:
+            self.assertEqual(line.name[0:len(line_prefix)], line_prefix)
+            if line.account_id.reconcile:
+                self.assertTrue(line.reconciled)
 
-    def test_reverse_closed_period(self):
-        move_period = self.env.ref('account.period_0')
-        move = self._create_move(with_partner=False, period=move_period)
-        self._close_period(move_period.id)
-        reversal_period = self.env.ref('account.period_1')
-        move.create_reversals(reversal_date=reversal_period.date_start,
-                              reversal_period_id=reversal_period.id)
+    def test_reverse_huge_move(self):
+
+        move = self._create_move()
+
+        for x in range(1, 100):
+            amount = random.randint(10, 100) * x
+            move.write({
+                'line_ids': [(0, 0, {
+                    'name': '/',
+                    'debit': amount,
+                    'credit': 0,
+                    'account_id': self.account_customer.id,
+                    'company_id': self.company_id,
+                    'partner_id': self.partner.id
+                }), (0, 0, {
+                    'name': '/',
+                    'debit': 0,
+                    'credit': amount,
+                    'company_id': self.company_id,
+                    'account_id': self.account_sale.id,
+                })]
+            })
+        self.assertEqual(len(move.line_ids), 200)
+
+        move_prefix = 'REV_TEST_MOVE:'
+        line_prefix = 'REV_TEST_LINE:'
+
+        rev = move.create_reversals(move_prefix=move_prefix,
+                                    line_prefix=line_prefix, reconcile=True)
+
+        self.assertEqual(len(rev.line_ids), 200)
+        self.assertEqual(rev.state, 'posted')
--- account_reversal/wizard/account_move_reverse.py
+++ account_reversal/wizard/account_move_reverse.py
@@ -1,116 +1,75 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Account reversal module for OpenERP
-#    Copyright (C) 2011 Akretion (http://www.akretion.com). All Rights Reserved
-#    @author Alexis de Lattre <alexis.delattre@akretion.com>
-#    Copyright (c) 2012-2013 Camptocamp SA (http://www.camptocamp.com)
-#    @author Guewen Baconnier
-#
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2011 Alexis de Lattre <alexis.delattre@akretion.com>
+# Copyright 2012-2013 Guewen Baconnier (Camptocamp)
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.osv import orm, fields
-from openerp.tools.translate import _
+from odoo import api, fields, models, _
 
 
-class account_move_reversal(orm.TransientModel):
+class AccountMoveReverse(models.TransientModel):
     _name = "account.move.reverse"
     _description = "Create reversal of account moves"
 
-    _columns = {
-        'date': fields.date(
-            'Reversal Date',
-            required=True,
-            help="Enter the date of the reversal account entries. "
-                 "By default, OpenERP proposes the first day of "
-                 "the next period."),
-        'period_id': fields.many2one(
-            'account.period',
-            'Reversal Period',
-            help="If empty, take the period of the date."),
-        'journal_id': fields.many2one(
-            'account.journal',
-            'Reversal Journal',
-            help='If empty, uses the journal of the journal entry '
-                 'to be reversed.'),
-        'move_prefix': fields.char(
-            'Entries Ref. Prefix',
-            help="Prefix that will be added to the 'Ref' of the journal "
-                 "entry to be reversed to create the 'Ref' of the "
-                 "reversal journal entry (no space added after the prefix)."),
-        'move_line_prefix': fields.char(
-            'Items Name Prefix',
-            help="Prefix that will be added to the name of the journal "
-                 "item to be reversed to create the name of the reversal "
-                 "journal item (a space is added after the prefix)."),
-    }
+    def _default_date(self):
+        active_id = (self.env.context.get('active_id') or
+                     self.env.context.get('active_ids', [None])[0])
+        move = self.env['account.move'].browse(active_id)
+        return move.date or fields.Date.today()
 
-    def _next_period_first_date(self, cr, uid, context=None):
-        if context is None:
-            context = {}
-        res = False
-        period_ctx = context.copy()
-        period_ctx['account_period_prefer_normal'] = True
-        period_obj = self.pool.get('account.period')
-        today_period_id = period_obj.find(cr, uid, context=period_ctx)
-        if today_period_id:
-            today_period = period_obj.browse(
-                cr, uid, today_period_id[0], context=context)
-            next_period_id = period_obj.next(
-                cr, uid, today_period, 1, context=context)
-            if next_period_id:
-                next_period = period_obj.browse(
-                    cr, uid, next_period_id, context=context)
-                res = next_period.date_start
-        return res
+    def _default_journal_id(self):
+        active_id = (self.env.context.get('active_id') or
+                     self.env.context.get('active_ids', [None])[0])
+        move = self.env['account.move'].browse(active_id)
+        return move.journal_id.id
 
-    _defaults = {
-        'date': _next_period_first_date,
-        'move_line_prefix': 'REV -',
-    }
+    date = fields.Date(
+        string="Reversal Date", required=True, default=_default_date,
+        help="Enter the date of the reversal account entries. "
+             "By default, Odoo proposes the same date of the move to reverse.")
+    journal_id = fields.Many2one(
+        comodel_name='account.journal', string="Reversal Journal",
+        default=_default_journal_id,
+        help="Enter the date of the reversal account entries. "
+             "If empty, Odoo uses the same journal of the move to reverse.")
+    move_prefix = fields.Char(
+        string="Entries Ref. Prefix",
+        help="Prefix that will be added to the 'Ref' of the reversal account "
+             "entries. If empty, Odoo uses the Ref of the move to reverse. "
+             "(NOTE: A space is added after the prefix).")
+    line_prefix = fields.Char(
+        string="Items Name Prefix",
+        help="Prefix that will be added to the 'Name' of the reversal account "
+             "entrie items. If empty, Odoo uses the same name of the move "
+             "line to reverse. (NOTE: A space is added after the prefix).")
+    reconcile = fields.Boolean(
+        string="Reconcile", default=True,
+        help="Mark this if you want to reconcile items of both moves.")
 
-    def action_reverse(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-        assert 'active_ids' in context, "active_ids missing in context"
-
-        form = self.read(cr, uid, ids, context=context)[0]
-
-        mod_obj = self.pool.get('ir.model.data')
-        act_obj = self.pool.get('ir.actions.act_window')
-        move_obj = self.pool.get('account.move')
-        move_ids = context['active_ids']
-
-        period_id = form['period_id'][0] if form.get('period_id') else False
-        journal_id = form['journal_id'][0] if form.get('journal_id') else False
-        reversed_move_ids = move_obj.create_reversals(
-            cr, uid,
-            move_ids,
-            form['date'],
-            reversal_period_id=period_id,
-            reversal_journal_id=journal_id,
-            move_prefix=form['move_prefix'],
-            move_line_prefix=form['move_line_prefix'],
-            context=context)
-
-        __, action_id = mod_obj.get_object_reference(
-            cr, uid, 'account', 'action_move_journal_line')
-        action = act_obj.read(cr, uid, [action_id], context=context)[0]
-        action['domain'] = unicode([('id', 'in', reversed_move_ids)])
-        action['name'] = _('Reversal Entries')
-        action['context'] = unicode({'search_default_to_be_reversed': 0})
+    @api.multi
+    def action_reverse(self):
+        moves = self.env['account.move']
+        for wizard in self:
+            orig = moves.browse(self.env.context.get('active_ids'))
+            moves |= orig.create_reversals(
+                date=wizard.date, journal=wizard.journal_id,
+                move_prefix=wizard.move_prefix, line_prefix=wizard.line_prefix,
+                reconcile=wizard.reconcile)
+        action = {
+            'name': _('Reverse moves'),
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'res_model': 'account.move',
+            'context': {'search_default_to_be_reversed': 0},
+        }
+        if len(moves) == 1:
+            action.update({
+                'view_mode': 'form,tree',
+                'res_id': moves.id,
+            })
+        else:
+            action.update({
+                'view_mode': 'tree,form',
+                'domain': [('id', 'in', moves.ids)],
+            })
         return action
