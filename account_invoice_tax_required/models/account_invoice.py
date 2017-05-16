f3ad862909af1b92d22e988af30be742e9fc27a1
Kitti U <kittiu@gmail.com>
2017-05-16 23:22:39 +0700

    [MIG] account_tax_invoice_required: Migrated to 10.0

--- account_invoice_tax_required/models/account_invoice.py
+++ account_invoice_tax_required/models/account_invoice.py
@@ -3,15 +3,16 @@
 # Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api, exceptions, _
-from openerp.tools import config
+from odoo import models, api, _
+from odoo.exceptions import UserError
+from odoo.tools import config
 
 
 class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
     @api.multi
-    def test_invoice_line_tax(self):
+    def _test_invoice_line_tax(self):
         errors = []
         error_template = _("Invoice has a line with product %s with no taxes")
         for invoice_line in self.mapped('invoice_line_ids'):
@@ -19,17 +20,15 @@ class AccountInvoice(models.Model):
                 error_string = error_template % (invoice_line.name)
                 errors.append(error_string)
         if errors:
-            raise exceptions.Warning(
+            raise UserError(
                 _('%s\n%s') % (_('No Taxes Defined!'),
                                '\n'.join(x for x in errors))
             )
-        else:
-            return True
 
     @api.multi
     def invoice_validate(self):
         if not (config['test_enable'] and
                 not self.env.context.get('test_tax_required')):
-            self.test_invoice_line_tax()
+            self._test_invoice_line_tax()
         res = super(AccountInvoice, self).invoice_validate()
         return res
