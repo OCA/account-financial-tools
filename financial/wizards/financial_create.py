8d2cc57ec10b4c0817451638b71c8cc1092a8f41
Luis Felipe Mileo <mileo@kmee.com.br>
2017-06-01 12:28:59 -0300

    [REF] Financial move for better integration with another modules

--- financial/wizards/financial_create.py
+++ financial/wizards/financial_create.py
@@ -40,6 +40,9 @@ class FinancialMoveCreate(models.TransientModel):
         string=u'Financial date',
         default=fields.Date.context_today,
     )
+    amount = fields.Monetary(
+        required=False,
+    )
     amount_total = fields.Monetary(
         string=u'Total',
         readonly=True,
@@ -57,11 +60,9 @@ class FinancialMoveCreate(models.TransientModel):
                 self.date and self.amount > 0.00):
             return res
 
-        computations = \
-            self.payment_term_id.compute(self.amount, self.date)
-
+        computations = self.payment_term_id.compute(self.amount, self.date)[0]
         payment_ids = []
-        for idx, item in enumerate(computations[0]):
+        for idx, item in enumerate(computations):
             payment = dict(
                 document_item=self.document_number + '/' + str(idx + 1),
                 date_maturity=item[0],
@@ -70,9 +71,43 @@ class FinancialMoveCreate(models.TransientModel):
             payment_ids.append((0, False, payment))
         self.line_ids = payment_ids
 
+    def _prepare_move_item(self, item):
+        return {
+            'document_number': item.document_item,
+            'date_maturity': item.date_maturity,
+            'amount': item.amount,
+        }
+
+    def _prepare_financial_move(self):
+        return {
+            'date': self.date,
+            'financial_type': self.financial_type,
+            'partner_id': self.partner_id.id,
+            'bank_id': self.bank_id.id,
+            'company_id': self.company_id and self.company_id.id,
+            'currency_id': self.currency_id.id,
+            'payment_term_id':
+                self.payment_term_id and self.payment_term_id.id or False,
+            'account_type_id':
+                self.account_type_id and
+                self.account_type_id.id or False,
+            'analytic_account_id':
+                self.analytic_account_id and
+                self.analytic_account_id.id or False,
+            'payment_mode_id':
+                self.payment_mode_id and
+                self.payment_mode_id.id or False,
+            'note': self.note,
+            'lines': [
+                self._prepare_move_item(item) for item in self.line_ids
+                ],
+        }
+
     @api.multi
     def compute(self):
-        financial_move = self.env['financial.move'].create_contract(self)
+        p = self._prepare_financial_move()
+        financial_move = self.env['financial.move']._create_from_dict(p)
+        financial_move.action_confirm()
         financial_type = financial_move and financial_move[0].financial_type
         return financial_move.action_view_financial(financial_type)
 
@@ -97,5 +132,5 @@ class FinancialMoveLineCreate(models.TransientModel):
     )
     financial_move_id = fields.Many2one(
         comodel_name='financial.move.create',
-        required=True
+
     )
