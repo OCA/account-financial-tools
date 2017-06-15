9fbea581fc43124b43cbf85bfbf9b518cf940d45
Ronald Portier <ronald@therp.nl>
2017-06-15 11:14:58 +0200

    [ADD] Adapt forwarded ported fix to 8.0 API.

--- account_constraints/wizards/account_fiscalyear_close.py
+++ account_constraints/wizards/account_fiscalyear_close.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 # © 2017 Therp BV <http://therp.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp.osv import orm
+from openerp import api, models
 
 
-class AccountFiscalyearCloseWizard(orm.TransientModel):
+class AccountFiscalyearCloseWizard(models.TransientModel):
     _inherit = 'account.fiscalyear.close'
 
-    def data_save(self, cr, uid, ids, context=None):
-        context = context and context.copy() or {}
-        context['from_parent_object'] = True
-        super(AccountFiscalyearCloseWizard, self).data_save(
-            cr, uid, ids, context=context
-        )
+    @api.multi
+    def data_save(self):
+        super(AccountFiscalyearCloseWizard, self).with_context(
+            from_parent_object=True
+        ).data_save()
