ac56dc27aa5478bd1a10b60ecd185a6e90c8bb38
Akim Juillerat <akim.juillerat@camptocamp.com>
2017-06-21 12:11:10 +0200

    Fix README and headers

--- account_move_locking/__manifest__.py
+++ account_move_locking/__manifest__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Move locked to prevent modification",
     "version": "10.0.1.0.0",
--- account_move_locking/models/account.py
+++ account_move_locking/models/account.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api, exceptions, _
 
