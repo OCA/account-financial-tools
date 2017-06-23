dddf0b139b59253b8fbefdc5a4d4bd976e9f91a8
David Vidal <chienandalu@gmail.com>
2017-06-23 19:53:23 +0200

    [MIG] base_vat_optional_vies: Migration to 10.0

--- base_vat_optional_vies/__manifest__.py
+++ base_vat_optional_vies/__manifest__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Tecnativa S.L. - Antonio Espinosa
-# See README.rst file on addon root folder for more details
-
+# Copyright 2015 Tecnativa - Antonio Espinosa
+# Copyright 2016 Tecnativa - Sergio Teruel
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Optional validation of VAT via VIES",
     'category': 'Accounting',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'depends': [
         'base_vat',
     ],
@@ -15,11 +16,9 @@
     'data': [
         'views/res_partner_view.xml',
     ],
-    'author': 'Antiun Ingeniería S.L., '
-              'Tecnativa,'
+    'author': 'Tecnativa,'
               'Odoo Community Association (OCA)',
-    'website': 'http://www.tecnativa.com',
+    'website': 'https://www.tecnativa.com',
     'license': 'AGPL-3',
-    'images': [],
     'installable': True,
 }
--- base_vat_optional_vies/models/res_partner.py
+++ base_vat_optional_vies/models/res_partner.py
@@ -1,23 +1,9 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Tecnativa S.L. - Antonio Espinosa
-# See README.rst file on addon root folder for more details
+# Copyright 2015 Tecnativa - Antonio Espinosa
+# Copyright 2017 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import logging
-import re
-from openerp import models, fields, api
-from openerp.exceptions import ValidationError
-
-_logger = logging.getLogger(__name__)
-
-try:
-    import vatnumber
-except ImportError:
-    _logger.warning(
-        "VAT validation partially unavailable because the `vatnumber` Python "
-        "library cannot be found. Install it to support more countries, "
-        "for example with `easy_install vatnumber` or "
-        "`pip install vatnumber`.")
-    vatnumber = None
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -26,112 +12,31 @@ class ResPartner(models.Model):
     vies_passed = fields.Boolean(
         string="VIES validation passed", readonly=True)
 
-    def __init__(self, pool, cr):
-        super(ResPartner, self).__init__(pool, cr)
-        self._constraints = []
+    @api.model
+    def simple_vat_check(self, country_code, vat_number):
+        res = super(ResPartner, self).simple_vat_check(
+            country_code, vat_number,
+        )
+        partner = self.env.context.get('vat_partner')
+        if partner and self.vies_passed:
+            # Can not be sure that this VAT is signed up in VIES
+            partner.update({'vies_passed': False})
+        return res
+
+    @api.model
+    def vies_vat_check(self, country_code, vat_number):
+        partner = self.env.context.get('vat_partner')
+        if partner:
+            # If there's an exception checking VIES, the upstream method will
+            # call simple_vat_check and thus the flag will be removed
+            partner.update({'vies_passed': True})
+        res = super(ResPartner, self).vies_vat_check(country_code, vat_number)
+        if not res:
+            return self.simple_vat_check(country_code, vat_number)
+        return res
 
     @api.constrains('vat')
     def check_vat(self):
         for partner in self:
-            if (not self.env.context.get('avoid_check_vat') and
-                    not partner.parent_id):
-                if not partner.validate_vat():
-                    raise ValidationError(partner._construct_constraint_msg())
-
-    @api.multi
-    def button_check_vat(self):
-        if not self.validate_vat():
-            raise ValidationError(self._construct_constraint_msg())
-        return True
-
-    def _split_vat(self, vat, country=False):
-        """
-        @summary: Split Partner vat into country_code and number
-        @result: (vat_country, vat_number)
-        """
-        vat_country = 'XX'
-        vat_number = vat
-        if vat and re.match(r'[A-Za-z]{2}', vat):
-            vat_country = vat[:2].lower()
-            vat_number = vat[2:].replace(' ', '')
-        elif country:
-            vat_country = country
-        return vat_country, vat_number
-
-    @api.multi
-    def validate_vat(self):
-        self.ensure_one()
-        if self.company_id.vat_check_vies:
-            # VIES online check
-            check_func = self.vies_vat_optional_check
-        else:
-            # quick and partial off-line checksum validation
-            check_func = self.simple_vat_optional_check
-        vat_country, vat_number = self._split_vat(self.vat)
-        if vat_number and vat_country == 'XX':
-            _logger.info("VAT country not found!")
-            raise ValidationError(self._construct_constraint_msg())
-        if vat_number and not check_func(vat_country, vat_number):
-            _logger.info("VAT Number [%s] is not valid !" % vat_number)
-            return False
-        return True
-
-    @api.multi
-    def simple_vat_optional_check(self, country_code, vat_number):
-        """
-        Check the VAT number depending of the country.
-        http://sima-pc.com/nif.php
-        """
-        self.ensure_one()
-        res = self.simple_vat_check(country_code.lower(), vat_number)
-        data = {}
-        if res and self.vies_passed and not self.company_id.vat_check_vies:
-            # Can not be sure that this VAT is signed up in VIES
-            data['vies_passed'] = False
-        if res:
-            vat = country_code.upper() + vat_number
-            if self.vat != vat:
-                data['vat'] = vat
-        if data:
-            self.with_context(avoid_check_vat=True).write(data)
-        return res
-
-    @api.multi
-    def vies_vat_optional_check(self, country_code, vat_number):
-        self.ensure_one()
-        data = {}
-        res = False
-        try:
-            # Validate against VAT Information Exchange System (VIES)
-            # see also http://ec.europa.eu/taxation_customs/vies/
-            vat = country_code + vat_number
-            res = vatnumber.check_vies(vat)
-            if res and not self.vies_passed:
-                data['vies_passed'] = True
-        except Exception:
-            # See:
-            #   http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl
-            # Fault code may contain INVALID_INPUT, SERVICE_UNAVAILABLE,
-            # MS_UNAVAILABLE, TIMEOUT or SERVER_BUSY. There is no way we can
-            # validate the input with VIES if any of these arise, including
-            # the first one (it means invalid country code or empty
-            # VAT number), so we fall back to the simple check.
-            pass
-
-        if not res:
-            res = self.simple_vat_optional_check(country_code, vat_number)
-            if self.vies_passed:
-                data['vies_passed'] = False
-        if res:
-            vat = country_code.upper() + vat_number
-            if self.vat != vat:
-                data['vat'] = vat
-        if data:
-            self.with_context(avoid_check_vat=True).write(data)
-        return res
-
-    # Delete old api constraint defined in base_vat addon
-    @api.multi
-    def _validate_fields(self, field_names):
-        self._constraints = [x for x in self._constraints if 'vat' not in x[2]]
-        super(ResPartner, self)._validate_fields(field_names)
+            partner = partner.with_context(vat_partner=partner)
+            super(ResPartner, partner).check_vat()
