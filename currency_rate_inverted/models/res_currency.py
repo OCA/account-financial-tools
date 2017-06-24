2dbc3f7b470253caf030c01e89793552e16a5bc7
Jean-Charles Drubay <jc@komit-consulting.com>
2017-06-24 12:14:47 +0700

    Migrate currency_rate_inverted to v10 + add integration with currency_rate_update

---
+++ currency_rate_inverted/models/res_currency.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © 2015 Eficent
+# © 2015 Techrifiv Solutions Pte Ltd
+# © 2015 Statecraft Systems Pte Ltd
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models
+
+
+class ResCurrency(models.Model):
+    _inherit = 'res.currency'
+
+    rate_inverted = fields.Boolean(
+        'Inverted exchange rate',
+        company_dependent=True)
+
+    @api.model
+    def _get_conversion_rate(self, from_currency, to_currency):
+        res = super(ResCurrency, self)._get_conversion_rate(from_currency,
+                                                            to_currency)
+
+        if (
+            from_currency.rate_inverted and to_currency.rate_inverted or
+                not from_currency.rate_inverted and
+                not to_currency.rate_inverted):
+            return res
+        else:
+            return 1/res
---
+++ currency_rate_update_inverted/models/currency_rate_update.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+# © 2017 Komit
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+from datetime import datetime, time
+from dateutil.relativedelta import relativedelta
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import float_compare
+
+import odoo.addons.currency_rate_update as cru
+
+from odoo.addons.currency_rate_update.services.currency_getter_interface import CurrencyGetterType
+
+
+_logger = logging.getLogger(__name__)
+
+
+class CurrencyRateUpdateService(models.Model):
+
+    _inherit = "currency.rate.update.service"
+
+    @api.multi
+    def refresh_currency(self):
+        """Refresh the currencies rates !!for all companies now"""
+        rate_obj = self.env['res.currency.rate']
+        for srv in self:
+            _logger.info(
+                'Starting to refresh currencies with service %s (company: %s)',
+                srv.service, srv.company_id.name)
+            company = srv.company_id
+            # The multi company currency can be set or no so we handle
+            # The two case
+            if company.auto_currency_up:
+                main_currency = company.currency_id
+                # No need to test if main_currency exists, because it is a
+                # required field
+                if float_compare(
+                        main_currency.rate, 1,
+                        precision_rounding=main_currency.rounding):
+                    raise UserError(_(
+                        "In company '%s', the rate of the main currency (%s) "
+                        "must be 1.00 (current rate: %s).") % (
+                            company.name,
+                            main_currency.name,
+                            main_currency.rate))
+                note = srv.note or ''
+                try:
+                    # We initialize the class that will handle the request
+                    # and return a dict of rate
+                    getter = CurrencyGetterType.get(srv.service)
+                    curr_to_fetch = [x.name for x in srv.currency_to_update]
+                    res, log_info = getter.get_updated_currency(
+                        curr_to_fetch,
+                        main_currency.name,
+                        srv.max_delta_days
+                        )
+                    rate_name = \
+                        fields.Datetime.to_string(datetime.utcnow().replace(
+                            hour=0, minute=0, second=0, microsecond=0))
+                    for curr in srv.currency_to_update:
+                        if curr == main_currency:
+                            continue
+                        rates = rate_obj.search([
+                            ('currency_id', '=', curr.id),
+                            ('company_id', '=', company.id),
+                            ('name', '=', rate_name)])
+                        if not rates:
+                            rate = (curr.rate_inverted
+                                    and 1 / res[curr.name]
+                                    or res[curr.name])
+                            vals = {
+                                'currency_id': curr.id,
+                                'rate': rate,
+                                'name': rate_name,
+                                'company_id': company.id,
+                            }
+                            rate_obj.create(vals)
+                            _logger.info(
+                                'Updated currency %s via service %s '
+                                'in company %s',
+                                curr.name, srv.service, company.name)
+
+                    # Show the most recent note at the top
+                    msg = '%s \n%s currency updated. %s' % (
+                        log_info or '',
+                        fields.Datetime.to_string(datetime.today()),
+                        note
+                    )
+                    srv.write({'note': msg})
+                except Exception as exc:
+                    error_msg = '\n%s ERROR: %s %s' % (
+                        fields.Datetime.to_string(datetime.today()),
+                        repr(exc),
+                        note
+                    )
+                    _logger.error(repr(exc))
+                    srv.write({'note': error_msg})
+                if self._context.get('cron'):
+                    midnight = time(0, 0)
+                    next_run = (
+                        datetime.combine(
+                            fields.Date.from_string(srv.next_run),
+                            midnight) +
+                            cru.currency_rate_update._intervalTypes[
+                                str(srv.interval_type)]
+                            (srv.interval_number)).date()
+                    srv.next_run = next_run
+        return True
