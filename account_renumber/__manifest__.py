0ab8c219a11f57f7bdc50699c49d6ef83c3545df
David Vidal <chienandalu@gmail.com>
2017-07-10 17:37:55 +0200

    [MIG] account_renumber: Migration to 10.0

--- account_renumber/__manifest__.py
+++ account_renumber/__manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-# © 2009 Pexego Sistemas Informáticos. All Rights Reserved
-# © 2013 Pedro Manuel Baeza <pedro.baeza@tecnativa.com>
-# © 2013 Joaquin Gutierrez <http://www.gutierrezweb.es>
-# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2009 Pexego Sistemas Informáticos. All Rights Reserved
+# Copyright 2013 Pedro Manuel Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2013 Joaquin Gutierrez <http://www.gutierrezweb.es>
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': "Account Renumber Wizard",
-    'version': "9.0.1.0.0",
+    'version': "10.0.1.0.0",
     'author': "Pexego,Tecnativa,Odoo Community Association (OCA)",
     'website': "http://www.pexego.es",
     'category': "Accounting & Finance",
@@ -18,5 +19,5 @@
     "data": [
         'wizard/wizard_renumber_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
