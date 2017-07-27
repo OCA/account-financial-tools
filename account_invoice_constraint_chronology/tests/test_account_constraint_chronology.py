083cb622a98f7594d90b5aeb34e8373f658a3fc9
Toni <ahermosilla@visiion.es>
2017-07-27 22:14:47 +0200

    Test form v8

--- account_invoice_constraint_chronology/tests/test_account_constraint_chronology.py
+++ account_invoice_constraint_chronology/tests/test_account_constraint_chronology.py
@@ -32,111 +32,93 @@ from openerp import workflow
 from openerp import exceptions
 from datetime import datetime, timedelta
 from openerp.tools import DEFAULT_SERVER_DATE_FORMAT
-from openupgradelib.openupgrade import UserError
+
+
+def get_simple_product_id(self):
+    return self.env['product.product'].create({'name': 'product_test_01',
+                                               'lst_price': 2000.00,
+                                               })
+
+
+def get_journal_check(self, value):
+    sale_journal_id = self.ref('account.sales_journal')
+    sale_journal = self.env['account.journal'].browse([sale_journal_id])
+    journal = sale_journal.copy()
+    journal.check_chronology = value
+    return journal
+
+
+def get_simple_account_invoice_line_values(self, product_id):
+    return {'name': 'test',
+            'account_id': self.ref('account.a_sale'),
+            'price_unit': 2000.00,
+            'quantity': 1,
+            'product_id': product_id,
+            }
+
+
+def create_simple_invoice(self, journal_id, date):
+    partner_id = self.ref('base.res_partner_2')
+    product = get_simple_product_id(self)
+    return self.env['account.invoice']\
+        .create({'partner_id': partner_id,
+                 'account_id':
+                 self.ref('account.a_recv'),
+                 'journal_id':
+                 journal_id,
+                 'date_invoice': date,
+                 'invoice_line': [(0, 0, {'name': 'test',
+                                          'account_id':
+                                          self.ref('account.a_sale'),
+                                          'price_unit': 2000.00,
+                                          'quantity': 1,
+                                          'product_id': product.id,
+                                          }
+                                   )
+                                  ],
+                 })
 
 
 class TestAccountConstraintChronology(common.TransactionCase):
 
     def setUp(self):
         super(TestAccountConstraintChronology, self).setUp()
-
-        # Needed to create invoice
-
-        self.account_type1 = self.env['account.account.type'].\
-            create({'name': 'acc type test 1',
-                    'type': 'receivable',
-                    'include_initial_balance': True})
-        self.account_type2 = self.env['account.account.type']. \
-            create({'name': 'acc type test 2',
-                    'type': 'other',
-                    'include_initial_balance': True})
-        self.account_account = self.env['account.account'].\
-            create({'name': 'acc test',
-                    'code': 'X2020',
-                    'user_type_id': self.account_type1.id,
-                    'reconcile': True})
-        self.account_account_line = self.env['account.account']. \
-            create({'name': 'acc inv line test',
-                    'code': 'X2021',
-                    'user_type_id': self.account_type2.id,
-                    'reconcile': True})
-        self.sequence = self.env['ir.sequence'].create(
-            {'name': 'Journal Sale',
-             'prefix': 'SALE', 'padding': 6,
-             'company_id': self.env.ref("base.main_company").id})
-        self.account_journal_sale = self.env['account.journal']\
-            .create({'name': 'Sale journal',
-                     'code': 'SALE',
-                     'type': 'sale',
-                     'sequence_id': self.sequence.id})
-        self.product = self.env['product.product'].create(
-            {'name': 'product name'})
-        self.analytic_account = self.env['account.analytic.account'].\
-            create({'name': 'test account'})
-
-    def get_journal_check(self, value):
-        journal = self.account_journal_sale.copy()
-        journal.check_chronology = value
-        return journal
-
-    def create_simple_invoice(self, journal_id, date):
-        invoice = self.env['account.invoice'].create({
-            'partner_id': self.env.ref('base.res_partner_2').id,
-            'account_id': self.account_account.id,
-            'type': 'in_invoice',
-            'journal_id': journal_id,
-            'date_invoice': date,
-            'state': 'draft',
-        })
-
-        self.env['account.invoice.line'].create({
-            'product_id': self.product.id,
-            'quantity': 1.0,
-            'price_unit': 100.0,
-            'invoice_id': invoice.id,
-            'name': 'product that cost 100',
-            'account_id': self.account_account_line.id,
-            'account_analytic_id': self.analytic_account.id,
-        })
-        return invoice
+        self.context = self.registry("res.users").context_get(self.cr,
+                                                              self.uid)
 
     def test_invoice_draft(self):
-        journal = self.get_journal_check(True)
+        journal = get_journal_check(self, True)
         today = datetime.now()
         yesterday = today - timedelta(days=1)
         date = yesterday.strftime(DEFAULT_SERVER_DATE_FORMAT)
-        self.create_simple_invoice(journal.id, date)
+        create_simple_invoice(self, journal.id, date)
         date = today.strftime(DEFAULT_SERVER_DATE_FORMAT)
-        invoice_2 = self.create_simple_invoice(journal.id, date)
-        self.assertTrue((invoice_2.state == 'draft'),
-                        "Initial invoice state is not Draft")
-        with self.assertRaises(UserError):
-            invoice_2.action_invoice_open()
+        invoice_2 = create_simple_invoice(self, journal.id, date)
+        self.assertRaises(exceptions.Warning, workflow.trg_validate, self.uid,
+                          'account.invoice', invoice_2.id, 'invoice_open',
+                          self.cr)
 
     def test_invoice_validate(self):
-        journal = self.get_journal_check(True)
+        journal = get_journal_check(self, True)
         today = datetime.now()
         tomorrow = today + timedelta(days=1)
-        date_tomorrow = tomorrow.strftime(DEFAULT_SERVER_DATE_FORMAT)
-        invoice_1 = self.create_simple_invoice(journal.id, date_tomorrow)
-        self.assertTrue((invoice_1.state == 'draft'),
-                        "Initial invoice state is not Draft")
-        invoice_1.action_invoice_open()
+        date = tomorrow.strftime(DEFAULT_SERVER_DATE_FORMAT)
+        invoice = create_simple_invoice(self, journal.id, date)
+        workflow.trg_validate(self.uid, 'account.invoice', invoice.id,
+                              'invoice_open', self.cr)
         date = today.strftime(DEFAULT_SERVER_DATE_FORMAT)
-        invoice_2 = self.create_simple_invoice(journal.id, date)
-        self.assertTrue((invoice_2.state == 'draft'),
-                        "Initial invoice state is not Draft")
-        with self.assertRaises(UserError):
-            invoice_2.action_invoice_open()
+        invoice_2 = create_simple_invoice(self, journal.id, date)
+        self.assertRaises(exceptions.Warning, workflow.trg_validate, self.uid,
+                          'account.invoice', invoice_2.id, 'invoice_open',
+                          self.cr)
 
     def test_invoice_without_date(self):
-        journal = self.get_journal_check(True)
+        journal = get_journal_check(self, True)
         today = datetime.now()
         yesterday = today - timedelta(days=1)
         date = yesterday.strftime(DEFAULT_SERVER_DATE_FORMAT)
-        self.create_simple_invoice(journal.id, date)
-        invoice_2 = self.create_simple_invoice(journal.id, False)
-        self.assertTrue((invoice_2.state == 'draft'),
-                        "Initial invoice state is not Draft")
-        with self.assertRaises(UserError):
-            invoice_2.action_invoice_open()
+        create_simple_invoice(self, journal.id, date)
+        invoice_2 = create_simple_invoice(self, journal.id, False)
+        self.assertRaises(exceptions.Warning, workflow.trg_validate, self.uid,
+                          'account.invoice', invoice_2.id, 'invoice_open',
+                          self.cr)
