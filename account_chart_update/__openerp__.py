0f8de3bb9c2adea5a8f7d46ac29da6a98c90f81c
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-14 19:36:35 +0200

    [FIX+IMP] account_chart_update: Fix fiscal position mapping + translations + clean authors
    As this module has been written from scratch with a totally different technique, I clean the list and update copyrights.

--- account_chart_update/__openerp__.py
+++ account_chart_update/__openerp__.py
@@ -1,31 +1,21 @@
 # -*- coding: utf-8 -*-
-# © 2010 Zikzakmedia S.L. (http://www.zikzakmedia.com)
-# © 2010 Pexego Sistemas Informáticos S.L.(http://www.pexego.es)
-# © 2013 Joaquin Gutierrez (http://www.gutierrezweb.es)
-# © 2015 Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
-# © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
-# © 2016 Jacques-Etienne Baudoux <je@bcim.be>
+# Copyright 2015-2017 Pedro Manuel Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
+# Copyright 2016 Jacques-Etienne Baudoux <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': "Detect changes and update the Account Chart from a template",
     "summary": "Wizard to update a company's account chart from a template",
     'version': "9.0.1.0.0",
-    'author': "Zikzakmedia SL, "
-              "Pexego, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
-              "ACSONE A/V, "
-              "Tecnativa, "
+    'author': "Tecnativa, "
               "BCIM,"
               "Odoo Community Association (OCA)",
     'website': "http://odoo-community.org",
     'depends': ["account"],
     'category': "Accounting & Finance",
     'contributors': [
-        'Joaquín Gutierrez',
         'Pedro M. Baeza',
-        'invitu',
-        'Stéphane Bidoul',
         'Jairo Llopis',
         'Jacques-Etienne Baudoux',
     ],
