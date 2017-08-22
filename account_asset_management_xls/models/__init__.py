74a98ee09eae83759e7f58936d6bf35a8ca41175
luc-demeyer <luc.demeyer@noviat.com>
2017-08-22 19:16:43 +0200

    fix import warning message

--- account_asset_management_xls/models/__init__.py
+++ account_asset_management_xls/models/__init__.py
@@ -1,9 +1,2 @@
 # -*- coding: utf-8 -*-
-try:
-    from . import account_asset
-    from . import wizard
-    from . import report
-except ImportError:
-    import logging
-    logging.getLogger(__name__).warn(
-        "report_xls not available in addons path")
+from . import account_asset
