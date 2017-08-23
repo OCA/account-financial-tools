b8d010b03f260bc7c71b06011cde49ed3713b04b
Nhomar Hernández [Vauxoo] <nhomar@vauxoo.com>
2017-08-22 20:48:14 -0500

    [MIG] account_move_template: to 10.0 (#511)
    * [MIG] account_move_template: to 10.0
    
    All working, removed feature of transitory account, I think that feature
    is redundant because it can be solved with python code lines and/or a
    proper server action per client, beside it was not properly documented.
    
    - All code refactored more new api oriented.
    - Tested with enterprise and all views fixed.
    - Removed all licences on files and moved as usual to where they belong.
    
    * [FIX] removed tax from view.
    
    The taxes should be triggered from the account itself and not wired into
    the template.
    
    * [FIX] account_move_template: Activities in just for saas-x comming on
    v100, for now I left it commented to simply uncomment it on v11
    
    * [FIX] account_move_template: Cleanup translations, not useful after
    refactor
    
    * [REF] account_move_template: Removing all reference to taxes.
    
    * [IMP] account_move_template: Translating
    
    * [FIX] account_move_template: Fixing lints.

--- account_move_template/models/__init__.py
+++ account_move_template/models/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2015-2017 See manifest
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from . import models
-from . import wizard
+from . import account_document_template
+from . import account_move_template
