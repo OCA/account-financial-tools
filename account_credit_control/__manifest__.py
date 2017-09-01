bfcb82782f1ce9ca0535a3b4e88dd614a104fb88
Benjamin Willig (ACSONE) <benjamin.willig@acsone.eu>
2017-09-01 16:38:32 +0200

    [ADD][account_credit_control] Add an additional text after the details of the credit control summary report (#534)

--- account_credit_control/__manifest__.py
+++ account_credit_control/__manifest__.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Okia SPRL (https://okia.be)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Account Credit Control',
- 'version': '10.0.1.0.0',
+ 'version': '10.0.1.1.0',
  'author': "Camptocamp,Odoo Community Association (OCA),Okia",
  'maintainer': 'Camptocamp',
  'category': 'Finance',
