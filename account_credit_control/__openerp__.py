e09dc8755cd7ef5c7a08dcf9df0c4f6efb156376
cubells <vicent@vcubells.net>
2017-09-07 22:08:57 +0200

    Revert security file permissions

--- account_credit_control/__openerp__.py
+++ account_credit_control/__openerp__.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Account Credit Control',
-    'version': '9.0.0.1.0',
+    'version': '9.0.1.0.1',
     'author': "Camptocamp, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
