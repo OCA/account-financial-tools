593e0d998e43e42300624c52a5e08d40e4da637b
YurGur <yurigural@inbox.ru>
2017-11-10 15:43:33 -0600

    Fix: Bank of Canada
    Bank of Canada
    Updated daily

--- currency_rate_update/services/update_service_CA_BOC.py
+++ currency_rate_update/services/update_service_CA_BOC.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
 # © 2009 Camptocamp
 # © 2014 Daniel Dico
+# © 2017 Yuriy Gural
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from .currency_getter_interface import CurrencyGetterInterface
 
 from odoo import _
 from odoo.exceptions import except_orm
+
+import feedparser
+import pytz
+from dateutil import parser
+
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -23,74 +29,87 @@ class CA_BOCGetter(CurrencyGetterInterface):
     #  (Thailand, Malaysia, Mexico...)
 
     code = 'CA_BOC'
-    name = 'Bank of Canada - noon rates'
+    name = 'Bank of Canada'
 
     supported_currency_array = [
-        "AED", "ANG", "ARS", "AUD", "BOC", "BRL", "BSD", "CHF", "CLP", "CNY",
-        "COP", "CZK", "DKK", "EUR", "FJD", "GBP", "GHS", "GTQ", "HKD", "HNL",
-        "HRK", "HUF", "IDR", "ILS", "INR", "ISK", "JMD", "JPY", "KRW", "LKR",
-        "MAD", "MMK", "MXN", "MYR", "NOK", "NZD", "PAB", "PEN", "PHP", "PKR",
-        "PLN", "RON", "RSD", "RUB", "SEK", "SGD", "THB", "TND", "TRY", "TTD",
-        "TWD", "USD", "VEF", "VND", "XAF", "XCD", "XPF", "ZAR"]
+        "AUD", "BRL", "CNY", "EUR", "HKD", "INR", "IDR", "JPY", "RUB", "SAR",
+        "SGD", "MYR", "MXN", "NZD", "NOK", "PEN", "ZAR", "KRW", "SEK", "CHF",
+        "TWD", "THB", "TRY", "GBP", "USD", "VND", "CAD"]
+
+    def rate_retrieve(self, curr):
+        res = {}
+
+        url = ('https://www.bankofcanada.ca/'
+               'valet/fx_rss/FX%sCAD') % (curr.upper())
+
+        dom = feedparser.parse(url)
+
+        # check if BOC service is running
+        if dom.bozo and dom.status != 404:
+            _logger.error("Bank of Canada - service is down - try again\
+                later...")
+
+        # check if BOC sent a valid response for this currency
+        if dom.status != 200:
+            _logger.error("Exchange data for %s is not reported by Bank\
+                of Canada." % curr)
+            raise except_orm(_('Error !'), _('Exchange data for %s is not '
+                                             'reported by Bank of Canada.'
+                                             % str(curr)))
+
+        _logger.debug("BOC sent a valid RSS file for: " + curr)
+
+        if (dom.entries[0].cb_basecurrency == 'CAD') and \
+                (dom.entries[0].cb_targetcurrency == curr):
+            res['rate_currency'] = float(
+                dom.entries[0].cb_exchangerate.split('\n', 1)[0]
+            )
+            res['rate_date_datetime'] = parser.parse(dom.entries[0].updated)\
+                .astimezone(pytz.utc).replace(tzinfo=None)
+        else:
+            _logger.error(
+                "Exchange data format error for Bank of Canada -"
+                "%s. Please check provider data format "
+                "and/or source code." % curr)
+            raise except_orm(_('Error !'),
+                             _('Exchange data format error for '
+                               'Bank of Canada - %s !' % str(curr)))
+        return res
 
     def get_updated_currency(self, currency_array, main_currency,
                              max_delta_days):
         """implementation of abstract method of Curreny_getter_interface"""
 
-        # as of Jan 2014 BOC is publishing noon rates for about 60 currencies
-        url = ('http://www.bankofcanada.ca/stats/assets/'
-               'rates_rss/noon/en_%s.xml')
+        url = ('https://www.bankofcanada.ca/'
+               'valet/fx_rss/FX%sCAD')
         # closing rates are available as well (please note there are only 12
         # currencies reported):
         # http://www.bankofcanada.ca/stats/assets/rates_rss/closing/en_%s.xml
+        # new url:
+        # https://www.bankofcanada.ca/valet/fx_rss/FX%sCAD
 
         # We do not want to update the main currency
         if main_currency in currency_array:
             currency_array.remove(main_currency)
+        _logger.debug("BOC currency rate service : connecting...")
 
-        import feedparser
-        import pytz
-        from dateutil import parser
+        self.validate_cur(main_currency)
+        if main_currency != 'CAD':
+            main_curr_data = self.rate_retrieve(main_currency)
+            self.check_rate_date(main_curr_data['rate_date_datetime'], max_delta_days)
 
         for curr in currency_array:
-
-            _logger.debug("BOC currency rate service : connecting...")
-            dom = feedparser.parse(url % curr)
-
             self.validate_cur(curr)
-
-            # check if BOC service is running
-            if dom.bozo and dom.status != 404:
-                _logger.error("Bank of Canada - service is down - try again\
-                    later...")
-
-            # check if BOC sent a valid response for this currency
-            if dom.status != 200:
-                _logger.error("Exchange data for %s is not reported by Bank\
-                    of Canada." % curr)
-                raise except_orm(_('Error !'), _('Exchange data for %s is not '
-                                                 'reported by Bank of Canada.'
-                                                 % str(curr)))
-
-            _logger.debug("BOC sent a valid RSS file for: " + curr)
-
-            # check for valid exchange data
-            if (dom.entries[0].cb_basecurrency == main_currency) and \
-                    (dom.entries[0].cb_targetcurrency == curr):
-                rate = dom.entries[0].cb_exchangerate.split('\n', 1)[0]
-                rate_date_datetime = parser.parse(dom.entries[0].updated)\
-                    .astimezone(pytz.utc).replace(tzinfo=None)
-                self.check_rate_date(rate_date_datetime, max_delta_days)
-                self.updated_currency[curr] = rate
-                _logger.debug("BOC Rate retrieved : %s = %s %s" %
-                              (main_currency, rate, curr))
+            if curr == 'CAD':
+                rate = 1 / main_curr_data['rate_currency']
             else:
-                _logger.error(
-                    "Exchange data format error for Bank of Canada -"
-                    "%s. Please check provider data format "
-                    "and/or source code." % curr)
-                raise except_orm(_('Error !'),
-                                 _('Exchange data format error for '
-                                   'Bank of Canada - %s !' % str(curr)))
+                curr_data = self.rate_retrieve(curr)
+                self.check_rate_date(curr_data['rate_date_datetime'], max_delta_days)
+                if main_currency == 'CAD':
+                    rate = curr_data['rate_currency']
+                else:
+                    rate = (curr_data['rate_currency'] /
+                            main_curr_data['rate_currency'])
+            self.updated_currency[curr] = rate
 
         return self.updated_currency, self.log_info
