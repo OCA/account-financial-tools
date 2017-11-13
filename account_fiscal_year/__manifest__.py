01acd3565b950d707dd5ba0e2a2f35aafde9564d
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2017-11-13 11:55:42 +0100

    [MIG] account_fiscal_year: Migration to 11.

--- account_fiscal_year/__manifest__.py
+++ account_fiscal_year/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Author: Damien Crier
 # Copyright 2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Fiscal Year',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting',
     'author': 'Camptocamp SA,'
               'Odoo Community Association (OCA)',
