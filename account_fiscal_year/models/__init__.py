8b9b3e2658bf11cd170fe88a1fd3531c15f3bc43
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2017-11-13 11:55:42 +0100

    [MIG] account_fiscal_year: Migration to 11.

--- account_fiscal_year/models/__init__.py
+++ account_fiscal_year/models/__init__.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# Author: Damien Crier
-# Copyright 2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import date_range_type
 from . import res_company
