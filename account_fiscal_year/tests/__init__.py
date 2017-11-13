9272735f6a2ff68769194e22d5fe76c20803daac
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2017-11-13 11:55:42 +0100

    [MIG] account_fiscal_year: Migration to 11.

--- account_fiscal_year/tests/__init__.py
+++ account_fiscal_year/tests/__init__.py
@@ -1,6 +1,2 @@
-# -*- coding: utf-8 -*-
-# Author: Damien Crier
-# Copyright 2016 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_account_fiscal_year
