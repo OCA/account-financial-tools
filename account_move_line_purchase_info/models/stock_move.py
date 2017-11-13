982fe446f5cf74217aecb362fdbbefdc6bcadc2d
lreficent <lois.rilo@eficent.com>
2017-11-13 12:40:59 +0100

    [FIX] odoo imports

--- account_move_line_purchase_info/models/stock_move.py
+++ account_move_line_purchase_info/models/stock_move.py
@@ -2,7 +2,7 @@
 # © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
+from odoo import api, models
 
 
 class StockMove(models.Model):
