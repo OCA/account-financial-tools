5e742dd2b2a4a9a4ff88754a3a2404b8ee472485
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-11-25 13:25:11 +0100

    [FIX] account_asset_disposal: Restore full functionality

---
+++ account_asset_disposal/models/account_asset_category.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Tecnativa - Antonio Espinosa
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class AccountAssetCategory(models.Model):
+    _inherit = "account.asset.category"
+
+    account_loss_id = fields.Many2one(
+        comodel_name="account.account", string="Loss Account",
+        oldname='loss_account_id',
+    )
