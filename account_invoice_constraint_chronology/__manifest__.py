7a7d2b7a0df2ba84fdeb1242e730c640bd0d2af8
Francesco Apruzzese <cescoap@gmail.com>
2017-12-19 17:28:44 +0100

    [MIG] account_invoice_constraint_chronology: Migration to 11.0

--- account_invoice_constraint_chronology/__manifest__.py
+++ account_invoice_constraint_chronology/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice Constraint Chronology",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "maintainer": "ACSONE SA/NV",
     "website": "http://www.acsone.eu",
