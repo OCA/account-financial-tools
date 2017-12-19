f8c3049657545e58752e014f7944c36a89886ce9
Francesco Apruzzese <cescoap@gmail.com>
2017-12-19 17:28:44 +0100

    [MIG] account_invoice_constraint_chronology: Migration to 11.0

--- account_invoice_constraint_chronology/tests/test_account_constraint_chronology.py
+++ account_invoice_constraint_chronology/tests/test_account_constraint_chronology.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from datetime import datetime, timedelta
 
