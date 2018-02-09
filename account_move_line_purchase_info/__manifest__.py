5888ca4ea9cc6b4847c6f4b6da4ce2930a508915
Lois Rilo <lois.rilo@eficent.com>
2018-02-09 14:30:59 +0100

    [11.0][MIG] account_move_line_purchase_info

--- account_move_line_purchase_info/__manifest__.py
+++ account_move_line_purchase_info/__manifest__.py
@@ -1,16 +1,16 @@
-# -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#           (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account Move Line Purchase Info",
     "summary": "Introduces the purchase order line to the journal items",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "http://www.github.com/OCA/account-financial-tools",
     "category": "Generic",
-    "depends": ["account_accountant", "purchase"],
+    "depends": ["account", "purchase"],
     "license": "AGPL-3",
     "data": [
         "security/account_security.xml",
