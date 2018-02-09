814886f174792288d9bedf35646d87b43b2a91cc
Lois Rilo <lois.rilo@eficent.com>
2018-02-09 14:30:59 +0100

    [11.0][MIG] account_move_line_purchase_info

--- account_move_line_purchase_info/models/account_move.py
+++ account_move_line_purchase_info/models/account_move.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#           (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -9,6 +9,8 @@ class AccountMoveLine(models.Model):
 
     _inherit = 'account.move.line'
 
-    purchase_line_id = fields.Many2one('purchase.order.line',
-                                       'Purchase Order Line',
-                                       ondelete='set null', index=True)
+    purchase_line_id = fields.Many2one(
+        comodel_name='purchase.order.line',
+        string='Purchase Order Line',
+        ondelete='set null', index=True,
+    )
