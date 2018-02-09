161435fd821102551095ad5a8172ce2d1d3e7c8e
Lois Rilo <lois.rilo@eficent.com>
2018-02-09 14:30:59 +0100

    [11.0][MIG] account_move_line_purchase_info

--- account_move_line_purchase_info/models/account_invoice.py
+++ account_move_line_purchase_info/models/account_invoice.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#           (www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
