ed7253592e3ee5c46805ead46d7de043611d1afa
Raf Ven <raf.ven@dynapps.be>
2018-03-05 20:31:53 +0100

    account_move_template: Migration to 11.0

--- account_move_template/__manifest__.py
+++ account_move_template/__manifest__.py
@@ -1,17 +1,20 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2017 See manifest
+# Copyright 2018 Raf Ven <raf.ven@dynapps.be>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': "Account Move Template",
-    'version': '10.0.2.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Generic Modules/Accounting',
     'summary': "Templates for recurring Journal Entries",
     'author': "Agile Business Group,Odoo Community Association (OCA), Aurium "
               "Technologies,Vauxoo",
     'website': 'https://github.com/OCA/account-financial-tools',
     'license': 'AGPL-3',
-    'depends': ['account_accountant', 'analytic'],
+    'depends': [
+        'account',
+        'analytic',
+    ],
     'data': [
         'security/ir.model.access.csv',
         'view/move_template.xml',
