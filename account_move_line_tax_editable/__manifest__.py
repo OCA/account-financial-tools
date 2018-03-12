04b683f4959de767a54aeec566ee446b29108f57
Fekete Mihai <mihai.fekete@forbiom.eu>
2018-03-12 13:53:49 +0200

    Migrate account_move_line_tax_ediatble to 11.0
    Update __manifest__.py

--- account_move_line_tax_editable/__manifest__.py
+++ account_move_line_tax_editable/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,7 +5,7 @@
     'name': 'Account Move Line Tax Editable',
     'summary': """
         Allows to edit taxes on non-posted account move lines""",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://www.acsone.eu',
@@ -16,7 +15,5 @@
     'data': [
         'views/account_move.xml',
         'views/account_move_line.xml',
-    ],
-    'demo': [
     ],
 }
