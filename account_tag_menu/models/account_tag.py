bb14ca1ecfd7afb7f67deae2b8bf4c358a5b6cb6
Fekete Mihai <mihai.fekete@forbiom.eu>
2018-03-15 08:05:15 +0200

    Update copyright year, add demo data for taxes, for accounts there are already in Minimal Chart of Accounts.

--- account_tag_menu/models/account_tag.py
+++ account_tag_menu/models/account_tag.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017 FOREST AND BIOMASS ROMANIA SA
+# Copyright 2018 FOREST AND BIOMASS ROMANIA SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
