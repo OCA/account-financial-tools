e76314dff4082ac533eba5daf24f08d725bb659e
Fekete Mihai <mihai.fekete@forbiom.eu>
2018-03-17 07:18:58 +0200

    Migrate account_fiscal_month.
    Update tests for unlink.
    
    Update tests.

--- account_fiscal_month/tests/test_account_fiscal_month.py
+++ account_fiscal_month/tests/test_account_fiscal_month.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -12,10 +11,23 @@ class TestAccountFiscalMonth(TransactionCase):
     def setUp(self):
         super(TestAccountFiscalMonth, self).setUp()
         self.DateRangeObj = self.env['date.range']
+        self.DateRangeType = self.env['date.range.type']
 
         self.company = self.env.ref('base.main_company')
+
+        self.date_range_type = self.DateRangeType.create(
+            {'name': 'Other Type',
+             'allow_overlap': False})
         self.date_range_type_month = self.env.ref(
             'account_fiscal_month.date_range_fiscal_month')
+
+        self.date_range_1 = self.DateRangeObj.create({
+            'name': "Other",
+            'date_start': '2017-01-01',
+            'date_end': '2017-01-31',
+            'type_id': self.date_range_type.id,
+            'company_id': self.company.id,
+        })
 
         self.date_range_january_2017 = self.DateRangeObj.create({
             'name': "January 2017",
@@ -31,6 +43,11 @@ class TestAccountFiscalMonth(TransactionCase):
             'type_id': self.date_range_type_month.id,
             'company_id': False,
         })
+
+    def test_00_delete_type(self):
+        with self.assertRaises(Exception):
+            with self.env.cr.savepoint():
+                self.date_range_type.unlink()
 
     def test_01_delete_type_fiscal_month(self):
         with self.assertRaises(UserError):
