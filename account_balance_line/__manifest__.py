45c3d808dfdfe7be9b2b06ef26d192f367956fd2
Robert Rübner <rruebner@bloopark.de>
2018-04-05 08:43:37 +0200

    [MIG] account_balance_line: Migration to 11.0

--- account_balance_line/__manifest__.py
+++ account_balance_line/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2010-2014 Camptocamp - Vincent Renaville
 # Copyright 2016 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Balance on journal items',
     'summary': 'Display balance totals in move line view',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': 'Accounting',
     'website': 'http://www.tecnativa.com,',
     'author': "Camptocamp,"
