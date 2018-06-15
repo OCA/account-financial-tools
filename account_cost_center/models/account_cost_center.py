eda8f8045c874b94d947f1e55476c5450e5af22b
Andrea <a.stirpe@onestein.nl>
2018-06-15 13:59:46 +0200

    [10.0][MIG] Porting account_cost_center to V10

--- account_cost_center/models/account_cost_center.py
+++ account_cost_center/models/account_cost_center.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2015-2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountCostCenter(models.Model):
@@ -12,8 +12,7 @@ class AccountCostCenter(models.Model):
     name = fields.Char(string='Title', required=True)
     code = fields.Char(required=True)
     company_id = fields.Many2one(
-        comodel_name='res.company',
+        'res.company',
         string='Company',
-        required=True,
         default=lambda self: self.env.user.company_id
     )
