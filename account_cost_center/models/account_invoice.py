b893a869f1a81a7ff813b86f1442ab22185c870f
Andrea <a.stirpe@onestein.nl>
2018-06-15 13:59:46 +0200

    [10.0][MIG] Porting account_cost_center to V10

--- account_cost_center/models/account_invoice.py
+++ account_cost_center/models/account_invoice.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2015-2018 Onestein (<http://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -15,8 +15,20 @@ class AccountInvoice(models.Model):
     )
 
     @api.model
-    def line_get_convert(self, line, part, date):
-        res = super(AccountInvoice, self).line_get_convert(line, part, date)
+    def line_get_convert(self, line, part):
+        res = super(AccountInvoice, self).line_get_convert(line, part)
         if line.get('cost_center_id'):
             res['cost_center_id'] = line['cost_center_id']
+        return res
+
+    @api.model
+    def invoice_line_move_line_get(self):
+        res = super(AccountInvoice, self).invoice_line_move_line_get()
+
+        for dict_data in res:
+            invl_id = dict_data.get('invl_id')
+            line = self.env['account.invoice.line'].browse(invl_id)
+            if line.cost_center_id:
+                dict_data['cost_center_id'] = line.cost_center_id.id
+
         return res
