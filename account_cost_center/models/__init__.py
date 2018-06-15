66ffd0ef020eedc3454f2e9fa8851880be0e44c6
Andrea <a.stirpe@onestein.nl>
2018-06-15 13:59:46 +0200

    [10.0][MIG] Porting account_cost_center to V10

--- account_cost_center/models/__init__.py
+++ account_cost_center/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2017 Onestein (<http://www.onestein.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import account_move_line
 from . import account_invoice
