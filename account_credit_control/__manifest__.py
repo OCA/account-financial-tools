8c329f57b0af8399eb213f1931ef8a6fbdde42e8
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-07-14 10:49:56 +0200

    [FIX] account_credit_control: Our co-authoring is missing even the contributions:
    https://github.com/OCA/account-financial-tools/commit/f9bc46ab57eb873a1709b38aa13b93988c09126e#diff-d2d83592881bd999fbe1e6f32a0a244a
    https://github.com/OCA/account-financial-tools/commit/6c684f53ea1f0d09c06cea2d7df93f6930314298#diff-d2d83592881bd999fbe1e6f32a0a244a
    https://github.com/OCA/account-financial-tools/commit/3c7475c3b5e6dec1f7b848acc5d8f75e3df3ba80#diff-d2d83592881bd999fbe1e6f32a0a244a
    https://github.com/OCA/account-financial-tools/commit/09260a00ca2cdbf756df518aa53bd3b7211bca3b#diff-d2d83592881bd999fbe1e6f32a0a244a
    https://github.com/OCA/account-financial-tools/commit/3029a6347c118ab1779dfa97903500cdc678d9fd#diff-d2d83592881bd999fbe1e6f32a0a244a
    https://github.com/OCA/account-financial-tools/commit/ad44b408071cceebea68da81398bf57856f156c1#diff-d2d83592881bd999fbe1e6f32a0a244a
    https://github.com/OCA/account-financial-tools/commit/c45538ed1774f6b5c484828efae7caf177cd0383#diff-d2d83592881bd999fbe1e6f32a0a244a
    https://github.com/OCA/account-financial-tools/commit/b125a794aec7d740e81a90c94cf31a3b230407fb#diff-d2d83592881bd999fbe1e6f32a0a244a

--- account_credit_control/__manifest__.py
+++ account_credit_control/__manifest__.py
@@ -4,7 +4,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {'name': 'Account Credit Control',
  'version': '11.0.1.0.1',
- 'author': "Camptocamp,Odoo Community Association (OCA),Okia,Access Bookings",
+ 'author': "Camptocamp,"
+           "Odoo Community Association (OCA),"
+           "Okia,"
+           "Access Bookings,"
+           "Tecnativa",
  'maintainer': 'Camptocamp',
  'category': 'Finance',
  'depends': [
