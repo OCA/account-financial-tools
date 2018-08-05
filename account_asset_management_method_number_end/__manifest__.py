b1e944ba14b9469d980a734d66b7c8309e981cb8
Luc De Meyer <luc.demeyer@noviat.com>
2018-08-05 20:33:56 +0200

    [10.0][MIG]account_asset_management suite refactoring + 10.0 port

---
+++ account_asset_management_method_number_end/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2018 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Financial asset management - method Number & End',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': "Noviat,Odoo Community Association (OCA)",
+    'website': 'http://www.noviat.com',
+    'category': 'Accounting & Finance',
+    'depends': [
+        'account_asset_management',
+    ],
+    'installable': True,
+}
---
+++ account_asset_management_method_number_end/models/account_asset_profile.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2009-2018 Noviat.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models, _
+
+
+class AccountAssetProfile(models.Model):
+    _inherit = 'account.asset.profile'
+
+    method_number = fields.Integer(
+        string='Number',
+        help="The meaning of this parameter depends on the Time Method.\n"
+             "  * Number of Years: Specify the number of years "
+             "for the depreciation.\n"
+             "  * Number of Depreciations: Fix the number of "
+             "depreciation lines and the time between 2 depreciations.\n"
+             "  * Ending Date: Choose the time between 2 depreciations "
+             "and the date the depreciations won't go beyond.")
+
+    method_time = fields.Selection(
+        help="Choose the method to use to compute the dates and "
+             "number of depreciation lines.\n"
+             "  * Number of Years: Specify the number of years "
+             "for the depreciation.\n"
+             "  * Number of Depreciations: Fix the number of "
+             "depreciation lines and the time between 2 depreciations.\n"
+             "  * Ending Date: Choose the time between 2 depreciations "
+             "and the date the depreciations won't go beyond.")
+
+    @api.model
+    def _selection_method_time(self):
+        res = super(AccountAssetProfile, self)._selection_method_time()
+        res += [
+            ('number', _('Number of Depreciations')),
+            ('end', _('Ending Date'))]
+        return res
