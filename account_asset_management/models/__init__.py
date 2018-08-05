8bdcf3f678445e38a34a7c9ec4703e5a442b0e81
Luc De Meyer <luc.demeyer@noviat.com>
2018-08-05 20:33:56 +0200

    [10.0][MIG]account_asset_management suite refactoring + 10.0 port

---
+++ account_asset_management/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from . import account_account
+from . import account_asset
+from . import account_asset_profile
+from . import account_asset_line
+from . import account_asset_recompute_trigger
+from . import account_invoice
+from . import account_move
+from . import date_range
+from . import res_config
