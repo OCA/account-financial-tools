c5d7e5e0268125c804324d96ce25a98ccdf6cd8d
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-09-18 14:41:19 +0200

    [FIX+IMP] account_chart_update: Major refactoring
    * Compare all fields from template
    
      Not all fields were properly compared with previous code. This means also
      to exclude some of them, but now we have a full and extensible system.
    
    * Optimize cached method
    * Generate accounts and fiscal positions using Odoo methods (tax already did)
    * README by fragments
    * Add tests up to full coverage

--- account_chart_update/__manifest__.py
+++ account_chart_update/__manifest__.py
@@ -1,28 +1,22 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2017 Pedro Manuel Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # Copyright 2016 Jacques-Etienne Baudoux <je@bcim.be>
 # Copyright 2016 Sylvain Van Hoof <sylvain@okia.be>
+# Copyright 2015-2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': "Detect changes and update the Account Chart from a template",
     "summary": "Wizard to update a company's account chart from a template",
-    'version': "10.0.1.0.2",
+    'version': "10.0.2.0.0",
     'author': "Tecnativa, "
               "BCIM, "
               "Okia, "
               "Odoo Community Association (OCA)",
     'website': "http://odoo-community.org",
     'depends': ["account"],
-    'category': "Accounting & Finance",
-    'contributors': [
-        'Pedro M. Baeza',
-        'Jairo Llopis',
-        'Jacques-Etienne Baudoux',
-        'Sylvain Van Hoof'
-        'Nacho Muñoz',
-    ],
+    'development_status': 'Mature',
+    'category': "Accounting",
     'license': "AGPL-3",
     "data": [
         'wizard/wizard_chart_update_view.xml',
---
+++ account_chart_update/readme/ROADMAP.rst
@@ -0,0 +1,5 @@
+* Generate and update account reconcile models.
+* Generate XML-ID for fiscal position tax and account mapping lines.
+* Allow to select independently operations to perform (create, update,
+  deactivate).
+* Detect fiscal positions to deactivate?
