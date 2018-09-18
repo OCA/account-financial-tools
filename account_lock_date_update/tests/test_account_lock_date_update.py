aeabc29f3cb1f74ade6734d543d549356968063b
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-09-18 11:38:45 +0200

    [FIX] account_lock_date_update: Fix tests

--- account_lock_date_update/tests/test_account_lock_date_update.py
+++ account_lock_date_update/tests/test_account_lock_date_update.py
@@ -10,11 +10,12 @@ class TestAccountLockDateUpdate(TransactionCase):
 
     def setUp(self):
         super(TestAccountLockDateUpdate, self).setUp()
-        self.UpdateLockDateUpdateObj = self.env['account.update.lock_date']
-
         self.company = self.env.ref('base.main_company')
         self.demo_user = self.env.ref('base.user_demo')
         self.adviser_group = self.env.ref('account.group_account_manager')
+        self.UpdateLockDateUpdateObj = self.env[
+            'account.update.lock_date'
+        ].sudo(self.demo_user)
 
     def create_account_lock_date_update(self):
         return self.UpdateLockDateUpdateObj.create({
@@ -27,23 +28,21 @@ class TestAccountLockDateUpdate(TransactionCase):
             'period_lock_date': '2000-01-01',
             'fiscalyear_lock_date': '2000-01-01',
         })
-
         self.demo_user.write({
             'groups_id': [(3, self.adviser_group.id)],
         })
-
         with self.assertRaises(UserError):
             wizard.sudo(self.demo_user.id).execute()
 
     def test_02_update_with_access(self):
         wizard = self.create_account_lock_date_update()
         wizard.write({
-            'period_lock_date': '2000-01-01',
+            'period_lock_date': '2000-02-01',
             'fiscalyear_lock_date': '2000-01-01',
         })
-
         self.demo_user.write({
             'groups_id': [(4, self.adviser_group.id)],
         })
-
         wizard.sudo(self.demo_user.id).execute()
+        self.assertEqual(self.company.period_lock_date, '2000-02-01')
+        self.assertEqual(self.company.fiscalyear_lock_date, '2000-01-01')
