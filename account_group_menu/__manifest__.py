ac0f9fc72da8be340c0c75fa521464f151988895
Raf Ven <raf.ven@dynapps.be>
2018-10-02 16:56:34 +0200

    [MIG] account_group_menu: Migration to 12.0

--- account_group_menu/__manifest__.py
+++ account_group_menu/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Account Group Menu',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Accounting',
     'license': 'AGPL-3',
     'summary': "Adds menu entries for Account Group and Tax Group",
