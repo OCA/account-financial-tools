1d3daa2e160e91debe3f909befafbe6f75b26a57
George Daramouskas <gdaramouskas@therp.nl>
2018-10-03 19:56:18 +0200

    [FIX] Fix lints, port to new API

---
+++ account_asset_management_xls/models/account_asset.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Noviat nv/sa (www.noviat.com). All rights reserved.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, models
+
+
+class AccountAssetAsset(models.Model):
+    _inherit = 'account.asset.asset'
+
+    @api.model
+    def _xls_acquisition_fields(self):
+        """
+        Update list in custom module to add/drop columns or change order
+        """
+        return [
+            'account', 'name', 'code', 'date_start', 'asset_value',
+            'salvage_value',
+        ]
+
+    @api.model
+    def _xls_active_fields(self):
+        """
+        Update list in custom module to add/drop columns or change order
+        """
+        return [
+            'account', 'name', 'code', 'date_start',
+            'asset_value', 'salvage_value',
+            'fy_start_value', 'fy_depr', 'fy_end_value',
+            'fy_end_depr',
+            'method', 'method_number', 'prorata',
+        ]
+
+    @api.model
+    def _xls_removal_fields(self):
+        """
+        Update list in custom module to add/drop columns or change order
+        """
+        return [
+            'account', 'name', 'code', 'date_remove', 'asset_value',
+            'salvage_value',
+        ]
+
+    @api.model
+    def _xls_acquisition_template(self):
+        """
+        Template updates
+
+        """
+        return {}
+
+    @api.model
+    def _xls_active_template(self):
+        """
+        Template updates
+
+        """
+        return {}
+
+    @api.model
+    def _xls_removal_template(self):
+        """
+        Template updates
+
+        """
+        return {}
--- account_asset_management_xls/wizard/account_asset_report_wizard.py
+++ account_asset_management_xls/wizard/account_asset_report_wizard.py
@@ -1,82 +1,70 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#
-#    Copyright (c) 2014 Noviat nv/sa (www.noviat.com). All rights reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from openerp.osv import orm, fields
-from openerp.tools.translate import _
+# -*- coding: utf-8 -*-
+# Copyright 2014 Noviat nv/sa (www.noviat.com). All rights reserved.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openerp import api, fields, models, _
+from openerp.exceptions import ValidationError
 
 
-class wiz_account_asset_report(orm.TransientModel):
+class WizAccountAssetReport(models.TransientModel):
 
     _name = 'wiz.account.asset.report'
     _description = 'Financial Assets report'
 
-    _columns = {
-        'fiscalyear_id': fields.many2one(
-            'account.fiscalyear', 'Fiscal Year', required=True),
-        'parent_asset_id': fields.many2one(
-            'account.asset.asset', 'Asset Filter',
-            domain=[('type', '=', 'view')]),
-    }
+    fiscalyear_id = fields.Many2one(
+        'account.fiscalyear',
+        'Fiscal Year',
+        required=True,
+    )
+    parent_asset_id = fields.Many2one(
+        'account.asset.asset',
+        'Asset Filter',
+        domain=[('type', '=', 'view')],
+    )
 
-    def xls_export(self, cr, uid, ids, context=None):
-        asset_obj = self.pool.get('account.asset.asset')
-        wiz_form = self.browse(cr, uid, ids)[0]
-        parent_asset_id = wiz_form.parent_asset_id.id
+    @api.multi
+    def xls_export(self):
+        self.ensure_one()
+        asset_obj = self.env['account.asset.asset']
+        parent_asset_id = self.parent_asset_id.id
         if not parent_asset_id:
-            parent_ids = asset_obj.search(
-                cr, uid, [('type', '=', 'view'), ('parent_id', '=', False)])
+            parent_ids = asset_obj.search([
+                ('type', '=', 'view'),
+                ('parent_id', '=', False)],
+                limit=1)
             if not parent_ids:
-                raise orm.except_orm(
-                    _('Configuration Error'),
-                    _("No top level asset of type 'view' defined!"))
+                raise ValidationError(_(
+                    "Configuration Error \n"
+                    "No top level asset of type 'view' defined!"))
             else:
-                parent_asset_id = parent_ids[0]
+                parent_asset_id = parent_ids
 
         # sanity check
-        error_ids = asset_obj.search(
-            cr, uid, [('type', '=', 'normal'), ('parent_id', '=', False)])
-        for error_id in error_ids:
-            error = asset_obj.browse(cr, uid, error_id, context=context)
+        error_ids = asset_obj.search([
+            ('type', '=', 'normal'),
+            ('parent_id', '=', False)])
+        for error in error_ids:
             error_name = error.name
             if error.code:
                 error_name += ' (' + error.code + ')' or ''
-            raise orm.except_orm(
-                _('Configuration Error'),
-                _("No parent asset defined for asset '%s'!") % error_name)
+            raise ValidationError(_(
+                "Configuration Error \n"
+                "No parent asset defined for asset '%s'!") % error_name)
 
-        domain = [('type', '=', 'normal'), ('id', 'child_of', parent_asset_id)]
-        asset_ids = asset_obj.search(cr, uid, domain)
+        domain = [
+            ('type', '=', 'normal'),
+            ('id', 'child_of', parent_asset_id.id),
+        ]
+        asset_ids = asset_obj.search_count(domain)
         if not asset_ids:
-            raise orm.except_orm(
-                _('No Data Available'),
-                _('No records found for your selection!'))
+            raise ValidationError(_(
+                "No Data Available"
+                "No records found for your selection!"))
 
         datas = {
             'model': 'account.asset.asset',
-            'fiscalyear_id': wiz_form.fiscalyear_id.id,
-            'ids': [parent_asset_id],
+            'fiscalyear_id': self.fiscalyear_id.id,
+            'ids': parent_asset_id.ids,
         }
         return {'type': 'ir.actions.report.xml',
                 'report_name': 'account.asset.xls',
                 'datas': datas}
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
