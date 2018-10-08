d6c52f04cfc6eaf7af96dd6376c422088861a5c8
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-10-08 14:52:44 +0200

    Move from TransactionCase to SavepointCase

--- account_asset_management_xls/tests/test_asset_management_xls.py
+++ account_asset_management_xls/tests/test_asset_management_xls.py
@@ -1,32 +1,34 @@
 # Copyright 2009-2018 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import odoo.tests.common as common
+from odoo.tests.common import SavepointCase
 from odoo import tools
 from odoo.modules.module import get_resource_path
 
 
-class TestAssetManagementXls(common.TransactionCase):
+class TestAssetManagementXls(SavepointCase):
 
-    def _load(self, module, *args):
-        tools.convert_file(self.cr, module,
+    @classmethod
+    def _load(cls, module, *args):
+        tools.convert_file(cls.cr, module,
                            get_resource_path(module, *args),
                            {}, 'init', False, 'test',
-                           self.registry._assertion_report)
+                           cls.registry._assertion_report)
 
-    def setUp(self):
-        super(TestAssetManagementXls, self).setUp()
+    @classmethod
+    def setUpClass(cls):
+        super(TestAssetManagementXls, cls).setUpClass()
 
-        self._load('account', 'test', 'account_minimal_test.xml')
-        self._load('account_asset_management', 'tests',
+        cls._load('account', 'test', 'account_minimal_test.xml')
+        cls._load('account_asset_management', 'tests',
                    'account_asset_test_data.xml')
         module = __name__.split('addons.')[1].split('.')[0]
-        self.xls_report_name = '{}.asset_report_xls'.format(module)
-        self.wiz_model = self.env['wiz.account.asset.report']
-        fy = self.env.ref('account_asset_management.date_range_fy')
+        cls.xls_report_name = '{}.asset_report_xls'.format(module)
+        cls.wiz_model = cls.env['wiz.account.asset.report']
+        fy = cls.env.ref('account_asset_management.date_range_fy')
         wiz_vals = {'date_range_id': fy.id}
-        self.xls_report = self.wiz_model.create(wiz_vals)
-        self.report_action = self.xls_report.xls_export()
+        cls.xls_report = cls.wiz_model.create(wiz_vals)
+        cls.report_action = cls.xls_report.xls_export()
 
     def test_01_action_xls(self):
         """ Check report XLS action """
