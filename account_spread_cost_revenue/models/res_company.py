620541fff2e4a29677718daef28943024b2a1f4b
Andrea <a.stirpe@onestein.nl>
2018-10-22 12:27:50 +0200

    [ADD] account_spread_cost_revenue

---
+++ account_spread_cost_revenue/models/res_company.py
@@ -0,0 +1,20 @@
+# Copyright 2018 Onestein (<https://www.onestein.eu>)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    default_spread_revenue_account_id = fields.Many2one(
+        'account.account', string='Revenue Spread Account')
+
+    default_spread_expense_account_id = fields.Many2one(
+        'account.account', string='Expense Spread Account')
+
+    default_spread_revenue_journal_id = fields.Many2one(
+        'account.journal', string='Revenue Spread Journal')
+
+    default_spread_expense_journal_id = fields.Many2one(
+        'account.journal', string='Expense Spread Journal')
