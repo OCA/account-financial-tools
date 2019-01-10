9bafc93c42a2107c4de9274c299293049a1db2bc
Jordy Blankestijn <jblankestijn@erpopen.nl>
2019-01-10 14:28:47 +0100

    [MIG] account_reversal: Migration to 12.0

--- account_reversal/__manifest__.py
+++ account_reversal/__manifest__.py
@@ -1,18 +1,20 @@
 # Copyright 2011 Alexis de Lattre <alexis.delattre@akretion.com>
 # Copyright 2012-2013 Guewen Baconnier (Camptocamp)
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2019 Jordy Blankestijn <jblankestijn@erpopen.nl>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Reversal",
     "summary": "Wizard for creating a reversal account move",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/account-financial-tools",
     "author": "Akretion,"
               "Camptocamp,"
               "ACSONE SA/NV,"
               "Tecnativa,"
+              "ERP|OPEN,"
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
