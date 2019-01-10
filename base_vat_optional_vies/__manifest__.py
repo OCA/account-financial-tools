d38ec840eb765e4ece021a94c1d38cd6000c282f
rodrigo <rodrigo.bonilla@factorlibre.com>
2019-01-10 12:14:00 +0100

    [MIG] base_vat_optional_vies: Migration to 11.0

--- base_vat_optional_vies/__manifest__.py
+++ base_vat_optional_vies/__manifest__.py
@@ -1,14 +1,15 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Tecnativa - Antonio Espinosa
 # Copyright 2016 Tecnativa - Sergio Teruel
 # Copyright 2017 Tecnativa - David Vidal
+# Copyright 2019 FactorLibre - Rodrigo Bonilla
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': "Optional validation of VAT via VIES",
     'category': 'Accounting',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'depends': [
         'base_vat',
+        'base',
     ],
     'external_dependencies': {
         'python': ['vatnumber'],
@@ -17,7 +18,8 @@
         'views/res_partner_view.xml',
     ],
     'author': 'Tecnativa,'
-              'Odoo Community Association (OCA)',
+              'Odoo Community Association (OCA),'
+              'FactorLibre',
     'website': 'https://www.tecnativa.com',
     'license': 'AGPL-3',
     'installable': True,
