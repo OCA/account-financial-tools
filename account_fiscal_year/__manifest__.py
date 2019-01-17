8b2c01ae02a5aa06084f0012bfe2ba27d7764cbe
Alexis de Lattre <alexis@via.ecp.fr>
2019-01-17 09:20:50 +0100

    [10.0] account_fiscal_year: add full rights on date.range* to account manager (#758)

--- account_fiscal_year/__manifest__.py
+++ account_fiscal_year/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Account Fiscal Year',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'Accounting',
     'author': 'Camptocamp SA,'
               'Odoo Community Association (OCA)',
@@ -16,6 +16,7 @@
     'data': [
         'data/date_range_type.xml',
         'views/date_range_type.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
     'application': True,
