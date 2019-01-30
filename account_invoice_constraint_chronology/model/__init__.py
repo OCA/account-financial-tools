97054914f1bb346437a83f17f538066baf3c8f67
Thomas Binsfeld <thomas.binsfeld@acsone.eu>
2019-01-30 14:27:00 +0100

    [MIG] Account Invoice Constraint Chronology to 12.0

--- account_invoice_constraint_chronology/model/__init__.py
+++ account_invoice_constraint_chronology/model/__init__.py
@@ -1,2 +1,2 @@
+from . import account_journal
 from . import account_invoice
-from . import account
--- account_invoice_constraint_chronology/model/account_journal.py
+++ account_invoice_constraint_chronology/model/account_journal.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api
@@ -7,9 +7,12 @@ from odoo import models, fields, api
 class AccountJournal(models.Model):
     _inherit = ['account.journal']
 
-    check_chronology = fields.Boolean(default=False)
+    check_chronology = fields.Boolean(
+        default=False,
+    )
 
     @api.onchange('type')
     def _onchange_type(self):
+        self.ensure_one()
         if self.type not in ['sale', 'purchase']:
             self.check_chronology = False
