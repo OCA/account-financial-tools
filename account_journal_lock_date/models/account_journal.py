51517cbc7bbef4203872909d0ec64850b4d093db
David Arnold <dar@xoe.solutions>
2019-02-01 17:55:01 -0500

    [IMP] journal lock user semantics
    The existing implementaiton already provided a "positive offset from period
    lock date" semantic. However the UX design & description would have made the
    user beleive to be able to specify any lock date on the journal that would
    ignore the more general lock dates.
    
    This is neither implemented nor intended. Therefore, improve the UX, so this
    behaviour is clearly communicated and proper onchange, warnings, and errors are
    generated for the user.
    
    Defining an offset also makes the need for wizard obsolete alltogether, which
    is why the Roadmap is updated accordingly.
    
    Signed-off-by: "David Arnold" <"dar@xoe.solutions">

--- account_journal_lock_date/models/account_journal.py
+++ account_journal_lock_date/models/account_journal.py
@@ -2,6 +2,8 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from dateutil.relativedelta import relativedelta
+
 from odoo import api, fields, models
 
 
@@ -9,12 +11,40 @@ class AccountJournal(models.Model):
 
     _inherit = 'account.journal'
 
+    journal_lock_date_period_offset = fields.Integer(
+        string="Lock date offset", default=0,
+        help="Set a stricter (later) lock date for this journal with this "
+             "offest in days respective of the period lock date."
+    )
+    has_period_lock_date = fields.Boolean(compute='_compute_lock_date')
     journal_lock_date = fields.Date(
-        string="Lock date",
+        string="Lock date", readonly=True, store=True,
+        compute='_compute_lock_date',
         help="Moves cannot be entered nor modified in this "
              "journal prior to the lock date, unless the user "
              "has the Adviser role."
     )
+
+    @api.onchange('journal_lock_date_period_offset')
+    def _onchange_lock_date_offset(self):
+        if self.journal_lock_date_period_offset < 0:
+            return {
+                "warning": {
+                    "title": "Negative lock date offset",
+                    "message": "Offset must be positive: you can only set "
+                               "stricter lock dates than the period lock date."
+                },
+                "value": {"journal_lock_date_period_offset":
+                    abs(self.journal_lock_date_period_offset)}
+            }
+
+    @api.depends('journal_lock_date_period_offset', 'company_id.period_lock_date')
+    def _compute_lock_date(self):
+        for journal in self.filtered('company_id.period_lock_date'):
+            period_lock_date = journal.company_id.period_lock_date
+            offset = relativedelta(days=+journal.journal_lock_date_period_offset)
+            journal.journal_lock_date = period_lock_date + offset
+            journal.has_period_lock_date = True
 
     @api.model
     def _can_bypass_journal_lock_date(self):
--- account_journal_lock_date/readme/DESCRIPTION.rst
+++ account_journal_lock_date/readme/DESCRIPTION.rst
@@ -10,5 +10,3 @@ implement early journal lock dates for all other journals.
 By default only users with "Adviser" role are exempt from the journal lock date
 check. Developers can implement more permissive exemptions from the lock date
 check through a hook function on `account.journal`.
-
-The lock date updating wizard will help you to updated journal lock dates, too.
