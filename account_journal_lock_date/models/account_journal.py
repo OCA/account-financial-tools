233f6b4078236ac243629d9e07d0f00dc778745b
David Arnold <dar@xoe.solutions>
2019-02-01 19:32:52 -0500

    [FIX] tests & improve them

--- account_journal_lock_date/models/account_journal.py
+++ account_journal_lock_date/models/account_journal.py
@@ -2,7 +2,7 @@
 # Copyright 2018 XOE Corp. SAS <https://xoe.solutions>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from dateutil.relativedelta import relativedelta
+from datetime import timedelta
 
 from odoo import api, fields, models, _
 from odoo.tools.misc import DEFAULT_SERVER_DATE_FORMAT
@@ -44,7 +44,7 @@ class AccountJournal(models.Model):
     @api.multi
     @api.constrains('journal_lock_date_period_offset')
     def _check_lock_date_existing_moves(self):
-        for journal in self:
+        for journal in self.filtered('journal_lock_date'):
             date_str = journal.journal_lock_date.strftime(
                 DEFAULT_SERVER_DATE_FORMAT)
             if journal.env['account.move'].search_count(
@@ -69,8 +69,7 @@ class AccountJournal(models.Model):
     def _compute_lock_date(self):
         for journal in self.filtered('company_id.period_lock_date'):
             period_lock_date = journal.company_id.period_lock_date
-            offset = relativedelta(
-                days=+journal.journal_lock_date_period_offset)
+            offset = timedelta(days=+journal.journal_lock_date_period_offset)
             journal.journal_lock_date = period_lock_date + offset
             journal.has_period_lock_date = True
 
--- account_journal_lock_date/tests/test_journal_lock_date.py
+++ account_journal_lock_date/tests/test_journal_lock_date.py
@@ -1,4 +1,5 @@
 # Copyright 2017 ACSONE SA/NV
+# Copyright 2018 XOE Corp. SAS <https://xoe.solutions>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import date, timedelta
@@ -6,6 +7,7 @@ from datetime import date, timedelta
 from odoo import fields, tools
 from odoo.modules import get_module_resource
 from odoo.tests import common
+from odoo.exceptions import ValidationError
 
 from ..exceptions import JournalLockDateError
 
@@ -19,23 +21,24 @@ class TestJournalLockDate(common.TransactionCase):
                                                'account_minimal_test.xml'),
                            {}, 'init', False, 'test')
         self.account_move_obj = self.env["account.move"]
-        self.account_move_line_obj = \
-            self.env["account.move.line"]
+        self.account_move_line_obj = self.env["account.move.line"]
         self.company_id = self.ref('base.main_company')
         self.partner = self.browse_ref("base.res_partner_12")
         self.account = self.browse_ref("account.a_recv")
         self.account2 = self.browse_ref("account.a_expense")
         self.journal = self.browse_ref("account.bank_journal")
+        self.env.user.write({'groups_id': [(5, None)]})
+        self.journal.update_posted = True
+        self.journal.company_id.period_lock_date = False
+        self.journal.journal_lock_date_period_offset = False
+        self.tomorrow = fields.Date.today() + timedelta(days=+1)
+        self.yesterday = fields.Date.today() + timedelta(days=-1)
+        self.three_days_ago= fields.Date.today() + timedelta(days=-3)
 
-    def test_journal_lock_date(self):
-        self.env.user.write({
-            'groups_id': [(3, self.ref('account.group_account_manager'))],
-        })
-        self.assertFalse(self.env.user.has_group(
-            'account.group_account_manager'))
 
-        # create a move and post it
-        move = self.account_move_obj.create({
+    def test_journal_lock_date_tracing_with_draft(self):
+        # create a move and leave it in draft
+        self.account_move_obj.create({
             'date': fields.Date.today(),
             'journal_id': self.journal.id,
             'line_ids': [(0, 0, {
@@ -48,37 +51,82 @@ class TestJournalLockDate(common.TransactionCase):
                 'name': 'Debit line',
             })]
         })
-        move.post()
+        # Cannot change the date if it would affect moves in draft
+        # They could not be validated any more unless by an "Adviser"
+        with self.assertRaises(ValidationError):
+            self.journal.company_id.period_lock_date = self.three_days_ago
+            self.journal.journal_lock_date_period_offset = 3
 
-        # lock journal
-        self.journal.journal_lock_date = fields.Date.today()
+    def test_journal_lock_date(self):
+        self.env.user.write({
+            'groups_id': [(4, self.ref('account.group_account_invoice'))],
+        })
+        self.assertFalse(self.env.user.has_group(
+            'account.group_account_manager'))
 
-        # Test that the move cannot be created, written, or cancelled
+        # create a move and post it without restrictions
+        goodmove = self.account_move_obj.create({
+            'date': fields.Date.today(),
+            'journal_id': self.journal.id,
+            'line_ids': [(0, 0, {
+                'account_id': self.account.id,
+                'credit': 1000.0,
+                'name': 'Credit line',
+            }), (0, 0, {
+                'account_id': self.account2.id,
+                'debit': 1000.0,
+                'name': 'Debit line',
+            })]
+        })
+
+        goodmove.post()
+
+        # Now, set the lock date
+        self.journal.company_id.period_lock_date = self.three_days_ago
+        self.journal.journal_lock_date_period_offset = 3
+        self.assertTrue(self.journal.journal_lock_date == fields.Date.today())
+
+        onlockdate = self.account_move_obj.create({
+            'date': fields.Date.today(),
+            'journal_id': self.journal.id,
+            'line_ids': [(0, 0, {
+                'account_id': self.account.id,
+                'credit': 1000.0,
+                'name': 'Credit line',
+            }), (0, 0, {
+                'account_id': self.account2.id,
+                'debit': 1000.0,
+                'name': 'Debit line',
+            })]
+        })
+
+        beforelockdate = self.account_move_obj.create({
+            'date': self.yesterday,
+            'journal_id': self.journal.id,
+            'line_ids': [(0, 0, {
+                'account_id': self.account.id,
+                'credit': 1000.0,
+                'name': 'Credit line',
+            }), (0, 0, {
+                'account_id': self.account2.id,
+                'debit': 1000.0,
+                'name': 'Debit line',
+            })]
+        })
+
+        # Test that the move cannot be popsted or cancelled
         with self.assertRaises(JournalLockDateError):
-            self.account_move_obj.create({
-                'date': fields.Date.today(),
-                'journal_id': self.journal.id,
-                'line_ids': [(0, 0, {
-                    'account_id': self.account.id,
-                    'credit': 1000.0,
-                    'name': 'Credit line',
-                }), (0, 0, {
-                    'account_id': self.account2.id,
-                    'debit': 1000.0,
-                    'name': 'Debit line',
-                })]
-            })
-
+            onlockdate.post()
         with self.assertRaises(JournalLockDateError):
-            move.write({'name': 'TEST'})
-
+            beforelockdate.post()
         with self.assertRaises(JournalLockDateError):
-            move.button_cancel()
+            onlockdate.button_cancel()
+        with self.assertRaises(JournalLockDateError):
+            beforelockdate.button_cancel()
 
         # create a move after ther lock date and post it
-        tomorrow = date.today() + timedelta(days=1)
         move3 = self.account_move_obj.create({
-            'date': tomorrow,
+            'date': self.tomorrow,
             'journal_id': self.journal.id,
             'line_ids': [(0, 0, {
                 'account_id': self.account.id,
@@ -99,12 +147,12 @@ class TestJournalLockDate(common.TransactionCase):
         })
         self.assertTrue(self.env.user.has_group(
             'account.group_account_manager'))
+        self.journal.company_id.period_lock_date = self.three_days_ago
+        self.journal.journal_lock_date_period_offset = 3
+        self.assertTrue(self.journal.journal_lock_date == fields.Date.today())
 
-        # lock journal
-        self.journal.journal_lock_date = fields.Date.today()
-
-        # advisers can create moves before or on the lock date
-        self.account_move_obj.create({
+        # advisers can post or cancel moves before or on the lock date
+        onlockdate = self.account_move_obj.create({
             'date': fields.Date.today(),
             'journal_id': self.journal.id,
             'line_ids': [(0, 0, {
@@ -117,3 +165,22 @@ class TestJournalLockDate(common.TransactionCase):
                 'name': 'Debit line',
             })]
         })
+
+        beforelockdate = self.account_move_obj.create({
+            'date': self.yesterday,
+            'journal_id': self.journal.id,
+            'line_ids': [(0, 0, {
+                'account_id': self.account.id,
+                'credit': 1000.0,
+                'name': 'Credit line',
+            }), (0, 0, {
+                'account_id': self.account2.id,
+                'debit': 1000.0,
+                'name': 'Debit line',
+            })]
+        })
+
+        onlockdate.post()
+        beforelockdate.post()
+        onlockdate.button_cancel()
+        beforelockdate.button_cancel()
