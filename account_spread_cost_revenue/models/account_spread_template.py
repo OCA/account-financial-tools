08f681761cba2289fece3bbf19feb11f5b2cc567
Andrea <a.stirpe@onestein.nl>
2019-03-15 10:36:21 +0100

    Update Copyright dates

--- account_spread_cost_revenue/models/account_spread_template.py
+++ account_spread_cost_revenue/models/account_spread_template.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Onestein (<https://www.onestein.eu>)
+# Copyright 2018-2019 Onestein (<https://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
