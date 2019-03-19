60a792558be0bdb91cebb7302b6329c8fe224c5d
Alexis de Lattre <alexis.delattre@akretion.com>
2019-03-19 23:12:50 +0100

    [MIG] account_netting from v11 to v12

--- account_netting/tests/test_account_netting.py
+++ account_netting/tests/test_account_netting.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import openerp.tests.common as common
+import odoo.tests.common as common
 
 
 class TestAccountNetting(common.SavepointCase):
