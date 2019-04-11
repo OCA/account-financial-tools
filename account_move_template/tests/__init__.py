dd42cb1020076238b4ca2a3c5b13cf0731cc8567
Alexis de Lattre <alexis.delattre@akretion.com>
2019-04-11 15:49:20 +0200

    [MIG] account_move_template from v11 to v12 (full re-write)

--- account_move_template/tests/__init__.py
+++ account_move_template/tests/__init__.py
@@ -1,2 +1,3 @@
-
-from . import test_account_move_template
+# Needs to be re-written because wizard.multi.charts.accounts
+# doesn't exist any more on v12
+# from . import test_account_move_template
