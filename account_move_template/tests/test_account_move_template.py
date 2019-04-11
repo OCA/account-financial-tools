d2da4f4276aa2bde93b4ca591ca28adc33b21fa5
Alexis de Lattre <alexis.delattre@akretion.com>
2019-04-11 15:49:20 +0200

    [MIG] account_move_template from v11 to v12 (full re-write)

--- account_move_template/tests/test_account_move_template.py
+++ account_move_template/tests/test_account_move_template.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018-2019 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 import logging
 from psycopg2 import IntegrityError
