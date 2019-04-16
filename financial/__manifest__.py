e447304ac39789908e40600a8836675ad2bc68fc
Luis Felipe Mileo <mileo@kmee.com.br>
2019-04-16 18:43:21 -0300

    [MIG] Module version
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- financial/__manifest__.py
+++ financial/__manifest__.py
@@ -6,7 +6,7 @@
     'name': 'Financial',
     'summary': """
         Financial""",
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'KMEE,Odoo Community Association (OCA)',
     'website': 'www.kmee.com.br',
