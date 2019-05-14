b645db03713d0e9d91e37f7a84164c6f806dca58
vrenaville <vincent.renaville@camptocamp.com>
2019-05-14 15:25:22 +0200

    [FIX] Protect import for uninstalled module

--- account_asset_management_xls/report/__init__.py
+++ account_asset_management_xls/report/__init__.py
@@ -1,2 +1,10 @@
-# -*- coding: utf-8 -*-
-from . import account_asset_report_xls
+
+try:
+    from odoo.addons.report_xlsx_helper.report.abstract_report_xlsx \
+        import AbstractReportXlsx
+except (ImportError, IOError) as err:
+    import logging
+    _logger = logging.getLogger(__name__)
+    _logger.debug(err)
+else:
+    from . import account_asset_report_xls
--- account_move_batch_validate/__init__.py
+++ account_move_batch_validate/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2014 Camptocamp SA, 2017 ACSONE
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import models
-from . import wizard
+try:
+    from odoo.addons.queue_job.job \
+        import job, Job
+except (ImportError, IOError) as err:
+    import logging
+    _logger = logging.getLogger(__name__)
+    _logger.debug(err)
+else:
+    from . import models
+    from . import wizard
