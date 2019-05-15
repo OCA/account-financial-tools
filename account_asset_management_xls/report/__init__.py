a33d9b0dcff035cbe92bdbb953cbaa8ce6c46f91
Vincent Renaville <vincent.renaville@camptocamp.com>
2019-05-15 09:04:54 +0200

    [FIX] protect import if module is not present (#818)
    * [FIX] Protect import for uninstalled module

--- account_asset_management_xls/report/__init__.py
+++ account_asset_management_xls/report/__init__.py
@@ -1,2 +1,13 @@
-# -*- coding: utf-8 -*-
-from . import account_asset_report_xls
+
+# The whole python file is ignored when the dependency is missing
+# because it contains an old-style rml_parse report which would
+# be loaded anyway
+try:
+    from odoo.addons.report_xlsx_helper.report.abstract_report_xlsx \
+        import AbstractReportXlsx
+except (ImportError, IOError) as err:
+    import logging
+    _logger = logging.getLogger(__name__)
+    _logger.debug(err)
+else:
+    from . import account_asset_report_xls
