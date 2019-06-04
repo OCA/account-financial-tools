ef6a5543d92e6b5711027690abf73d4d509dfa9f
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-06-04 12:38:36 +0200

    [MIG] base_vat_optional_vies: Migration to 12.0

--- base_vat_optional_vies/models/res_partner.py
+++ base_vat_optional_vies/models/res_partner.py
@@ -9,7 +9,7 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     vies_passed = fields.Boolean(
-        string="VIES validation passed", readonly=True)
+        string="VIES validation", readonly=True)
 
     @api.model
     def simple_vat_check(self, country_code, vat_number):
@@ -34,7 +34,7 @@ class ResPartner(models.Model):
             return self.simple_vat_check(country_code, vat_number)
         return res
 
-    @api.constrains('vat', 'commercial_partner_country_id')
+    @api.constrains('vat', 'commercial_partner.country_id')
     def check_vat(self):
         for partner in self:
             partner = partner.with_context(vat_partner=partner)
---
+++ base_vat_optional_vies/readme/CONTRIBUTORS.rst
@@ -0,0 +1,6 @@
+* Rafael Blasco <rafael.blasco@tecnativa.com>
+* Antonio Espinosa <antonio.espinosa@tecnativa.com>
+* Sergio Teruel <sergio.teruel@tecnativa.com>
+* David Vidal <david.vidal@tecnativa.com>
+* Rodrigo Bonilla <rodrigo.bonilla@factorlibre.com>
+* Alexandre Díaz <alexandre.diaz@tecnativa.com>
