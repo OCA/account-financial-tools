f229ec2d7c0d57b488a5cd02276428639ea54e7d
MMI <michael.michot@masthercell.com>
2019-07-01 16:37:25 +0200

    [MIG] account_journal_lock_date: Migration to 11.0

--- account_journal_lock_date/__manifest__.py
+++ account_journal_lock_date/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -6,16 +5,14 @@
     'name': 'Account Journal Lock Date',
     'summary': """
         Lock each journal independently""",
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'website': 'https://acsone.eu/',
+    'website': 'https://github.com/OCA/account-financial-tools',
     'depends': [
         'account_permanent_lock_move',
     ],
     'data': [
         'views/account_journal.xml',
-    ],
-    'demo': [
     ],
 }
