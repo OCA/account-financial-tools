eb95da310266b14b5f8539ee00c92a3e84f5f540
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2019-07-11 18:49:31 +0200

    [FIX] bump version account_move_line_purchase_info
    To force wheel regeneration, because the
    currently published wheel wrongly
    depends on odoo12-addon-purchase_stock
    which is a standard Odoo module.

--- account_move_line_purchase_info/__manifest__.py
+++ account_move_line_purchase_info/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Account Move Line Purchase Info",
     "summary": "Introduces the purchase order line to the journal items",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "https://www.github.com/OCA/account-financial-tools",
