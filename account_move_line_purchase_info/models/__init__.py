ff67485f283d7b54443136a69fe2e20dd4d6fa9c
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-07-29 12:32:21 +0200

    forward port latest changes from 11.0

--- account_move_line_purchase_info/models/__init__.py
+++ account_move_line_purchase_info/models/__init__.py
@@ -1,5 +1,5 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import account_move
 from . import account_invoice
+from . import purchase_order_line
 from . import stock_move
+
