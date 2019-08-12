61a4042c81a084fefc06592a805d203906c005df
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-08-12 11:42:21 +0200

    [FIX] account_asset_management: Provide hook _compute_depreciation_amount_per_fiscal_year
    This way, other method_time mechanism like localization ones is able to overwrite it.
    
    You also got an error of variable `number` not set due to previous code. Although no
    alternate implementation for a new method_time, now you don't have any error,
    fallbacking to standard proportional repartition.

--- account_asset_management/__manifest__.py
+++ account_asset_management/__manifest__.py
@@ -1,9 +1,10 @@
 # Copyright 2009-2018 Noviat
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Assets Management',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'license': 'AGPL-3',
     'depends': [
         'account',
