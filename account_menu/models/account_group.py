1485bf16386d6a12ec2a5162955456fe832f9324
Raf Ven <raf.ven@dynapps.be>
2019-09-30 15:50:37 +0200

    [MIG] account_menu: Migration to 13.0

---
+++ account_menu/models/account_group.py
@@ -0,0 +1,15 @@
+# Copyright 2018 FOREST AND BIOMASS ROMANIA SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class AccountGroup(models.Model):
+    _inherit = "account.group"
+
+    account_ids = fields.One2many(
+        comodel_name="account.account",
+        inverse_name="group_id",
+        string="Accounts",
+        help="Assigned accounts.",
+    )
