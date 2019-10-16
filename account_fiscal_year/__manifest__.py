d07d131e0a755b01f4c5e71dfe2302cbdc156dfd
Andrea <a.stirpe@onestein.nl>
2019-10-16 09:24:41 +0200

    [13.0][MIG] account_fiscal_year

--- account_fiscal_year/__manifest__.py
+++ account_fiscal_year/__manifest__.py
@@ -4,22 +4,16 @@
 {
     "name": "Account Fiscal Year",
     "summary": "Create a menu for Account Fiscal Year",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "development_status": "Beta",
     "category": "Accounting",
     "website": "https://github.com/OCA/account-financial-tools",
     "author": "Agile Business Group, Camptocamp SA, "
-              "Odoo Community Association (OCA)",
+    "Odoo Community Association (OCA)",
     "maintainers": ["eLBati"],
     "license": "LGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "account",
-        "date_range",
-    ],
-    "data": [
-        "data/date_range_type.xml",
-        "views/account_views.xml"
-    ],
+    "depends": ["account", "date_range"],
+    "data": ["data/date_range_type.xml", "views/account_views.xml"],
 }
