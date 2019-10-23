a11ea4e25cd5b590cc41ee8a8e5bc80cd25293a5
Joan Sisquella <joan.sisquella@eficent.com>
2019-10-23 13:05:26 +0200

    [ADD] allows setting a depreciation ending date

--- account_asset_management/models/account_asset_profile.py
+++ account_asset_management/models/account_asset_profile.py
@@ -153,7 +153,7 @@ class AccountAssetProfile(models.Model):
         'Number' and 'End' Time Methods.
         """
         return [
-            ('year', _('Number of Years')),
+            ('year', _('Number of Years or end date')),
         ]
 
     @api.multi
