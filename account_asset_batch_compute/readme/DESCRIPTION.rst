dee91f6e164360cda8bce5bf59fd2475feafd594
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2019-10-31 07:20:20 +0100

    migrate account_asset_batch_compute

---
+++ account_asset_batch_compute/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+Add the possibility to compute assets in batch.
+This module adds a flag on compute assets wizard in order to execute
+this process in batch.
+
--- account_asset_batch_compute/wizards/account_asset_compute.py
+++ account_asset_batch_compute/wizards/account_asset_compute.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models, _
+from odoo import api, fields, models, _
 
 import logging
 _logger = logging.getLogger(__name__)
@@ -12,14 +11,10 @@ try:
 except ImportError:
     _logger.debug('Can not `import queue_job`.')
 
-    def empty_decorator(func):
-        return func
-    job = empty_decorator
 
+class AccountAssetCompute(models.TransientModel):
 
-class AssetDepreciationConfirmationWizard(models.TransientModel):
-
-    _inherit = 'asset.depreciation.confirmation.wizard'
+    _inherit = 'account.asset.compute'
 
     batch_processing = fields.Boolean()
 
@@ -28,8 +23,7 @@ class AssetDepreciationConfirmationWizard(models.TransientModel):
     def asset_compute(self):
         self.ensure_one()
         if not self.batch_processing:
-            return super(AssetDepreciationConfirmationWizard, self)\
-                .asset_compute()
+            return super(AccountAssetCompute, self).asset_compute()
         if not self.env.context.get('job_uuid'):
             description = \
                 _("Creating jobs to create moves for assets to %s") % (
@@ -37,6 +31,6 @@ class AssetDepreciationConfirmationWizard(models.TransientModel):
             job = self.with_delay(description=description).asset_compute()
             return u'Job created with uuid %s' % (job.uuid,)
         else:
-            self = self.with_context(asset_batch_processing=True)
-            return super(AssetDepreciationConfirmationWizard, self)\
-                .asset_compute()
+            return super(
+                AccountAssetCompute, self.with_context(
+                    asset_batch_processing=True)).asset_compute()
