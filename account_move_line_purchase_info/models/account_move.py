07809e4451b577af70393ec18cc89a8299be1c15
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-24 10:29:45 +0100

    [MIG] account_move_line_purchase_info: Migration to 13.0

--- account_move_line_purchase_info/models/account_move.py
+++ account_move_line_purchase_info/models/account_move.py
@@ -1,5 +1,5 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-#           (www.eficent.com)
+# Copyright 2019 ForgeFlow S.L.
+#   (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -8,13 +8,6 @@ from odoo import fields, models
 class AccountMoveLine(models.Model):
 
     _inherit = "account.move.line"
-
-    purchase_line_id = fields.Many2one(
-        comodel_name="purchase.order.line",
-        string="Purchase Order Line",
-        ondelete="set null",
-        index=True,
-    )
 
     purchase_id = fields.Many2one(
         comodel_name="purchase.order",
