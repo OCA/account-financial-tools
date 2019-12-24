270d7e00a35a3f2a9198aa6b92b0a3dd56cec4b6
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-24 09:09:12 +0100

    [IMP] account_move_line_purchase_info: black, isort

--- account_move_line_purchase_info/models/account_invoice.py
+++ account_move_line_purchase_info/models/account_invoice.py
@@ -7,23 +7,23 @@ from odoo import api, models
 
 class AccountInvoice(models.Model):
 
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     @api.model
     def invoice_line_move_line_get(self):
         res = super(AccountInvoice, self).invoice_line_move_line_get()
 
-        invoice_line_model = self.env['account.invoice.line']
+        invoice_line_model = self.env["account.invoice.line"]
         for move_line_dict in res:
-            if 'invl_id' in move_line_dict:
-                line = invoice_line_model.browse(move_line_dict['invl_id'])
-                move_line_dict['purchase_line_id'] = line.purchase_line_id.id
+            if "invl_id" in move_line_dict:
+                line = invoice_line_model.browse(move_line_dict["invl_id"])
+                move_line_dict["purchase_line_id"] = line.purchase_line_id.id
 
         return res
 
     @api.model
     def line_get_convert(self, line, part):
         res = super(AccountInvoice, self).line_get_convert(line, part)
-        if line.get('purchase_line_id', False):
-            res['purchase_line_id'] = line.get('purchase_line_id')
+        if line.get("purchase_line_id", False):
+            res["purchase_line_id"] = line.get("purchase_line_id")
         return res
