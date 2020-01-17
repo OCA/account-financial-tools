d051f6d84485f19e8857e407bc4332a8453dfca0
Andrea <a.stirpe@onestein.nl>
2020-01-17 11:39:54 +0100

    [13.0][MIG] account_lock_date_update

--- account_lock_date_update/tests/test_account_lock_date_update.py
+++ account_lock_date_update/tests/test_account_lock_date_update.py
@@ -9,13 +9,13 @@ from odoo.exceptions import UserError
 class TestAccountLockDateUpdate(TransactionCase):
 
     def setUp(self):
-        super(TestAccountLockDateUpdate, self).setUp()
+        super().setUp()
         self.company = self.env.ref('base.main_company')
         self.demo_user = self.env.ref('base.user_demo')
         self.adviser_group = self.env.ref('account.group_account_manager')
         self.UpdateLockDateUpdateObj = self.env[
             'account.update.lock_date'
-        ].sudo(self.demo_user)
+        ].with_user(self.demo_user)
 
     def create_account_lock_date_update(self):
         return self.UpdateLockDateUpdateObj.create({
@@ -32,7 +32,7 @@ class TestAccountLockDateUpdate(TransactionCase):
             'groups_id': [(3, self.adviser_group.id)],
         })
         with self.assertRaises(UserError):
-            wizard.sudo(self.demo_user.id).execute()
+            wizard.with_user(self.demo_user.id).execute()
 
     def test_02_update_with_access(self):
         wizard = self.create_account_lock_date_update()
@@ -43,7 +43,7 @@ class TestAccountLockDateUpdate(TransactionCase):
         self.demo_user.write({
             'groups_id': [(4, self.adviser_group.id)],
         })
-        wizard.sudo(self.demo_user.id).execute()
+        wizard.with_user(self.demo_user.id).execute()
         self.assertEqual(
             fields.Date.to_string(self.company.period_lock_date),
             '2000-02-01')
