bad407a63da7d1629a5dd191d4ee993ad89575f5
Stéphane Valaeys <svalaeys@fiefmanage.ch>
2020-01-17 16:05:25 +0100

    Fixed dynamic build of the name of the view to be called when errors are found

--- account_asset_management/wizard/account_asset_compute.py
+++ account_asset_management/wizard/account_asset_compute.py
@@ -26,7 +26,7 @@ class AccountAssetCompute(models.TransientModel):
             module = __name__.split('addons.')[1].split('.')[0]
             result_view = self.env.ref(
                 '%s.%s_view_form_result'
-                % (module, self._name))
+                % (module, self._table))
             self.note = _("Compute Assets errors") + ':\n' + error_log
             return {
                 'name': _('Compute Assets result'),
