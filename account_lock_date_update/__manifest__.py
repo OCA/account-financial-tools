1a31dadfdc44a30bcbc4a53118f2ff2eacd6c56b
Andrea <a.stirpe@onestein.nl>
2020-01-17 11:41:26 +0100

    pre-commit

--- account_lock_date_update/__manifest__.py
+++ account_lock_date_update/__manifest__.py
@@ -2,19 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Lock Date Update',
-    'summary': """
+    "name": "Account Lock Date Update",
+    "summary": """
         Allow an Account adviser to update locking date without having
         access to all technical settings""",
-    'version': '13.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-financial-tools',
-    'installable': True,
-    'depends': [
-        'account',
-    ],
-    'data': [
-        'wizards/account_update_lock_date.xml',
-    ],
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "installable": True,
+    "depends": ["account"],
+    "data": ["wizards/account_update_lock_date.xml"],
 }
