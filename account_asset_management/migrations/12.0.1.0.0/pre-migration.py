034c59e6f5ab2275f04b248430064edac029e4ce
ernestotejeda <ernesto.tejeda87@gmail.com>
2020-01-29 12:34:31 -0500

    [IMP] account_asset_management: black, isort

--- account_asset_management/migrations/12.0.1.0.0/pre-migration.py
+++ account_asset_management/migrations/12.0.1.0.0/pre-migration.py
@@ -4,47 +4,46 @@
 from openupgradelib import openupgrade
 
 _model_renames = [
-    ('account.asset.category', 'account.asset.profile'),
-    ('account.asset.depreciation.line', 'account.asset.line'),
-    ('account.asset.asset', 'account.asset'),
+    ("account.asset.category", "account.asset.profile"),
+    ("account.asset.depreciation.line", "account.asset.line"),
+    ("account.asset.asset", "account.asset"),
 ]
 
 _table_renames = [
-    (old.replace('.', '_'), new.replace('.', '_'))
-    for (old, new) in _model_renames
+    (old.replace(".", "_"), new.replace(".", "_")) for (old, new) in _model_renames
 ]
 
 _column_copies = {
-    'account_asset': [
-        ('method_number', None, None),
-        ('method_time', None, None),
-    ],
-    'account_asset_profile': [
-        ('method_number', None, None),
-        ('method_time', None, None),
+    "account_asset": [("method_number", None, None), ("method_time", None, None)],
+    "account_asset_profile": [
+        ("method_number", None, None),
+        ("method_time", None, None),
     ],
 }
 
 _column_renames = {
-    'account_asset': [
-        ('method_period', None),
-    ],
-    'account_asset_profile': [
-        ('method_period', None),
-    ],
+    "account_asset": [("method_period", None)],
+    "account_asset_profile": [("method_period", None)],
 }
 
 _field_renames = [
-    ('account.asset', 'account_asset', 'category_id', 'profile_id'),
-    ('account.asset', 'account_asset', 'currency_id', 'company_currency_id'),
-    ('account.asset', 'account_asset', 'date', 'date_start'),
-    ('account.asset', 'account_asset', 'value', 'purchase_value'),
-    ('account.asset.line', 'account_asset_line',
-     'depreciation_date', 'line_date'),
-    ('account.asset.profile', 'account_asset_profile',
-     'account_depreciation_expense_id', 'account_expense_depreciation_id'),
-    ('account.invoice.line', 'account_invoice_line',
-     'asset_category_id', 'asset_profile_id'),
+    ("account.asset", "account_asset", "category_id", "profile_id"),
+    ("account.asset", "account_asset", "currency_id", "company_currency_id"),
+    ("account.asset", "account_asset", "date", "date_start"),
+    ("account.asset", "account_asset", "value", "purchase_value"),
+    ("account.asset.line", "account_asset_line", "depreciation_date", "line_date"),
+    (
+        "account.asset.profile",
+        "account_asset_profile",
+        "account_depreciation_expense_id",
+        "account_expense_depreciation_id",
+    ),
+    (
+        "account.invoice.line",
+        "account_invoice_line",
+        "asset_category_id",
+        "asset_profile_id",
+    ),
 ]
 
 
@@ -54,20 +53,27 @@ def handle_account_asset_disposal_migration(env):
     In this phase we rename stuff for adapting to the new data structure.
     """
     cr = env.cr
-    if not openupgrade.column_exists(cr, 'account_asset', 'disposal_move_id'):
+    if not openupgrade.column_exists(cr, "account_asset", "disposal_move_id"):
         return
-    openupgrade.copy_columns(cr, {'account_asset': [('state', None, None)]})
-    openupgrade.rename_columns(
-        cr, {'account_asset': [('disposal_move_id', None)]})
+    openupgrade.copy_columns(cr, {"account_asset": [("state", None, None)]})
+    openupgrade.rename_columns(cr, {"account_asset": [("disposal_move_id", None)]})
     openupgrade.map_values(
-        cr, openupgrade.get_legacy_name('state'), 'state',
-        [('disposed', 'removed')], table='account_asset',
+        cr,
+        openupgrade.get_legacy_name("state"),
+        "state",
+        [("disposed", "removed")],
+        table="account_asset",
     )
     openupgrade.rename_fields(
-        env, [
-            ('account.asset', 'account_asset', 'disposal_date', 'date_remove'),
-            ('account.asset.profile', 'account_asset_profile',
-             'account_loss_id', 'account_residual_value_id'),
+        env,
+        [
+            ("account.asset", "account_asset", "disposal_date", "date_remove"),
+            (
+                "account.asset.profile",
+                "account_asset_profile",
+                "account_loss_id",
+                "account_residual_value_id",
+            ),
         ],
     )
 
@@ -75,9 +81,9 @@ def handle_account_asset_disposal_migration(env):
 @openupgrade.migrate()
 def migrate(env, version):
     cr = env.cr
-    if openupgrade.table_exists(cr, 'account_asset_asset'):
+    if openupgrade.table_exists(cr, "account_asset_asset"):
         # `account_asset` module was installed in v11
-        if openupgrade.table_exists(cr, 'account_asset'):
+        if openupgrade.table_exists(cr, "account_asset"):
             # `account_asset_management` module also installed in v11
             # TODO: Merge in existing tables assets if both module installed
             pass
@@ -88,9 +94,16 @@ def migrate(env, version):
             openupgrade.rename_columns(cr, _column_renames)
             openupgrade.rename_fields(env, _field_renames)
             handle_account_asset_disposal_migration(env)
-            if openupgrade.column_exists(cr, 'account_asset',
-                                         'analytic_account_id'):
+            if openupgrade.column_exists(cr, "account_asset", "analytic_account_id"):
                 # account_asset_analytic module in OCA/account_analytic
                 openupgrade.rename_fields(
-                    env, [('account.asset', 'account_asset',
-                           'analytic_account_id', 'account_analytic_id')])
+                    env,
+                    [
+                        (
+                            "account.asset",
+                            "account_asset",
+                            "analytic_account_id",
+                            "account_analytic_id",
+                        )
+                    ],
+                )
--- account_asset_management/migrations/12.0.2.0.0/pre-migration.py
+++ account_asset_management/migrations/12.0.2.0.0/pre-migration.py
@@ -3,14 +3,9 @@
 from openupgradelib import openupgrade
 from psycopg2 import sql
 
-
 _column_renames = {
-    'account_asset_profile': [
-        ('parent_id', None),
-    ],
-    'account_asset': [
-        ('parent_id', None),
-    ],
+    "account_asset_profile": [("parent_id", None)],
+    "account_asset": [("parent_id", None)],
 }
 
 
@@ -18,31 +13,35 @@ def move_view_assets(cr):
     """Copy view assets to other table for preserving them, but outside of the
     main table, so remove them from there.
     """
-    temp_table = sql.Identifier(
-        openupgrade.get_legacy_name('account_asset_view'))
+    temp_table = sql.Identifier(openupgrade.get_legacy_name("account_asset_view"))
     openupgrade.logged_query(
-        cr, sql.SQL("""
+        cr,
+        sql.SQL(
+            """
         CREATE TABLE {} AS (
             SELECT * FROM account_asset
             WHERE type='view'
-        )""").format(temp_table),
+        )"""
+        ).format(temp_table),
     )
     openupgrade.logged_query(cr, "DELETE FROM account_asset WHERE type='view'")
 
 
 @openupgrade.migrate()
 def migrate(env, version):
-    if openupgrade.column_exists(env.cr, 'account_asset', 'parent_id'):
+    if openupgrade.column_exists(env.cr, "account_asset", "parent_id"):
         # if migrating directly from v11 `account_asset` module, there are no
         # view assets nor parents
         openupgrade.rename_columns(env.cr, _column_renames)
         openupgrade.logged_query(
-            env.cr, """
+            env.cr,
+            """
             ALTER TABLE account_asset
             DROP CONSTRAINT account_asset_parent_id_fkey""",
         )
         openupgrade.logged_query(
-            env.cr, """
+            env.cr,
+            """
             ALTER TABLE account_asset_profile
             DROP CONSTRAINT account_asset_profile_parent_id_fkey""",
         )
