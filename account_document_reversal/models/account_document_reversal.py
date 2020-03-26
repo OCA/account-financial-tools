0cbb02677037c852331a71c9ef94ca1f9021a898
kittiu <kittiu@gmail.com>
2020-03-26 11:38:12 +0700

    [IMP] account_document_reversal: black, isort

--- account_document_reversal/models/account_document_reversal.py
+++ account_document_reversal/models/account_document_reversal.py
@@ -1,17 +1,18 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-from odoo import models, api
+from odoo import api, models
 
 
 class AccountDocumentReversal(models.AbstractModel):
-    _name = 'account.document.reversal'
-    _description = 'Abstract Module for Document Reversal'
+    _name = "account.document.reversal"
+    _description = "Abstract Module for Document Reversal"
 
     @api.model
     def reverse_document_wizard(self):
         """ Return Wizard to Cancel Document """
-        action = self.env.ref('account_document_reversal.'
-                              'action_view_reverse_account_document')
+        action = self.env.ref(
+            "account_document_reversal." "action_view_reverse_account_document"
+        )
         vals = action.read()[0]
         return vals
 
