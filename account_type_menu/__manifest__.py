724df81de45b1fe35bc7eb563fb0967416b54c9d
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-05-13 18:25:13 +0200

    [IMP] account_type_menu: black, isort, prettier

--- account_type_menu/__manifest__.py
+++ account_type_menu/__manifest__.py
@@ -3,14 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Types Menu',
-    'version': '12.0.1.0.0',
-    'category': 'Accounting',
-    'license': 'AGPL-3',
-    'summary': "Adds a menu entry for Account Types",
-    'author': "Akretion, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/account-financial-tools',
-    'depends': ['account'],
-    'data': ['views/account_type.xml'],
-    'installable': True,
+    "name": "Account Types Menu",
+    "version": "12.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "summary": "Adds a menu entry for Account Types",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "depends": ["account"],
+    "data": ["views/account_type.xml"],
+    "installable": True,
 }
