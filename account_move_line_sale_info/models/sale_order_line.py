c6e4b41d4f1332131b76525bc315ecee852ce679
ahenriquez <ahenriquez@eficent.com>
2020-05-15 15:12:20 +0200

    [ADD]account_move_line_sale_info

---
+++ account_move_line_sale_info/models/sale_order_line.py
@@ -0,0 +1,20 @@
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    @api.multi
+    def name_get(self):
+        result = []
+        orig_name = dict(super(SaleOrderLine, self).name_get())
+        for line in self:
+            name = orig_name[line.id]
+            if self.env.context.get('so_line_info', False):
+                name = "[%s] %s (%s)" % (line.order_id.name, name,
+                                         line.order_id.state)
+            result.append((line.id, name))
+        return result
