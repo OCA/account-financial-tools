e13346dfa56cd00cc09b48849e5689d811d963a5
ahenriquez <ahenriquez@eficent.com>
2020-05-15 15:12:20 +0200

    [ADD]account_move_line_sale_info

---
+++ account_move_line_sale_info/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Move Line Sale Info",
+    "summary": "Introduces the purchase order line to the journal items",
+    "version": "11.0.1.0.0",
+    "author": "ForgeFlow S.L., "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.github.com/OCA/account-financial-tools",
+    "category": "Generic",
+    "depends": ["account", "sale"],
+    "license": "AGPL-3",
+    "data": [
+        "security/account_security.xml",
+        "views/account_move_view.xml",
+    ],
+    'installable': True,
+    'post_init_hook': 'post_init_hook',
+}
