729b0c2a73b4e445523aecfa0ac48371535b73aa
qubiq <andres.garcia@qubiq.es>
2020-05-19 10:14:25 +0200

    [IMP] account_fiscal_position_vat_check: black, isort

--- account_fiscal_position_vat_check/__manifest__.py
+++ account_fiscal_position_vat_check/__manifest__.py
@@ -3,16 +3,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Fiscal Position VAT Check',
-    'version': '12.0.1.0.0',
-    'category': 'Invoices & Payments',
-    'license': 'AGPL-3',
-    'summary': 'Check VAT on invoice validation',
-    'author': "Akretion,Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/account-financial-tools',
-    'depends': ['account', 'base_vat'],
-    'data': [
-        'views/account_fiscal_position.xml',
-    ],
-    'installable': True,
+    "name": "Account Fiscal Position VAT Check",
+    "version": "12.0.1.0.0",
+    "category": "Invoices & Payments",
+    "license": "AGPL-3",
+    "summary": "Check VAT on invoice validation",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "depends": ["account", "base_vat"],
+    "data": ["views/account_fiscal_position.xml"],
+    "installable": True,
 }
--- account_fiscal_position_vat_check/models/account_invoice.py
+++ account_fiscal_position_vat_check/models/account_invoice.py
@@ -2,12 +2,12 @@
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import UserError
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     @api.multi
     def action_move_create(self):
@@ -15,15 +15,18 @@ class AccountInvoice(models.Model):
         fiscal position"""
         for invoice in self:
             if (
-                    invoice.type in ('out_invoice', 'out_refund') and
-                    invoice.fiscal_position_id.vat_required and
-                    not invoice.partner_id.vat):
-                raise UserError(_(
-                    "You are trying to validate a customer invoice/refund "
-                    "with the fiscal position '%s' that require the customer "
-                    "to have a VAT number. But the Customer '%s' doesn't have "
-                    "a VAT number in Odoo. Please add the VAT number of this "
-                    "Customer in Odoo and try to validate again.") % (
-                        invoice.fiscal_position_id.name,
-                        invoice.partner_id.display_name))
+                invoice.type in ("out_invoice", "out_refund")
+                and invoice.fiscal_position_id.vat_required
+                and not invoice.partner_id.vat
+            ):
+                raise UserError(
+                    _(
+                        "You are trying to validate a customer invoice/refund "
+                        "with the fiscal position '%s' that require the customer "
+                        "to have a VAT number. But the Customer '%s' doesn't have "
+                        "a VAT number in Odoo. Please add the VAT number of this "
+                        "Customer in Odoo and try to validate again."
+                    )
+                    % (invoice.fiscal_position_id.name, invoice.partner_id.display_name)
+                )
         return super(AccountInvoice, self).action_move_create()
