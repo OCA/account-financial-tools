a6fc10b0690053c8a1e3658a272b337abb107ad6
qubiq <andres.garcia@qubiq.es>
2020-05-19 13:01:59 +0200

    [MIG] account_fiscal_position_vat_check: Migration to 13.0

--- account_fiscal_position_vat_check/__manifest__.py
+++ account_fiscal_position_vat_check/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Fiscal Position VAT Check",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Invoices & Payments",
     "license": "AGPL-3",
     "summary": "Check VAT on invoice validation",
