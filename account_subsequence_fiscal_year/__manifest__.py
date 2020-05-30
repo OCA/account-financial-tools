b500dd27b82c111f5f749e3fc1ffbd0bec0efb60
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-30 14:50:27 +0200

    [ADD] new module account_subsequence_fiscal_year
    Co-authored-by: Saran @ Ecosoft <saranl@ecosoft.co.th>

---
+++ account_subsequence_fiscal_year/__manifest__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Accounting Subsequences per Fiscal Years",
+    "summary": "Allow to create sub sequences for account moves number, based"
+    " on the fiscal years settings",
+    "version": "12.0.1.0.0",
+    "category": "Accounting",
+    "author": "GRAP,Odoo Community Association (OCA)",
+    "maintainers": ["legalsylvain"],
+    "website": "http://www.github.com/OCA/account-financial-tools",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/view_res_config_settings.xml",
+    ],
+    "demo": [
+        "demo/res_groups.xml",
+    ],
+    "images": [
+        "static/description/res_config_setting.png",
+    ],
+}
