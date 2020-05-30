1ef9c7ead84f35c9087c76533a510f008a41994b
Sylvain LE GAL <sylvain.legal@grap.coop>
2020-05-30 14:50:27 +0200

    [ADD] new module account_subsequence_fiscal_year
    Co-authored-by: Saran @ Ecosoft <saranl@ecosoft.co.th>

---
+++ account_subsequence_fiscal_year/models/res_config_settings.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    account_subsequence_method = fields.Selection(
+        related="company_id.account_subsequence_method",
+        readonly=False,
+    )
