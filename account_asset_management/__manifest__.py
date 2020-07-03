6fe8e004f6d85bf652e6a6b1f09c72c8747b9f4d
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-07-03 16:11:20 +0200

    [IMP] account_asset_management: Add chatter entry in invoice for generated asset

--- account_asset_management/__manifest__.py
+++ account_asset_management/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Assets Management",
-    "version": "13.0.1.0.4",
+    "version": "13.0.1.1.0",
     "license": "AGPL-3",
     "depends": ["account"],
     "excludes": ["account_asset"],
