4c0f16ee6206a537f2bb430266f77f4a4297caa1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-07-22 11:37:43 +0200

    [MIG] account_credit_control: Migration script

---
+++ account_credit_control/migrations/11.0.1.0.0/pre-migration.py
@@ -0,0 +1,21 @@
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.rename_tables(
+        env.cr, [
+            ("credit_control_policy_credit_control_run_rel",
+             "credit_run_policy_rel"),
+        ]
+    )
+    openupgrade.rename_columns(
+        env.cr, {
+            "credit_run_policy_rel": [
+                ("credit_control_run_id", "run_id,"),
+                ("credit_control_policy_id", "policy_id"),
+            ]
+        }
+    )
