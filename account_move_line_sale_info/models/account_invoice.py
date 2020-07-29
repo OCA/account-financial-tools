fb56e3dffb7e5ebbee269f1b5a8adfdac50ec343
AaronHForgeFlow <ahenriquez@eficent.com>
2020-07-29 17:26:15 +0200

    [IMP] : black, isort, prettier

--- account_move_line_sale_info/models/account_invoice.py
+++ account_move_line_sale_info/models/account_invoice.py
@@ -6,25 +6,25 @@ from odoo import api, models
 
 class AccountInvoice(models.Model):
 
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     @api.model
     def invoice_line_move_line_get(self):
         res = super(AccountInvoice, self).invoice_line_move_line_get()
 
-        invoice_line_model = self.env['account.invoice.line']
+        invoice_line_model = self.env["account.invoice.line"]
         for move_line_dict in res:
-            if 'invl_id' in move_line_dict:
-                line = invoice_line_model.browse(move_line_dict['invl_id'])
+            if "invl_id" in move_line_dict:
+                line = invoice_line_model.browse(move_line_dict["invl_id"])
                 if line.sale_line_ids and len(line.sale_line_ids) == 1:
-                    move_line_dict['sale_line_id'] = line.sale_line_ids[0].id
+                    move_line_dict["sale_line_id"] = line.sale_line_ids[0].id
         return res
 
     @api.model
     def line_get_convert(self, line, part):
         res = super(AccountInvoice, self).line_get_convert(line, part)
-        if line.get('sale_line_id', False):
-            res['sale_line_id'] = line.get('sale_line_id')
+        if line.get("sale_line_id", False):
+            res["sale_line_id"] = line.get("sale_line_id")
         return res
 
     @api.model
@@ -35,5 +35,5 @@ class AccountInvoice(models.Model):
         res = super()._anglo_saxon_sale_move_lines(i_line)
         for aml in res:
             if i_line.sale_line_ids and len(i_line.sale_line_ids) == 1:
-                aml['sale_line_id'] = i_line.sale_line_ids[0].id
+                aml["sale_line_id"] = i_line.sale_line_ids[0].id
         return res
