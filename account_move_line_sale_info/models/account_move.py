2649263517ce8f68bb0b7a1eb616a0145ca4d71f
AaronHForgeFlow <ahenriquez@eficent.com>
2020-07-29 17:26:15 +0200

    [IMP] : black, isort, prettier

--- account_move_line_sale_info/models/account_move.py
+++ account_move_line_sale_info/models/account_move.py
@@ -6,18 +6,20 @@ from odoo import fields, models
 
 class AccountMoveLine(models.Model):
 
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     sale_line_id = fields.Many2one(
-        comodel_name='sale.order.line',
-        string='Sale Order Line',
-        ondelete='set null', index=True,
+        comodel_name="sale.order.line",
+        string="Sale Order Line",
+        ondelete="set null",
+        index=True,
     )
 
     sale_id = fields.Many2one(
-        comodel_name='sale.order',
-        related='sale_line_id.order_id',
-        string='Sales Order',
-        ondelete='set null',
-        store=True, index=True,
+        comodel_name="sale.order",
+        related="sale_line_id.order_id",
+        string="Sales Order",
+        ondelete="set null",
+        store=True,
+        index=True,
     )
