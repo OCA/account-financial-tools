faf175e9f06e3d92a37ebc19e66d67be016e3cd7
AaronHForgeFlow <ahenriquez@eficent.com>
2020-07-29 17:26:15 +0200

    [IMP] : black, isort, prettier

--- account_move_line_sale_info/__manifest__.py
+++ account_move_line_sale_info/__manifest__.py
@@ -5,16 +5,12 @@
     "name": "Account Move Line Sale Info",
     "summary": "Introduces the purchase order line to the journal items",
     "version": "11.0.1.0.0",
-    "author": "ForgeFlow S.L., "
-              "Odoo Community Association (OCA)",
+    "author": "ForgeFlow S.L., " "Odoo Community Association (OCA)",
     "website": "http://www.github.com/OCA/account-financial-tools",
     "category": "Generic",
     "depends": ["account_move_line_stock_info", "sale"],
     "license": "AGPL-3",
-    "data": [
-        "security/account_security.xml",
-        "views/account_move_view.xml",
-    ],
-    'installable': True,
-    'post_init_hook': 'post_init_hook',
+    "data": ["security/account_security.xml", "views/account_move_view.xml",],
+    "installable": True,
+    "post_init_hook": "post_init_hook",
 }
