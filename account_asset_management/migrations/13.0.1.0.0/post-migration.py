1dd71cb706d45ef28cc13ad9dcb92bc10679bc36
fromera <fromera@astrohotel.es>
2020-07-31 14:56:45 +0200

    [FIX] version
    [FIX] new line
    
    [FIX] new line
    
    [FIX] without openupgrade
    
    [FIX] without openupgrade
    
    [FIX] without openupgrade
    
    [FIX] without openupgrade
    
    [FIX] without openupgrade

--- account_asset_management/migrations/13.0.1.0.0/post-migration.py
+++
@@ -1,8 +0,0 @@
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from openupgradelib import openupgrade  # pylint: disable=W7936
-
-@openupgrade.migrate()
-def migrate(env, version):
-    openupgrade.load_data(
-        env.cr, "account_asset_management", "migrations/13.0.1.0.0/noupdate_changes.xml"
-    )
