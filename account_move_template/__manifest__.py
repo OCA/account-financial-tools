77b1df4d0e9ee769f3bceb730b877bde41b35d0c
ps-tubtim <pimolnats@ecosoft.co.th>
2020-08-04 17:14:58 +0700

    [IMP] account_move_template: black, isort, prettier

--- account_move_template/__manifest__.py
+++ account_move_template/__manifest__.py
@@ -4,22 +4,20 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': "Account Move Template",
-    'version': '12.0.1.0.0',
-    'category': 'Accounting',
-    'summary': "Templates for recurring Journal Entries",
-    'author': "Agile Business Group, Aurium Technologies, Vauxoo, Eficent, "
-              "Akretion, Odoo Community Association (OCA)",
-    'website': 'https://github.com/OCA/account-financial-tools',
-    'license': 'AGPL-3',
-    'depends': [
-        'account',
+    "name": "Account Move Template",
+    "version": "13.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Templates for recurring Journal Entries",
+    "author": "Agile Business Group, Aurium Technologies, Vauxoo, Eficent, "
+    "Akretion, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "security/account_move_template_security.xml",
+        "security/ir.model.access.csv",
+        "wizard/account_move_template_run_view.xml",
+        "view/account_move_template.xml",
     ],
-    'data': [
-        'security/account_move_template_security.xml',
-        'security/ir.model.access.csv',
-        'wizard/account_move_template_run_view.xml',
-        'view/account_move_template.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
