eb628e8fa887c51ec1132b57f86e23b00f5b1c1b
Saran440 <saranl@ecosoft.co.th>
2020-08-06 17:33:27 +0700

    [FIX] account_asset_management - Allow editing salvage value

--- account_asset_management/__manifest__.py
+++ account_asset_management/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Assets Management",
-    "version": "13.0.1.2.0",
+    "version": "13.0.1.2.1",
     "license": "AGPL-3",
     "depends": ["account"],
     "excludes": ["account_asset"],
