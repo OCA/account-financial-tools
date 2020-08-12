17f7e933928023df0fdca659aba10bb5cd431d4f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-08-12 17:47:23 +0200

    [IMP] account_asset_management: Refinements

--- account_asset_management/__manifest__.py
+++ account_asset_management/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Assets Management",
-    "version": "13.0.1.2.1",
+    "version": "13.0.1.3.0",
     "license": "AGPL-3",
     "depends": ["account"],
     "excludes": ["account_asset"],
