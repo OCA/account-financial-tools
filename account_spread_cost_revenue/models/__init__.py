33a5b57d5438db61eb95968359466c1d2b81bb22
Andrea <a.stirpe@onestein.nl>
2020-09-07 14:31:18 +0200

    [13.0][MIG] account_spread_cost_revenue

--- account_spread_cost_revenue/models/__init__.py
+++ account_spread_cost_revenue/models/__init__.py
@@ -1,7 +1,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import account_invoice
-from . import account_invoice_line
+from . import account_move
+from . import account_move_line
 from . import account_spread_line
 from . import account_spread
 from . import account_spread_template
