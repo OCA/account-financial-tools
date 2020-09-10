efc33461381d1028cc6ce7944e6068f955ddecd1
Bhavesh Odedra <bodedra@opensourceintegrators.com>
2020-09-10 15:14:32 -0700

    [10.0][ADD] account_move_chatter backport from 12.0

--- account_move_chatter/__manifest__.py
+++ account_move_chatter/__manifest__.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Chatter on journal entries',
-    'version': '12.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Invoices & Payments',
     'license': 'AGPL-3',
     'author': 'Tecnativa, '
--- account_move_chatter/models/account_move.py
+++ account_move_chatter/models/account_move.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import models, fields
 
 
 class AccountMove(models.Model):
-    _inherit = ["account.move", "mail.thread", "mail.activity.mixin"]
+    _inherit = ["account.move", "mail.thread"]
     _name = "account.move"
+
+    state = fields.Selection(track_visibility='always')
