3b649fa2cf8705fcacc755393d833b458faaf3f2
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2020-10-07 18:28:42 -0400

    [IMP] account_journal_lock_date: black, isort, prettier

--- account_journal_lock_date/models/account_journal.py
+++ account_journal_lock_date/models/account_journal.py
@@ -6,18 +6,18 @@ from odoo import fields, models
 
 class AccountJournal(models.Model):
 
-    _inherit = 'account.journal'
+    _inherit = "account.journal"
 
     fiscalyear_lock_date = fields.Date(
         string="Lock Date",
         help="No users, including Advisers, can edit accounts prior "
-             "to and inclusive of this date for this journal. Use it "
-             "for fiscal year locking for this journal, for example."
+        "to and inclusive of this date for this journal. Use it "
+        "for fiscal year locking for this journal, for example.",
     )
     period_lock_date = fields.Date(
         string="Lock Date for Non-Advisers",
         help="Only users with the 'Adviser' role can edit accounts "
-             "prior to and inclusive of this date for this journal. "
-             "Use it for period locking inside an open fiscal year "
-             "for this journal, for example."
+        "prior to and inclusive of this date for this journal. "
+        "Use it for period locking inside an open fiscal year "
+        "for this journal, for example.",
     )
