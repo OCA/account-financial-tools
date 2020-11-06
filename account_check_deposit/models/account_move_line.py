96fc7b863bbf2aa69586d14f7959663a79e03608
Saran440 <saranl@ecosoft.co.th>
2020-11-06 17:09:53 +0700

    [IMP] account_check_deposit: black, isort, prettier

--- account_check_deposit/models/account_move_line.py
+++ account_check_deposit/models/account_move_line.py
@@ -13,5 +13,7 @@ class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
     check_deposit_id = fields.Many2one(
-        comodel_name="account.check.deposit", string="Check Deposit", copy=False,
+        comodel_name="account.check.deposit",
+        string="Check Deposit",
+        copy=False,
     )
