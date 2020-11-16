18020826c31e6a5a84e1f96424ad20c7bb507638
remihb <remi@hobo.live>
2020-11-17 00:09:33 +0100

    [MIG] account_fiscal_month: Migration to 13.0

--- account_fiscal_month/models/res_company.py
+++ account_fiscal_month/models/res_company.py
@@ -1,14 +1,13 @@
 # Copyright 2017 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import models
 
 
 class ResCompany(models.Model):
 
     _inherit = "res.company"
 
-    @api.multi
     def find_daterange_fm(self, date_str):
         self.ensure_one()
         fm_id = self.env.ref("account_fiscal_month.date_range_fiscal_month")
