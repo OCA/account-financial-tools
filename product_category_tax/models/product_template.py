f58e9f0414c708a7e752c7ec08b629540c05e5ab
AaronHForgeFlow <ahenriquez@eficent.com>
2020-11-16 18:54:53 +0100

    [ADD]product_category_tax

---
+++ product_category_tax/models/product_template.py
@@ -0,0 +1,18 @@
+# Copyright 2020 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    @api.onchange("categ_id")
+    def onchange_categ_id(self):
+        if self.categ_id:
+            self.set_tax_from_category()
+
+    def set_tax_from_category(self):
+        self.ensure_one()
+        self.taxes_id = [(6, 0, self.categ_id.taxes_id.ids)]
+        self.supplier_taxes_id = [(6, 0, self.categ_id.supplier_taxes_id.ids)]
---
+++ product_category_tax/readme/USAGE.rst
@@ -0,0 +1,8 @@
+In case all the products within the category will use the same tax setup:
+
+#. Select the taxes in the product category form
+#. Click on "Apply to Products"
+
+In case a specific product need a different tax configuration you have to
+either to include it in a subcategory or adjust its taxes after applying those
+to the others
