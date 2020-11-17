2856af8c9db05a04c2ca11d0440ef141c86ade07
remihb <remi@hobo.live>
2020-11-17 02:38:56 +0100

    [IMP] account_move_fiscal_month: black, isort, prettier

--- account_move_fiscal_month/models/account_move.py
+++ account_move_fiscal_month/models/account_move.py
@@ -7,39 +7,45 @@ from odoo.osv import expression
 
 class AccountMove(models.Model):
 
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     date_range_fm_id = fields.Many2one(
-        comodel_name='date.range', string="Fiscal month",
-        compute='_compute_date_range_fm', search='_search_date_range_fm',
+        comodel_name="date.range",
+        string="Fiscal month",
+        compute="_compute_date_range_fm",
+        search="_search_date_range_fm",
     )
 
     @api.multi
-    @api.depends('date', 'company_id')
+    @api.depends("date", "company_id")
     def _compute_date_range_fm(self):
         for rec in self:
             date = rec.date
             company = rec.company_id
-            rec.date_range_fm_id =\
-                company and company.find_daterange_fm(date) or False
+            rec.date_range_fm_id = company and company.find_daterange_fm(date) or False
 
     @api.model
     def _search_date_range_fm(self, operator, value):
-        if operator in ('=', '!=', 'in', 'not in'):
-            date_range_domain = [('id', operator, value)]
+        if operator in ("=", "!=", "in", "not in"):
+            date_range_domain = [("id", operator, value)]
         else:
-            date_range_domain = [('name', operator, value)]
+            date_range_domain = [("name", operator, value)]
 
-        date_ranges = self.env['date.range'].search(date_range_domain)
+        date_ranges = self.env["date.range"].search(date_range_domain)
         domain = []
         for date_range in date_ranges:
-            domain = expression.OR([domain, [
-                '&',
-                '&',
-                ('date', '>=', date_range.date_start),
-                ('date', '<=', date_range.date_end),
-                '|',
-                ('company_id', '=', False),
-                ('company_id', '=', date_range.company_id.id),
-            ]])
+            domain = expression.OR(
+                [
+                    domain,
+                    [
+                        "&",
+                        "&",
+                        ("date", ">=", date_range.date_start),
+                        ("date", "<=", date_range.date_end),
+                        "|",
+                        ("company_id", "=", False),
+                        ("company_id", "=", date_range.company_id.id),
+                    ],
+                ]
+            )
         return domain
