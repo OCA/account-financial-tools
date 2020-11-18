7d6193f60775f1dfd233ad592e736e7a79eebc59
AaronHForgeFlow <ahenriquez@eficent.com>
2020-11-18 18:05:58 +0100

    [IMP] product_category_tax * add a boleean to exclude specific products to be updated

--- product_category_tax/models/product_template.py
+++ product_category_tax/models/product_template.py
@@ -1,11 +1,13 @@
 # Copyright 2020 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
+
+    taxes_updeatable_from_category = fields.Boolean(default=True)
 
     @api.onchange("categ_id")
     def onchange_categ_id(self):
