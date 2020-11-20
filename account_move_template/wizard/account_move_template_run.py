f36a6aab1f621cdf3c79228235247e45bdb03e35
Kitti U <kittiu@gmail.com>
2020-11-20 10:59:59 +0700

    [13.0][IMP] account_move_template, add options to overwirte output lines

--- account_move_template/wizard/account_move_template_run.py
+++ account_move_template/wizard/account_move_template_run.py
@@ -1,8 +1,9 @@
 # Copyright 2015-2019 See manifest
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from ast import literal_eval
 
 from odoo import _, fields, models
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tools import float_is_zero
 
 
@@ -15,7 +16,7 @@ class AccountMoveTemplateRun(models.TransientModel):
         "res.company",
         required=True,
         readonly=True,
-        default=lambda self: self.env["res.company"]._company_default_get(),
+        default=lambda self: self.env.company,
     )
     partner_id = fields.Many2one(
         "res.partner",
@@ -32,6 +33,13 @@ class AccountMoveTemplateRun(models.TransientModel):
         [("select_template", "Select Template"), ("set_lines", "Set Lines")],
         readonly=True,
         default="select_template",
+    )
+    overwrite = fields.Text(
+        help="""
+Valid dictionary to overwrite template lines:
+{'L1': {'partner_id': 1, 'amount': 100, 'name': 'some label'},
+ 'L2': {'partner_id': 2, 'amount': 200, 'name': 'some label 2'}, }
+        """
     )
 
     def _prepare_wizard_line(self, tmpl_line):
@@ -57,6 +65,8 @@ class AccountMoveTemplateRun(models.TransientModel):
     # STEP 1
     def load_lines(self):
         self.ensure_one()
+        # Verify and get overwrite dict
+        overwrite_vals = self._get_overwrite_vals()
         amtlro = self.env["account.move.template.line.run"]
         if self.company_id != self.template_id.company_id:
             raise UserError(
@@ -86,7 +96,74 @@ class AccountMoveTemplateRun(models.TransientModel):
         action = self.env.ref("account_move_template.account_move_template_run_action")
         result = action.read()[0]
         result.update({"res_id": self.id, "context": self.env.context})
+
+        # Overwrite self.line_ids to show overwrite values
+        self._overwrite_line(overwrite_vals)
+        # Pass context furtner to generate_move function, only readonly field
+        for key in overwrite_vals.keys():
+            overwrite_vals[key].pop("amount", None)
+        context = result.get("context", {}).copy()
+        context.update({"overwrite": overwrite_vals})
+        result["context"] = context
         return result
+
+    def _get_valid_keys(self):
+        return ["partner_id", "amount", "name", "date_maturity"]
+
+    def _get_overwrite_vals(self):
+        """ valid_dict = {
+                'L1': {'partner_id': 1, 'amount': 10},
+                'L2': {'partner_id': 2, 'amount': 20},
+            }
+        """
+        self.ensure_one()
+        valid_keys = self._get_valid_keys()
+        overwrite_vals = self.overwrite or "{}"
+        try:
+            overwrite_vals = literal_eval(overwrite_vals)
+            assert isinstance(overwrite_vals, dict)
+        except (SyntaxError, ValueError, AssertionError):
+            raise ValidationError(_("Overwrite value must be a valid python dict"))
+        # First level keys must be L1, L2, ...
+        keys = overwrite_vals.keys()
+        if list(filter(lambda x: x[:1] != "L" or not x[1:].isdigit(), keys)):
+            raise ValidationError(_("Keys must be line sequence, i..e, L1, L2, ..."))
+        # Second level keys must be a valid keys
+        try:
+            if dict(
+                filter(lambda x: set(overwrite_vals[x].keys()) - set(valid_keys), keys)
+            ):
+                raise ValidationError(
+                    _("Valid fields to overwrite are %s") % valid_keys
+                )
+        except ValidationError as e:
+            raise e
+        except Exception as e:
+            msg = """
+    valid_dict = {
+        'L1': {'partner_id': 1, 'amount': 10},
+        'L2': {'partner_id': 2, 'amount': 20},
+    }
+            """
+            raise ValidationError(_("Invalid dictionary: {}\n{}".format(e, msg)))
+        return overwrite_vals
+
+    def _safe_vals(self, model, vals):
+        obj = self.env[model]
+        copy_vals = vals.copy()
+        invalid_keys = list(
+            set(list(vals.keys())) - set(list(dict(obj._fields).keys()))
+        )
+        for key in invalid_keys:
+            copy_vals.pop(key)
+        return copy_vals
+
+    def _overwrite_line(self, overwrite_vals):
+        self.ensure_one()
+        for line in self.line_ids:
+            vals = overwrite_vals.get("L{}".format(line.sequence), {})
+            safe_vals = self._safe_vals(line._name, vals)
+            line.write(safe_vals)
 
     # STEP 2
     def generate_move(self):
@@ -138,7 +215,7 @@ class AccountMoveTemplateRun(models.TransientModel):
     def _prepare_move_line(self, line, amount):
         date_maturity = False
         if line.payment_term_id:
-            pterm_list = line.payment_term_id.compute(value=1, date_ref=self.date)[0]
+            pterm_list = line.payment_term_id.compute(value=1, date_ref=self.date)
             date_maturity = max(l[0] for l in pterm_list)
         debit = line.move_line_type == "dr"
         values = {
@@ -165,7 +242,23 @@ class AccountMoveTemplateRun(models.TransientModel):
             values["tag_ids"] = [(6, 0, atrl_ids.mapped("tag_ids").ids)]
         if line.tax_repartition_line_id:
             values["tag_ids"] = [(6, 0, line.tax_repartition_line_id.tag_ids.ids)]
+        # With overwrite options
+        overwrite = self._context.get("overwrite", {})
+        move_line_vals = overwrite.get("L{}".format(line.sequence), {})
+        values.update(move_line_vals)
+        # Use optional account, when amount is negative
+        self._update_account_on_negative(line, values)
         return values
+
+    def _update_account_on_negative(self, line, vals):
+        if not line.opt_account_id:
+            return
+        for key in ["debit", "credit"]:
+            if vals[key] < 0:
+                ikey = (key == "debit") and "credit" or "debit"
+                vals["account_id"] = line.opt_account_id.id
+                vals[ikey] = abs(vals[key])
+                vals[key] = 0
 
 
 class AccountMoveTemplateLineRun(models.TransientModel):
