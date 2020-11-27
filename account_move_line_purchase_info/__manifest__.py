0f6f4a3a7762892c1e1747238a4b192308170cc4
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2020-11-27 09:42:07 +0100

    backport changes from 12.0

--- account_move_line_purchase_info/__manifest__.py
+++ account_move_line_purchase_info/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# Copyright 2020 ForgeFlow, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account Move Line Purchase Info",
     "summary": "Introduces the purchase order line to the journal items",
     "version": "10.0.1.0.0",
-    "author": "Eficent, "
+    "author": "ForgeFlow, "
               "Odoo Community Association (OCA)",
     "website": "http://www.github.com/OCA/account-financial-tools",
     "category": "Generic",
--- account_move_line_purchase_info/models/__init__.py
+++ account_move_line_purchase_info/models/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L. (www.eficent.com)
+# Copyright 2020 ForgeFlow, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import account_move
 from . import account_invoice
 from . import stock_move
+from . import purchase_order_line
