ba87c5a1f69599c0206701b246777a4f1d263135
Francisco Ivan Anton Prieto <ozono@ozonomultimedia.com>
2020-12-16 18:16:14 +0100

    [FIX] account_reconciliation_widget: solve flake8 complexity error

--- account_reconciliation_widget/models/account_bank_statement.py
+++ account_reconciliation_widget/models/account_bank_statement.py
@@ -183,65 +183,10 @@ class AccountBankStatementLine(models.Model):
         # (eg. invoice), in which case we reconcile the existing and the new
         # move lines together, or being a write-off.
         if counterpart_aml_dicts or new_aml_dicts:
+            counterpart_moves = self._create_counterpart_and_new_aml(
+                counterpart_moves, counterpart_aml_dicts, new_aml_dicts
+            )
 
-            # Delete previous move_lines
-            self.move_id.line_ids.with_context(force_delete=True).unlink()
-
-            # Create liquidity line
-            liquidity_aml_dict = self._prepare_liquidity_move_line_vals()
-            aml_obj.with_context(check_move_validity=False).create(liquidity_aml_dict)
-
-            self.sequence = self.statement_id.line_ids.ids.index(self.id) + 1
-            counterpart_moves = counterpart_moves | self.move_id
-
-            # Complete dicts to create both counterpart move lines and write-offs
-            to_create = counterpart_aml_dicts + new_aml_dicts
-            date = self.date or fields.Date.today()
-            for aml_dict in to_create:
-                aml_dict["move_id"] = self.move_id.id
-                aml_dict["partner_id"] = self.partner_id.id
-                aml_dict["statement_line_id"] = self.id
-                self._prepare_move_line_for_currency(aml_dict, date)
-
-            # Create write-offs
-            for aml_dict in new_aml_dicts:
-                aml_obj.with_context(check_move_validity=False).create(aml_dict)
-
-            # Create counterpart move lines and reconcile them
-            aml_to_reconcile = []
-            for aml_dict in counterpart_aml_dicts:
-                if not aml_dict["move_line"].statement_line_id:
-                    aml_dict["move_line"].write({"statement_line_id": self.id})
-                if aml_dict["move_line"].partner_id.id:
-                    aml_dict["partner_id"] = aml_dict["move_line"].partner_id.id
-                aml_dict["account_id"] = aml_dict["move_line"].account_id.id
-
-                counterpart_move_line = aml_dict.pop("move_line")
-                new_aml = aml_obj.with_context(check_move_validity=False).create(
-                    aml_dict
-                )
-
-                aml_to_reconcile.append((new_aml, counterpart_move_line))
-
-            # Post to allow reconcile
-            self.move_id.with_context(
-                skip_account_move_synchronization=True
-            ).action_post()
-
-            # Reconcile new lines with counterpart
-            for new_aml, counterpart_move_line in aml_to_reconcile:
-                (new_aml | counterpart_move_line).reconcile()
-
-                self._check_invoice_state(counterpart_move_line.move_id)
-
-            # Needs to be called manually as lines were created 1 by 1
-            self.move_id.update_lines_tax_exigibility()
-            self.move_id.with_context(
-                skip_account_move_synchronization=True
-            ).action_post()
-            # record the move name on the statement line to be able to retrieve
-            # it in case of unreconciliation
-            self.write({"move_name": self.move_id.name})
         elif self.move_name:
             raise UserError(
                 _(
@@ -261,6 +206,67 @@ class AccountBankStatementLine(models.Model):
             self.partner_bank_id = self._find_or_create_bank_account()
 
         counterpart_moves._check_balanced()
+        return counterpart_moves
+
+    def _create_counterpart_and_new_aml(
+        self, counterpart_moves, counterpart_aml_dicts, new_aml_dicts
+    ):
+
+        aml_obj = self.env["account.move.line"]
+
+        # Delete previous move_lines
+        self.move_id.line_ids.with_context(force_delete=True).unlink()
+
+        # Create liquidity line
+        liquidity_aml_dict = self._prepare_liquidity_move_line_vals()
+        aml_obj.with_context(check_move_validity=False).create(liquidity_aml_dict)
+
+        self.sequence = self.statement_id.line_ids.ids.index(self.id) + 1
+        counterpart_moves = counterpart_moves | self.move_id
+
+        # Complete dicts to create both counterpart move lines and write-offs
+        to_create = counterpart_aml_dicts + new_aml_dicts
+        date = self.date or fields.Date.today()
+        for aml_dict in to_create:
+            aml_dict["move_id"] = self.move_id.id
+            aml_dict["partner_id"] = self.partner_id.id
+            aml_dict["statement_line_id"] = self.id
+            self._prepare_move_line_for_currency(aml_dict, date)
+
+        # Create write-offs
+        for aml_dict in new_aml_dicts:
+            aml_obj.with_context(check_move_validity=False).create(aml_dict)
+
+        # Create counterpart move lines and reconcile them
+        aml_to_reconcile = []
+        for aml_dict in counterpart_aml_dicts:
+            if not aml_dict["move_line"].statement_line_id:
+                aml_dict["move_line"].write({"statement_line_id": self.id})
+            if aml_dict["move_line"].partner_id.id:
+                aml_dict["partner_id"] = aml_dict["move_line"].partner_id.id
+            aml_dict["account_id"] = aml_dict["move_line"].account_id.id
+
+            counterpart_move_line = aml_dict.pop("move_line")
+            new_aml = aml_obj.with_context(check_move_validity=False).create(aml_dict)
+
+            aml_to_reconcile.append((new_aml, counterpart_move_line))
+
+        # Post to allow reconcile
+        self.move_id.with_context(skip_account_move_synchronization=True).action_post()
+
+        # Reconcile new lines with counterpart
+        for new_aml, counterpart_move_line in aml_to_reconcile:
+            (new_aml | counterpart_move_line).reconcile()
+
+            self._check_invoice_state(counterpart_move_line.move_id)
+
+        # Needs to be called manually as lines were created 1 by 1
+        self.move_id.update_lines_tax_exigibility()
+        self.move_id.with_context(skip_account_move_synchronization=True).action_post()
+        # record the move name on the statement line to be able to retrieve
+        # it in case of unreconciliation
+        self.write({"move_name": self.move_id.name})
+
         return counterpart_moves
 
     def _prepare_move_line_for_currency(self, aml_dict, date):
