bba0297a23722068967d45a9708a9e63783f2227
Stefan Rijnhart <stefan@opener.am>
2021-02-03 20:22:01 +0100

    [FIX] account_asset_management: don't set class attributes in reporting
    Also, actually generate report in test plus other tests to make it to the
    codecov target

--- account_asset_management/tests/test_asset_management_xls.py
+++ account_asset_management/tests/test_asset_management_xls.py
@@ -1,11 +1,13 @@
 # Copyright 2009-2019 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import time
 
 from odoo import fields
-from odoo.tests.common import SavepointCase
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-class TestAssetManagementXls(SavepointCase):
+class TestAssetManagementXls(AccountTestInvoicingCommon):
     @classmethod
     def setUpClass(cls):
         super(TestAssetManagementXls, cls).setUpClass()
@@ -14,11 +16,54 @@ class TestAssetManagementXls(SavepointCase):
         cls.xls_report_name = "{}.asset_report_xls".format(module)
         cls.wiz_model = cls.env["wiz.account.asset.report"]
         cls.company = cls.env.ref("base.main_company")
-        asset_group_id = cls.wiz_model._default_asset_group_id()
+        # Ensure we have something to report on
+        group_fa = cls.env["account.asset.group"].create(
+            {
+                "name": "Fixed Assets",
+                "code": "FA",
+            }
+        )
+        group_tfa = cls.env["account.asset.group"].create(
+            {
+                "name": "Tangible Fixed Assets",
+                "code": "TFA",
+                "parent_id": group_fa.id,
+            }
+        )
+        ict3Y = cls.env["account.asset.profile"].create(
+            {
+                "account_expense_depreciation_id": cls.company_data[
+                    "default_account_expense"
+                ].id,
+                "account_asset_id": cls.company_data["default_account_assets"].id,
+                "account_depreciation_id": cls.company_data[
+                    "default_account_assets"
+                ].id,
+                "group_ids": [(6, 0, group_tfa.ids)],
+                "journal_id": cls.company_data["default_journal_purchase"].id,
+                "name": "Hardware - 3 Years",
+                "method_time": "year",
+                "method_number": 3,
+                "method_period": "year",
+            }
+        )
+        cls.env["account.asset"].create(
+            {
+                "state": "draft",
+                "method_time": "year",
+                "method_number": 3,
+                "method_period": "year",
+                "name": "Laptop",
+                "code": "PI00101",
+                "purchase_value": 1500.0,
+                "profile_id": ict3Y.id,
+                "date_start": time.strftime("%Y-01-01"),
+            }
+        ).validate()
         fy_dates = cls.company.compute_fiscalyear_dates(fields.date.today())
 
         wiz_vals = {
-            "asset_group_id": asset_group_id,
+            "asset_group_id": group_fa.id,
             "date_from": fy_dates["date_from"],
             "date_to": fy_dates["date_to"],
         }
@@ -26,7 +71,7 @@ class TestAssetManagementXls(SavepointCase):
         cls.report_action = cls.xls_report.xls_export()
 
     def test_01_action_xls(self):
-        """ Check report XLS action """
+        """ Check report XLS action and generate report """
         self.assertGreaterEqual(
             self.report_action.items(),
             {
@@ -35,3 +80,7 @@ class TestAssetManagementXls(SavepointCase):
                 "report_name": self.xls_report_name,
             }.items(),
         )
+        model = self.env["report.%s" % self.report_action["report_name"]].with_context(
+            active_model=self.xls_report._name, **self.report_action["context"]
+        )
+        model.create_xlsx_report(self.xls_report.ids, data=self.report_action["data"])
