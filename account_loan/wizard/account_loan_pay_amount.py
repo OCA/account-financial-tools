bf0c9feccae069153e2597d8ef77355eea8b4fd1
Alba Riera <alba.riera@creublanca.es>
2021-02-23 11:00:15 +0100

    [IMP] account_loan: black,isort,prettier

--- account_loan/wizard/account_loan_pay_amount.py
+++ account_loan/wizard/account_loan_pay_amount.py
@@ -1,54 +1,42 @@
 # Copyright 2018 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class AccountLoan(models.TransientModel):
-    _name = 'account.loan.pay.amount'
-    _description = 'Loan pay amount'
+    _name = "account.loan.pay.amount"
+    _description = "Loan pay amount"
 
-    loan_id = fields.Many2one(
-        'account.loan',
-        required=True,
-        readonly=True,
-    )
+    loan_id = fields.Many2one("account.loan", required=True, readonly=True,)
     currency_id = fields.Many2one(
-        'res.currency',
-        related='loan_id.currency_id',
-        readonly=True
+        "res.currency", related="loan_id.currency_id", readonly=True
     )
-    cancel_loan = fields.Boolean(
-        default=False,
-    )
+    cancel_loan = fields.Boolean(default=False,)
     date = fields.Date(required=True, default=fields.Date.today())
     amount = fields.Monetary(
-        currency_field='currency_id',
-        string='Amount to reduce from Principal',
+        currency_field="currency_id", string="Amount to reduce from Principal",
     )
-    fees = fields.Monetary(
-        currency_field='currency_id',
-        string='Bank fees'
-    )
+    fees = fields.Monetary(currency_field="currency_id", string="Bank fees")
 
-    @api.onchange('cancel_loan')
+    @api.onchange("cancel_loan")
     def _onchange_cancel_loan(self):
         if self.cancel_loan:
-            self.amount = max(self.loan_id.line_ids.filtered(
-                lambda r: not r.move_ids and not r.invoice_ids).mapped(
-                    'pending_principal_amount'
-                )
+            self.amount = max(
+                self.loan_id.line_ids.filtered(
+                    lambda r: not r.move_ids and not r.invoice_ids
+                ).mapped("pending_principal_amount")
             )
 
     def new_line_vals(self, sequence):
         return {
-            'loan_id': self.loan_id.id,
-            'sequence': sequence,
-            'payment_amount': self.amount + self.fees,
-            'rate': 0,
-            'interests_amount': self.fees,
-            'date': self.date,
+            "loan_id": self.loan_id.id,
+            "sequence": sequence,
+            "payment_amount": self.amount + self.fees,
+            "rate": 0,
+            "interests_amount": self.fees,
+            "date": self.date,
         }
 
     @api.multi
@@ -56,41 +44,40 @@ class AccountLoan(models.TransientModel):
         self.ensure_one()
         if self.loan_id.is_leasing:
             if self.loan_id.line_ids.filtered(
-                    lambda r: r.date < self.date and not r.invoice_ids
+                lambda r: r.date < self.date and not r.invoice_ids
             ):
-                raise UserError(_('Some invoices are not created'))
+                raise UserError(_("Some invoices are not created"))
             if self.loan_id.line_ids.filtered(
-                    lambda r: r.date > self.date and r.invoice_ids
+                lambda r: r.date > self.date and r.invoice_ids
             ):
-                raise UserError(_('Some future invoices already exists'))
+                raise UserError(_("Some future invoices already exists"))
         if self.loan_id.line_ids.filtered(
-                lambda r: r.date < self.date and not r.move_ids
+            lambda r: r.date < self.date and not r.move_ids
         ):
-            raise UserError(_('Some moves are not created'))
-        if self.loan_id.line_ids.filtered(
-                lambda r: r.date > self.date and r.move_ids
-        ):
-            raise UserError(_('Some future moves already exists'))
-        lines = self.loan_id.line_ids.filtered(
-            lambda r: r.date > self.date).sorted('sequence', reverse=True)
-        sequence = min(lines.mapped('sequence'))
+            raise UserError(_("Some moves are not created"))
+        if self.loan_id.line_ids.filtered(lambda r: r.date > self.date and r.move_ids):
+            raise UserError(_("Some future moves already exists"))
+        lines = self.loan_id.line_ids.filtered(lambda r: r.date > self.date).sorted(
+            "sequence", reverse=True
+        )
+        sequence = min(lines.mapped("sequence"))
         for line in lines:
             line.sequence += 1
         old_line = lines.filtered(lambda r: r.sequence == sequence + 1)
         pending = old_line.pending_principal_amount
         if self.loan_id.currency_id.compare_amounts(self.amount, pending) == 1:
-            raise UserError(_('Amount cannot be bigger than debt'))
+            raise UserError(_("Amount cannot be bigger than debt"))
         if self.loan_id.currency_id.compare_amounts(self.amount, 0) <= 0:
-            raise UserError(_('Amount cannot be less than zero'))
+            raise UserError(_("Amount cannot be less than zero"))
         self.loan_id.periods += 1
         self.loan_id.fixed_periods = self.loan_id.periods - sequence
         self.loan_id.fixed_loan_amount = pending - self.amount
-        new_line = self.env['account.loan.line'].create(
-            self.new_line_vals(sequence))
+        new_line = self.env["account.loan.line"].create(self.new_line_vals(sequence))
         new_line.long_term_pending_principal_amount = (
-            old_line.long_term_pending_principal_amount)
+            old_line.long_term_pending_principal_amount
+        )
         amount = self.loan_id.loan_amount
-        for line in self.loan_id.line_ids.sorted('sequence'):
+        for line in self.loan_id.line_ids.sorted("sequence"):
             if line.move_ids:
                 amount = line.final_pending_principal_amount
             else:
@@ -102,5 +89,5 @@ class AccountLoan(models.TransientModel):
         if self.loan_id.long_term_loan_account_id:
             self.loan_id.check_long_term_principal_amount()
         if self.loan_id.currency_id.compare_amounts(pending, self.amount) == 0:
-            self.loan_id.write({'state': 'cancelled'})
+            self.loan_id.write({"state": "cancelled"})
         return new_line.view_process_values()
