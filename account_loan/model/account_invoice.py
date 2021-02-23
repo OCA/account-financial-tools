b3933656c6ec5b38b44df424c386de690e08bf12
Alba Riera <alba.riera@creublanca.es>
2021-02-23 11:00:15 +0100

    [IMP] account_loan: black,isort,prettier

--- account_loan/model/account_invoice.py
+++ account_loan/model/account_invoice.py
@@ -5,18 +5,13 @@ from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     loan_line_id = fields.Many2one(
-        'account.loan.line',
-        readonly=True,
-        ondelete='restrict',
+        "account.loan.line", readonly=True, ondelete="restrict",
     )
     loan_id = fields.Many2one(
-        'account.loan',
-        readonly=True,
-        store=True,
-        ondelete='restrict',
+        "account.loan", readonly=True, store=True, ondelete="restrict",
     )
 
     @api.multi
@@ -24,18 +19,27 @@ class AccountInvoice(models.Model):
         vals = super().finalize_invoice_move_lines(move_lines)
         if self.loan_line_id:
             ll = self.loan_line_id
-            if (
-                ll.long_term_loan_account_id and
-                ll.long_term_principal_amount != 0
-            ):
-                vals.append((0, 0, {
-                    'account_id': ll.loan_id.short_term_loan_account_id.id,
-                    'credit': ll.long_term_principal_amount,
-                    'debit': 0,
-                }))
-                vals.append((0, 0, {
-                    'account_id': ll.long_term_loan_account_id.id,
-                    'credit': 0,
-                    'debit': ll.long_term_principal_amount,
-                }))
+            if ll.long_term_loan_account_id and ll.long_term_principal_amount != 0:
+                vals.append(
+                    (
+                        0,
+                        0,
+                        {
+                            "account_id": ll.loan_id.short_term_loan_account_id.id,
+                            "credit": ll.long_term_principal_amount,
+                            "debit": 0,
+                        },
+                    )
+                )
+                vals.append(
+                    (
+                        0,
+                        0,
+                        {
+                            "account_id": ll.long_term_loan_account_id.id,
+                            "credit": 0,
+                            "debit": ll.long_term_principal_amount,
+                        },
+                    )
+                )
         return vals
