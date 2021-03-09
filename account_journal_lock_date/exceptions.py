eccf1b3c0fa5139902f0418a7ece9e053ea6bf63
Zar21 <ivanperezfita@gmail.com>
2021-03-09 16:53:35 +0100

    [MIG] account_journal_lock_date: Migration to 14.0

--- account_journal_lock_date/exceptions.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2017 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo.exceptions import UserError
-
-
-class JournalLockDateError(UserError):
-    pass
