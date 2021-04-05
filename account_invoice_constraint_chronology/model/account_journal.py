e767fc7be4b48a7b14d70e5aca626a00b6797bbb
Alexey Pelykh <alexey.pelykh@gmail.com>
2021-04-05 16:26:59 +0200

    [IMP] account_invoice_constraint_chronology: black, isort, prettier

--- account_invoice_constraint_chronology/model/account_journal.py
+++ account_invoice_constraint_chronology/model/account_journal.py
@@ -1,18 +1,16 @@
 # Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class AccountJournal(models.Model):
-    _inherit = ['account.journal']
+    _inherit = ["account.journal"]
 
-    check_chronology = fields.Boolean(
-        default=False,
-    )
+    check_chronology = fields.Boolean(default=False,)
 
-    @api.onchange('type')
+    @api.onchange("type")
     def _onchange_type(self):
         self.ensure_one()
-        if self.type not in ['sale', 'purchase']:
+        if self.type not in ["sale", "purchase"]:
             self.check_chronology = False
