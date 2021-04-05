705dd8f7851545631c9ae70e5e0d1b047927bdc5
Alexey Pelykh <alexey.pelykh@gmail.com>
2021-04-05 16:27:30 +0200

    [MIG] account_invoice_constraint_chronology: Migration to 13.0

--- account_invoice_constraint_chronology/__manifest__.py
+++ account_invoice_constraint_chronology/__manifest__.py
@@ -1,14 +1,13 @@
 # Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2021 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Invoice Constraint Chronology",
-    "version": "12.0.1.0.2",
-    "author": "Odoo Community Association (OCA), ACSONE SA/NV",
-    "maintainer": "ACSONE SA/NV",
-    "website": "https://github.com/OCA/account-financial-tools/tree/12.0/"
-    "account_invoice_constraint_chronology",
+    "version": "13.0.1.0.0",
+    "author": "Odoo Community Association (OCA), ACSONE SA/NV, CorporateHub",
+    "website": "https://github.com/OCA/account-financial-tools/",
     "license": "AGPL-3",
     "category": "Accounting",
     "depends": ["account"],
-    "data": ["view/account_view.xml"],
+    "data": ["view/account_journal.xml"],
 }
--- account_invoice_constraint_chronology/model/account_journal.py
+++ account_invoice_constraint_chronology/model/account_journal.py
@@ -1,16 +1,17 @@
 # Copyright 2015-2019 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2021 CorporateHub (https://corporatehub.eu)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
 
 
 class AccountJournal(models.Model):
-    _inherit = ["account.journal"]
+    _inherit = "account.journal"
 
-    check_chronology = fields.Boolean(default=False,)
+    check_chronology = fields.Boolean()
 
     @api.onchange("type")
     def _onchange_type(self):
-        self.ensure_one()
+        super()._onchange_type()
         if self.type not in ["sale", "purchase"]:
             self.check_chronology = False
