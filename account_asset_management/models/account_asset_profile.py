241aa3e5633a1d1e97e6710929466a8e0eb43fda
mreficent <miquel.raich@eficent.com>
2021-04-19 13:04:19 +0200

    [FIX] account_asset_management: method_time migration was incomplete

--- account_asset_management/models/account_asset_profile.py
+++ account_asset_management/models/account_asset_profile.py
@@ -99,7 +99,10 @@ class AccountAssetProfile(models.Model):
         help="Choose the method to use to compute the dates and "
              "number of depreciation lines.\n"
              "  * Number of Years: Specify the number of years "
-             "for the depreciation.\n")
+             "for the depreciation.\n"
+             "  * Number of Depreciations: Fix the number of "
+             "depreciation lines and the time between 2 depreciations.\n"
+    )
     days_calc = fields.Boolean(
         string='Calculate by days',
         default=False,
@@ -164,6 +167,7 @@ class AccountAssetProfile(models.Model):
         """
         return [
             ('year', _('Number of Years or end date')),
+            ('number', _('Number of Depreciations')),
         ]
 
     @api.multi
