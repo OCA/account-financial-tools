1ae219a095d892922d3bdac1d0c1a53687fd4021
Alexis de Lattre <alexis.delattre@akretion.com>
2021-09-08 14:21:43 +0200

    sequence is now required on journals
    Add post-install script to create a sequence for all existing journals
    Update README accordingly

---
+++ account_move_name_sequence/post_install.py
@@ -0,0 +1,25 @@
+# Copyright 2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import SUPERUSER_ID, api
+
+
+def create_journal_sequences(cr, registry):
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        journals = env["account.journal"].with_context(active_test=False).search([])
+        for journal in journals:
+            vals = {}
+            journal_vals = {
+                "code": journal.code,
+                "name": journal.name,
+                "company_id": journal.company_id.id,
+            }
+            seq_vals = journal._prepare_sequence(journal_vals)
+            vals["sequence_id"] = env["ir.sequence"].create(seq_vals).id
+            if journal.type in ("sale", "purchase") and journal.refund_sequence:
+                rseq_vals = journal._prepare_sequence(journal_vals, refund=True)
+                vals["refund_sequence_id"] = env["ir.sequence"].create(rseq_vals).id
+            journal.write(vals)
+    return
