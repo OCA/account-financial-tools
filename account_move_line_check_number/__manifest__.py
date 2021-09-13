ebc8dbf36e3862ad44f69e90912a8fd01d601ff0
ps-tubtim <pimolnats@ecosoft.co.th>
2021-09-13 23:29:57 +0700

    [14.0][ADD] account_move_line_check_number

---
+++ account_move_line_check_number/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Move Line Check Number",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "license": "AGPL-3",
+    "category": "Accounting & Finance",
+    "summary": "Add the check number in the journal items",
+    "depends": ["account_check_printing"],
+    "data": ["views/account_move_views.xml"],
+    "installable": True,
+    "development_status": "Alpha",
+    "maintainers": ["ps-tubtim"],
+}
