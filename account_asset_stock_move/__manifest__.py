592219a970fc25320a5e28d5845c44bac402a63f
Kitti U <kittiu@ecosoft.co.th>
2021-09-23 17:09:17 +0700

    [14.0][ADD] account_asset_stock_move

---
+++ account_asset_stock_move/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Ecosoft Co., Ltd. <http://ecosoft.co.th>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Assets Management - Create asset during stock move",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["account_asset_management", "stock_account"],
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "category": "Accounting & Finance",
+    "data": [
+        "views/stock.xml",
+    ],
+    "development_status": "Alpha",
+    "maintainers": ["kittiu"],
+}
