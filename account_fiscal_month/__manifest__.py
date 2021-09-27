a4f38a82d411751f2b51f6fb157ff05837904ef6
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-09-27 17:26:48 +0200

    [14.0][MIG] account_fiscal_month

--- account_fiscal_month/__manifest__.py
+++ account_fiscal_month/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Fiscal Month",
     "summary": """Provide a fiscal month date range type""",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
