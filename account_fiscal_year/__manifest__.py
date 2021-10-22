3cb1a7dca5c90c6f0441156d11dfddda2faf3e94
tafaRU <alex.comba@agilebg.com>
2021-10-22 12:48:10 +0200

    account_fiscal_year: change development_status to Production/Stable

--- account_fiscal_year/__manifest__.py
+++ account_fiscal_year/__manifest__.py
@@ -6,7 +6,7 @@
     "name": "Account Fiscal Year",
     "summary": "Create Account Fiscal Year",
     "version": "14.0.1.1.0",
-    "development_status": "Beta",
+    "development_status": "Production/Stable",
     "category": "Accounting",
     "website": "https://github.com/OCA/account-financial-tools"
     "14.0/account_fiscal_year",
