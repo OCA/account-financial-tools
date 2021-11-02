03347763d29e3cb2f4e84ade604c2110e464ecc4
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-11-02 14:36:24 +0100

    [IMP] account_move_fiscal_year : black, isort, prettier

--- account_move_fiscal_year/models/res_company.py
+++ account_move_fiscal_year/models/res_company.py
@@ -5,17 +5,20 @@ from odoo import models
 
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
+    _inherit = "res.company"
 
     def find_daterange_fy(self, date):
         """
         try to find a date range with type 'fiscalyear'
         with @param:date contained in its date_start/date_end interval
         """
-        fiscalyear = self.env['account.fiscal.year'].search([
-            ('company_id', '=', self.id),
-            ('date_from', '<=', date),
-            ('date_to', '>=', date),
-        ], limit=1)
+        fiscalyear = self.env["account.fiscal.year"].search(
+            [
+                ("company_id", "=", self.id),
+                ("date_from", "<=", date),
+                ("date_to", ">=", date),
+            ],
+            limit=1,
+        )
 
         return fiscalyear
