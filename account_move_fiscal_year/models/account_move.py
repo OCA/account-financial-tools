5764f8e252888f66111f7faa958c3ca16599546f
xavier-bouquiaux <xavier.bouquiaux@acsone.eu>
2021-11-02 14:36:24 +0100

    [IMP] account_move_fiscal_year : black, isort, prettier

--- account_move_fiscal_year/models/account_move.py
+++ account_move_fiscal_year/models/account_move.py
@@ -7,38 +7,45 @@ from odoo.osv import expression
 
 class AccountMove(models.Model):
 
-    _inherit = 'account.move'
+    _inherit = "account.move"
 
     date_range_fy_id = fields.Many2one(
-        comodel_name='account.fiscal.year', string="Fiscal year",
-        compute='_compute_date_range_fy', search='_search_date_range_fy')
+        comodel_name="account.fiscal.year",
+        string="Fiscal year",
+        compute="_compute_date_range_fy",
+        search="_search_date_range_fy",
+    )
 
     @api.multi
-    @api.depends('date', 'company_id')
+    @api.depends("date", "company_id")
     def _compute_date_range_fy(self):
         for rec in self:
             date = fields.Date.to_date(rec.date)
             company = rec.company_id
-            rec.date_range_fy_id =\
-                company and company.find_daterange_fy(date) or False
+            rec.date_range_fy_id = company and company.find_daterange_fy(date) or False
 
     @api.model
     def _search_date_range_fy(self, operator, value):
-        if operator in ('=', '!=', 'in', 'not in'):
-            date_range_domain = [('id', operator, value)]
+        if operator in ("=", "!=", "in", "not in"):
+            date_range_domain = [("id", operator, value)]
         else:
-            date_range_domain = [('name', operator, value)]
+            date_range_domain = [("name", operator, value)]
 
-        date_ranges = self.env['account.fiscal.year'].search(date_range_domain)
+        date_ranges = self.env["account.fiscal.year"].search(date_range_domain)
 
-        domain = [('id', '=', -1)]
+        domain = [("id", "=", -1)]
         for date_range in date_ranges:
-            domain = expression.OR([domain, [
-                '&',
-                ('date', '>=', date_range.date_from),
-                ('date', '<=', date_range.date_to),
-                '|',
-                ('company_id', '=', False),
-                ('company_id', '=', date_range.company_id.id),
-            ]])
+            domain = expression.OR(
+                [
+                    domain,
+                    [
+                        "&",
+                        ("date", ">=", date_range.date_from),
+                        ("date", "<=", date_range.date_to),
+                        "|",
+                        ("company_id", "=", False),
+                        ("company_id", "=", date_range.company_id.id),
+                    ],
+                ]
+            )
         return domain
--- account_move_fiscal_year/models/account_move_line.py
+++ account_move_fiscal_year/models/account_move_line.py
@@ -6,8 +6,8 @@ from odoo import fields, models
 
 class AccountMoveLine(models.Model):
 
-    _inherit = 'account.move.line'
+    _inherit = "account.move.line"
 
     date_range_fy_id = fields.Many2one(
-        related='move_id.date_range_fy_id',
+        related="move_id.date_range_fy_id",
     )
