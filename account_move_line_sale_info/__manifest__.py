05a34625ad523b300b1b8b549b23f09dc164813c
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-11-09 14:38:50 +0100

    [15.0][MIG] account_move_line_sale_info: Migration to 15.0

--- account_move_line_sale_info/__manifest__.py
+++ account_move_line_sale_info/__manifest__.py
@@ -4,14 +4,13 @@
 {
     "name": "Account Move Line Sale Info",
     "summary": "Introduces the purchase order line to the journal items",
-    "version": "13.0.1.0.3",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow S.L., " "Odoo Community Association (OCA)",
-    "website": "http://www.github.com/OCA/account-financial-tools",
+    "website": "https://github.com/OCA/account-financial-tools",
     "category": "Generic",
     "depends": [
         "account_move_line_stock_info",
         "sale_stock",
-        "stock_account_prepare_anglo_saxon_out_lines_hook",
     ],
     "license": "AGPL-3",
     "data": ["security/account_security.xml", "views/account_move_view.xml"],
