a571f061ee0a4ee4b3a8fdc3a95f01392df18e0b
AaronHForgeFlow <ahenriquez@eficent.com>
2021-11-23 17:08:24 +0100

    [ADD] purchase_unreconciled

---
+++ purchase_unreconciled/models/company.py
@@ -0,0 +1,20 @@
+# Copyright 2019 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    purchase_reconcile_account_id = fields.Many2one(
+        "account.account",
+        domain=lambda self: [("deprecated", "=", False)],
+        string="Write-Off Account On Purchases",
+        ondelete="restrict",
+        copy=False,
+        help="Write-off account to reconcile Unreconciled Purchase Orders",
+    )
+    purchase_reconcile_journal_id = fields.Many2one(
+        "account.journal", string="WriteOff Journal for Purchases"
+    )
