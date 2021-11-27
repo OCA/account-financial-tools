15e4a4dc01205c88c552ce5c2a7783c12ab80869
AaronHForgeFlow <ahenriquez@eficent.com>
2021-11-27 11:58:37 +0100

    [14.0][ADD] sale_unreconciled

---
+++ sale_unreconciled/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Sale Unreconciled",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "category": "Accounting",
+    "depends": ["sale_mrp", "account_move_line_sale_info"],
+    "data": ["views/sale_order_view.xml", "views/res_config_settings_view.xml"],
+    "license": "AGPL-3",
+    "installable": True,
+    "development_status": "Alpha",
+    "maintainers": ["AaronHForgeFlow"],
+}
