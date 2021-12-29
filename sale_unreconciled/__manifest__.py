b4429a25eaf357d96d7b77c42f46b1b7cabee549
AaronHForgeFlow <ahenriquez@eficent.com>
2021-12-29 15:48:15 +0100

    [15.0][MIG] sale_unreconciled

--- sale_unreconciled/__manifest__.py
+++ sale_unreconciled/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Sale Unreconciled",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
     "category": "Accounting",
