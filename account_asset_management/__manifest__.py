7ee98f1d3a77ed4cf4288c88a1ad039bddfd521f
Enric Tobella <etobella@creublanca.es>
2022-01-06 20:55:34 +0100

    [UPD] account_asset_management: Mark as mature

--- account_asset_management/__manifest__.py
+++ account_asset_management/__manifest__.py
@@ -9,6 +9,7 @@
     "license": "AGPL-3",
     "depends": ["account", "report_xlsx_helper"],
     "excludes": ["account_asset"],
+    "development_status": "Mature",
     "external_dependencies": {"python": ["python-dateutil"]},
     "author": "Noviat, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
