62c7abcf3226877ec0aba628203689836dc315a8
aromera <aromera@pmsastro.es>
2022-01-10 15:07:49 +0100

    [MIG] account_loan: Migration to 14.0

--- account_loan/__manifest__.py
+++ account_loan/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Loan management",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "author": "Creu Blanca,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
     "license": "AGPL-3",
