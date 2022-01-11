cf19a91adeb9ddd190a7b110f87fc59ce2039de8
Kitti U <kittiu@ecosoft.co.th>
2022-01-11 17:49:17 +0700

    [IMP] account_document_reversal black, isort, prettier

--- account_document_reversal/models/account_payment.py
+++ account_document_reversal/models/account_payment.py
@@ -1,20 +1,21 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-from odoo import api, models, _
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
 class AccountPayment(models.Model):
-    _name = 'account.payment'
-    _inherit = ['account.payment', 'account.document.reversal']
+    _name = "account.payment"
+    _inherit = ["account.payment", "account.document.reversal"]
 
     @api.multi
     def cancel(self):
         """ If cancel method is to reverse, use document reversal wizard """
         cancel_reversal = all(
-            self.mapped('move_line_ids.move_id.journal_id.is_cancel_reversal'))
-        states = self.mapped('state')
-        if cancel_reversal and 'draft' not in states:
+            self.mapped("move_line_ids.move_id.journal_id.is_cancel_reversal")
+        )
+        states = self.mapped("state")
+        if cancel_reversal and "draft" not in states:
             return self.reverse_document_wizard()
         return super().cancel()
 
@@ -22,19 +23,16 @@ class AccountPayment(models.Model):
     def action_document_reversal(self, date=None, journal_id=None):
         """ Reverse all moves related to this payment + set state to cancel """
         # Check document state
-        if 'cancelled' in self.mapped('state'):
-            raise ValidationError(
-                _('You are trying to cancel the cancelled document'))
-        move_lines = self.mapped('move_line_ids')
-        moves = move_lines.mapped('move_id')
+        if "cancelled" in self.mapped("state"):
+            raise ValidationError(_("You are trying to cancel the cancelled document"))
+        move_lines = self.mapped("move_line_ids")
+        moves = move_lines.mapped("move_id")
         # Set all moves to unreconciled
-        move_lines.filtered(lambda x:
-                            x.account_id.reconcile).remove_move_reconcile()
+        move_lines.filtered(lambda x: x.account_id.reconcile).remove_move_reconcile()
         # Important to remove relation with move.line before reverse
-        move_lines.write({'payment_id': False})
+        move_lines.write({"payment_id": False})
         # Create reverse entries
         moves.reverse_moves(date, journal_id)
         # Set state cancelled and unlink with account.move
-        self.write({'move_name': False,
-                    'state': 'cancelled'})
+        self.write({"move_name": False, "state": "cancelled"})
         return True
