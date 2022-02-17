51653130e561e3e3f068aac35b6390c2c3efd821
david <david.vidal@tecnativa.com>
2022-02-17 10:26:39 +0100

    [FIX] account_invoice_constraint_chronology: modify past invoices
    Addresses the issue as commented here:
    
    - https://github.com/OCA/account-financial-tools/issues/1206
    - https://github.com/OCA/account-financial-tools/pull/1152#issuecomment-847854629
    
    Summary of the steps to reproduce the problem:
    
    1. Set on the invoices journal the constraint chronology.
    2. Validate an invoice for this journal today.
    3. Your accountant says there's an error on an invoice from yesterday.
    4. Cancel it an set it to draft. You don't even need to modify anything
    5. Try to post the invoice.
    6. You'll get a chronology error.
    
    In v12 this was considered but it was lost on the migration to v13:
    
    https://github.com/OCA/account-financial-tools/blob/800c02474e5aa43cee592e93499d2a480be46d32/account_invoice_constraint_chronology/model/account_invoice.py#L50
    
    https://github.com/OCA/account-financial-tools/blob/800c02474e5aa43cee592e93499d2a480be46d32/account_invoice_constraint_chronology/model/account_invoice.py#L70-L73
    
    TT34624

--- account_invoice_constraint_chronology/tests/test_account_invoice_constraint_chronology.py
+++ account_invoice_constraint_chronology/tests/test_account_invoice_constraint_chronology.py
@@ -133,3 +133,19 @@ class TestAccountInvoiceConstraintChronology(common.SavepointCase):
         self.invoice_1.invoice_date = self.tomorrow
         with self.assertRaisesRegex(UserError, "higher number"):
             self.invoice_1.action_post()
+
+    def test_modify_validated_past_invoice(self):
+        """We've got an invoice from yesterday that we need to modify but new ones
+        have been posted since. As the invoice already has a name, we should be able
+        to validate it"""
+        self.invoice_1.invoice_date = self.yesterday
+        self.invoice_1.action_post()
+        self.invoice_2.invoice_date = self.today
+        self.invoice_2.action_post()
+        self.invoice_1.button_cancel()
+        self.invoice_1.button_draft()
+        self.invoice_1.action_post()
+        self.invoice_1_5 = self.invoice_1.copy()
+        self.invoice_1_5.invoice_date = self.yesterday
+        with self.assertRaises(UserError):
+            self.invoice_1_5.action_post()
