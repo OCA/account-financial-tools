e981667d9493bd730b4236aebcb572304cd73f16
Miquel Raïch <miquel.raich@forgeflow.com>
2022-03-24 17:02:55 +0100

    [FIX] account_asset_management: add move line migration scripts

---
+++ account_asset_management/migrations/13.0.1.0.0/post-migration.py
@@ -0,0 +1,20 @@
+# Copyright 2022 ForgeFlow
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+def handle_account_invoice_move_migration(env):
+    openupgrade.logged_query(
+        env.cr,
+        """
+        UPDATE account_move_line aml
+        SET asset_profile_id = ail.asset_profile_id, asset_id = ail.asset_id
+        FROM account_invoice_line ail
+        WHERE aml.old_invoice_line_id = ail.id AND
+            (ail.asset_profile_id IS NOT NULL OR ail.asset_id IS NOT NULL)"""
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    handle_account_invoice_move_migration(env)
