59682a94d64a532b46807cac914e8ec03565175b
Joan <joan.segui@qubiq.es>
2022-03-29 11:14:50 +0200

    [MIG] account_move_line_tax_editable: Migration to 15.0

--- account_move_line_tax_editable/models/account_move_line.py
+++ account_move_line_tax_editable/models/account_move_line.py
@@ -16,3 +16,23 @@ class AccountMoveLine(models.Model):
     def _compute_is_tax_editable(self):
         for rec in self:
             rec.is_tax_editable = rec.move_id.state == "draft"
+
+    force_tax_line_id = fields.Many2one(
+        "account.tax",
+        string="Force Originator Tax",
+        ondelete="restrict",
+        help="When giving a value to this field (only appears in edit mode), Originator"
+        " tax will take its value. If non is set, Originator tax will maintain its value",
+        readonly=False,
+    )
+
+    @api.depends(
+        "tax_repartition_line_id.invoice_tax_id",
+        "tax_repartition_line_id.refund_tax_id",
+        "force_tax_line_id",
+    )
+    def _compute_tax_line_id(self):
+        forced_lines = self.filtered(lambda r: r.force_tax_line_id)
+        for rec in forced_lines:
+            rec.tax_line_id = rec.force_tax_line_id
+        return super(AccountMoveLine, self - forced_lines)._compute_tax_line_id()
--- account_move_line_tax_editable/readme/DESCRIPTION.rst
+++ account_move_line_tax_editable/readme/DESCRIPTION.rst
@@ -1,1 +1,1 @@
-Allows to edit taxes on account move lines.
+New field Force Originator Tax appears in edit mode in order to change the value of Originator Tax on account move lines.
