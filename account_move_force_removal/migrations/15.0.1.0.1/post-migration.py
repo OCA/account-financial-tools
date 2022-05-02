ccde1699158e38e54f94702f2855b0afa0a8aed5
angel <angel.garcia@sygel.com>
2022-05-02 14:04:07 +0200

    [15.0][IMP] account_move_force_removal: add a new user group.

---
+++ account_move_force_removal/migrations/15.0.1.0.1/post-migration.py
@@ -0,0 +1,4 @@
+def migrate(env, version):
+    users_billing = env.ref("account.group_account_invoice").users
+    group = env.ref("account_move_force_removal.group_account_move_force_removal")
+    group.users = [(4, user.id) for user in users_billing]
