5267ad78bf5750b6565c4501bff55d14e9bf1e0d
Jairo Llopis <yajo.sk8@gmail.com>
2022-05-10 09:47:06 +0100

    [FIX] account_move_force_removal: buggy migration script
    Running the migration script fails with:
    
        AttributeError: 'psycopg2.extensions.cursor' object has no attribute 'ref'
    
    @moduon

--- account_move_force_removal/migrations/15.0.1.0.1/post-migration.py
+++ account_move_force_removal/migrations/15.0.1.0.1/post-migration.py
@@ -1,3 +1,7 @@
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
 def migrate(env, version):
     users_billing = env.ref("account.group_account_invoice").users
     group = env.ref("account_move_force_removal.group_account_move_force_removal")
