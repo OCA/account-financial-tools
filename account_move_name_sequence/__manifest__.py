6c55c7b5860945872fc5eeba8b1afc7424499313
Moises Lopez <moylop260@vauxoo.com>
2022-05-13 17:14:30 +0000

    [FIX] account_move_name_sequence: Fix required journal fields in view
    The required flag was wrong for sequence_id and refund_sequence_id
    
    So, it was not possible to store any change in journal for journal different to sale and purchase

--- account_move_name_sequence/__manifest__.py
+++ account_move_name_sequence/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Account Move Number Sequence",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.2.0",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Generate journal entry number from sequence",
