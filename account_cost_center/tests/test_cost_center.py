bcb5bbf3a165d35713eb8902e349efe7b9dd6265
Shakir <smemon@surekhatech.com>
2022-05-31 14:51:03 +0530

    [MIG] account_cost_centre: Migration to 15.0

--- account_cost_center/tests/test_cost_center.py
+++ account_cost_center/tests/test_cost_center.py
@@ -1,92 +1,98 @@
-# Copyright 2017-2019 Onestein (<http://www.onestein.eu>)
+# Copyright 2017-2020 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo.tests import common
+from odoo.tests import Form, tagged
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-class TestAccountCostCenter(common.TransactionCase):
-    def setUp(self):
-        super(TestAccountCostCenter, self).setUp()
-
-        acc_rec = self.env.ref("account.data_account_type_receivable")
-        acc_exp = self.env.ref("account.data_account_type_expenses")
-        self.invoice_account = (
-            self.env["account.account"]
-            .search([("user_type_id", "=", acc_rec.id)], limit=1)
-            .id
-        )
-        self.invoice_line_account = (
-            self.env["account.account"]
-            .search([("user_type_id", "=", acc_exp.id)], limit=1)
-            .id
-        )
-
-        self.invoice1 = self.env["account.invoice"].create(
+@tagged("post_install", "-at_install")
+class TestAccountCostCenter(AccountTestInvoicingCommon):
+    @classmethod
+    def setUpClass(cls, chart_template_ref=None):
+        super().setUpClass(chart_template_ref=chart_template_ref)
+        cls.expenses_account = cls.env["account.account"].create(
             {
-                "partner_id": self.env.ref("base.res_partner_2").id,
-                "account_id": self.invoice_account,
-                "type": "in_invoice",
+                "user_type_id": cls.env.ref("account.data_account_type_expenses").id,
+                "code": "EXPTEST",
+                "name": "Test expense account",
             }
         )
 
-        self.line1 = self.env["account.invoice.line"].create(
-            {
-                "product_id": self.env.ref("product.product_product_2").id,
-                "quantity": 1.0,
-                "price_unit": 100.0,
-                "invoice_id": self.invoice1.id,
-                "name": "product that cost 100",
-                "account_id": self.invoice_line_account,
-            }
-        )
-
-        self.costcenter = self.env["account.cost.center"].create(
+        cls.costcenter = cls.env["account.cost.center"].create(
             {
                 "name": "Cost Center Test",
                 "code": "CC1",
-                "company_id": self.env.user.company_id.id,
+                "company_id": cls.env.company.id,
             }
         )
 
-        self.invoice2 = self.env["account.invoice"].create(
-            {
-                "partner_id": self.env.ref("base.res_partner_2").id,
-                "account_id": self.invoice_account,
-                "type": "in_invoice",
-                "cost_center_id": self.costcenter.id,
-            }
-        )
+        # Invoice created with tests Form
+        cls.invoice1 = cls.init_invoice("in_invoice")
 
-        self.line2 = (
-            self.env["account.invoice.line"]
-            .with_context(cost_center_id=self.costcenter.id)
+        # Invoice created with standard create
+        cls.invoice2 = (
+            cls.env["account.move"]
+            .with_context(cost_center_id=cls.costcenter.id)
             .create(
                 {
-                    "product_id": self.env.ref("product.product_product_4").id,
-                    "quantity": 1.0,
-                    "price_unit": 130.0,
-                    "invoice_id": self.invoice2.id,
-                    "name": "product that cost 130",
-                    "account_id": self.invoice_line_account,
+                    "partner_id": cls.env.ref("base.res_partner_2").id,
+                    "move_type": "in_invoice",
+                    "cost_center_id": cls.costcenter.id,
                 }
             )
         )
 
+        cls.invoice2.invoice_line_ids += cls.env["account.move.line"].new(
+            {
+                "product_id": cls.env.ref("product.product_product_4").id,
+                "quantity": 1.0,
+                "price_unit": 130.0,
+                "name": "product that cost 130",
+                "cost_center_id": cls.costcenter.id,
+            }
+        )
+
     def test_01_check_lines(self):
         self.assertFalse(
-            self.line1.cost_center_id, "Default cost center per line not set"
+            self.invoice1.invoice_line_ids.cost_center_id,
+            "Default cost center per line not set",
         )
 
-        self.assertTrue(
-            (self.line2.cost_center_id == self.costcenter),
-            "Default cost center per line set",
+        self.assertEqual(
+            self.invoice2.invoice_line_ids[0].cost_center_id, self.costcenter
         )
 
-    def test_02_confirm_invoice(self):
-        self.invoice2.action_invoice_open()
-        for move in self.invoice2.move_id.line_ids:
-            cost_center = move.cost_center_id
-            if move.name == "product that cost 130":
-                self.assertTrue(cost_center)
-            else:
-                self.assertFalse(cost_center)
+    def test_02_check_lines(self):
+        invoice_lines = self.invoice1.invoice_line_ids
+        self.assertFalse(any(line.cost_center_id for line in invoice_lines))
+
+        invoice_form = Form(self.invoice1)
+        invoice_form.cost_center_id = self.costcenter
+        with invoice_form.invoice_line_ids.new() as line:
+            line.name = "Test line2"
+            line.quantity = 2.0
+            line.price_unit = 200.0
+            line.account_id = self.expenses_account
+        self.invoice1 = invoice_form.save()
+
+        invoice_lines = self.invoice1.invoice_line_ids
+        self.assertFalse(any(line.cost_center_id for line in invoice_lines))
+        self.assertTrue(any(not line.cost_center_id for line in invoice_lines))
+
+    def test_03_confirm_invoice(self):
+        invoice_lines = self.invoice2.invoice_line_ids
+        for move_line in invoice_lines:
+            self.assertEqual(move_line.cost_center_id, self.costcenter)
+        for move_line in self.invoice2.line_ids - invoice_lines:
+            self.assertFalse(move_line.cost_center_id)
+
+    def test_04_search_read(self):
+        expected_cost_center = self.costcenter
+
+        records = self.env["account.invoice.report"].sudo().search_read([])
+        result = records[0].get("cost_center_id")
+
+        self.assertTrue(result)
+        self.assertEqual(result[0], expected_cost_center.id)
+        self.assertEqual(result[1], expected_cost_center.name)
