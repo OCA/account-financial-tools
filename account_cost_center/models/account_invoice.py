1df9bece9688b5736757fdc5c456955e7cf02552
Shakir <smemon@surekhatech.com>
2022-05-31 14:49:00 +0530

    [IMP] : black, isort, prettier

--- account_cost_center/models/account_invoice.py
+++ account_cost_center/models/account_invoice.py
@@ -5,21 +5,21 @@ from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     cost_center_id = fields.Many2one(
-        'account.cost.center',
-        string='Cost Center',
+        "account.cost.center",
+        string="Cost Center",
         readonly=True,
-        states={'draft': [('readonly', False)]},
-        help='Default Cost Center',
+        states={"draft": [("readonly", False)]},
+        help="Default Cost Center",
     )
 
     @api.model
     def line_get_convert(self, line, part):
         res = super(AccountInvoice, self).line_get_convert(line, part)
-        if line.get('cost_center_id'):
-            res['cost_center_id'] = line['cost_center_id']
+        if line.get("cost_center_id"):
+            res["cost_center_id"] = line["cost_center_id"]
         return res
 
     @api.model
@@ -27,9 +27,9 @@ class AccountInvoice(models.Model):
         res = super(AccountInvoice, self).invoice_line_move_line_get()
 
         for dict_data in res:
-            invl_id = dict_data.get('invl_id')
-            line = self.env['account.invoice.line'].browse(invl_id)
+            invl_id = dict_data.get("invl_id")
+            line = self.env["account.invoice.line"].browse(invl_id)
             if line.cost_center_id:
-                dict_data['cost_center_id'] = line.cost_center_id.id
+                dict_data["cost_center_id"] = line.cost_center_id.id
 
         return res
--- account_cost_center/models/account_invoice_report.py
+++ account_cost_center/models/account_invoice_report.py
@@ -5,22 +5,23 @@ from odoo import fields, models
 
 
 class AccountInvoiceReport(models.Model):
-    _inherit = 'account.invoice.report'
+    _inherit = "account.invoice.report"
 
     cost_center_id = fields.Many2one(
-        'account.cost.center',
-        string='Cost Center',
-        readonly=True
+        "account.cost.center", string="Cost Center", readonly=True
     )
 
     def _select(self):
-        return super(AccountInvoiceReport, self)._select() + \
-            ", sub.cost_center_id as cost_center_id"
+        return (
+            super(AccountInvoiceReport, self)._select()
+            + ", sub.cost_center_id as cost_center_id"
+        )
 
     def _sub_select(self):
-        return super(AccountInvoiceReport, self)._sub_select() + \
-            ", ail.cost_center_id as cost_center_id"
+        return (
+            super(AccountInvoiceReport, self)._sub_select()
+            + ", ail.cost_center_id as cost_center_id"
+        )
 
     def _group_by(self):
-        return super(AccountInvoiceReport, self)._group_by() + \
-            ", ail.cost_center_id"
+        return super(AccountInvoiceReport, self)._group_by() + ", ail.cost_center_id"
