0cfb421860c43e25a8e9fc7b474b609a9e940812
Shakir <smemon@surekhatech.com>
2022-05-31 14:51:03 +0530

    [MIG] account_cost_centre: Migration to 15.0

--- account_cost_center/__manifest__.py
+++ account_cost_center/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2019 Onestein (<http://www.onestein.eu>)
+# Copyright 2016-2020 Onestein (<http://www.onestein.eu>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
@@ -8,16 +8,15 @@
     "license": "AGPL-3",
     "website": "https://github.com/OCA/account-financial-tools",
     "category": "Accounting",
-    "version": "12.0.1.0.0",
-    "depends": ["account", "base_view_inheritance_extension"],
+    "version": "15.0.1.0.0",
+    "depends": ["account"],
     "data": [
         "security/ir.model.access.csv",
         "security/account_cost_center_security.xml",
-        "views/account_cost_center.xml",
-        "views/account_move.xml",
-        "views/account_move_line.xml",
-        "views/account_invoice.xml",
-        "views/account_invoice_report.xml",
+        "views/account_cost_center_views.xml",
+        "views/account_move_views.xml",
+        "views/account_move_line_views.xml",
+        "views/account_invoice_report_views.xml",
     ],
     "installable": True,
 }
