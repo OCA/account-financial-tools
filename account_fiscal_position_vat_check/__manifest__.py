3ae629f51d74744d57b6df973ca65a8bc7cf9a59
Eduardo De Miguel <edu@moduon.team>
2022-06-07 16:24:17 +0200

    [MIG] account_fiscal_position_vat_check: Migration to 15.0

--- account_fiscal_position_vat_check/__manifest__.py
+++ account_fiscal_position_vat_check/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account Fiscal Position VAT Check",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Invoices & Payments",
     "license": "AGPL-3",
     "summary": "Check VAT on invoice validation",
