ebece3bbb449c451a495dea07f3b350924f20752
Ivàn Todorovich <ivan.todorovich@gmail.com>
2022-06-30 16:03:32 -0300

    [IMP] product_category_tax: copy taxes when creating products programatically

--- product_category_tax/models/product_product.py
+++ product_category_tax/models/product_product.py
@@ -14,3 +14,15 @@ class ProductProduct(models.Model):
 
     def set_tax_from_category(self):
         return self.product_tmpl_id.set_tax_from_category()
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("categ_id"):
+                if "taxes_id" not in vals:
+                    categ = self.env["product.category"].browse(vals["categ_id"])
+                    vals["taxes_id"] = [(6, 0, categ.taxes_id.ids)]
+                if "supplier_taxes_id" not in vals:
+                    categ = self.env["product.category"].browse(vals["categ_id"])
+                    vals["supplier_taxes_id"] = [(6, 0, categ.supplier_taxes_id.ids)]
+        return super().create(vals_list)
