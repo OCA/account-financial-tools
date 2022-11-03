9de8fc9d503457a6ee5ea1b2ed5384ccb3bb7572
Lois Rilo <lois.rilo@forgeflow.com>
2022-11-03 11:23:15 +0100

    [ADD] account_chart_update_l10n_eu_oss_oca

---
+++ account_chart_update_l10n_eu_oss_oca/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Chart update OSS OCA",
+    "version": "15.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "maintainers": ["LoisRForgeFlow"],
+    "website": "https://github.com/OCA/account-financial-tools",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "depends": [
+        "account_chart_update",
+        "l10n_eu_oss_oca",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
