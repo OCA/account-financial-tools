662f182a57a9462d1b9ee7847abe3ecfa57428b6
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-28 20:35:47 +0100

    [MIG] account_partner_required to v16

--- account_partner_required/tests/test_account_partner_required.py
+++ account_partner_required/tests/test_account_partner_required.py
@@ -1,69 +1,66 @@
-# © 2014-2016 Acsone (http://acsone.eu)
-# © 2016 Akretion (http://www.akretion.com/)
+# Copyright 2014-2022 Acsone (http://acsone.eu)
+# Copyright 2016-2022 Akretion (http://www.akretion.com/)
 # @author Stéphane Bidoul <stephane.bidoul@acsone.eu>
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from datetime import datetime
-
 from odoo.exceptions import ValidationError
-from odoo.tests import common
+from odoo.tests import tagged
+from odoo.tests.common import TransactionCase
 
 
-class TestAccountPartnerRequired(common.TransactionCase):
-    def setUp(self):
-        super(TestAccountPartnerRequired, self).setUp()
-        self.account_obj = self.env["account.account"]
-        self.account_type_obj = self.env["account.account.type"]
-        self.move_obj = self.env["account.move"]
-        self.move_line_obj = self.env["account.move.line"]
-        self.sale_journal = self.env["account.journal"].create(
+@tagged("post_install", "-at_install")
+class TestAccountPartnerRequired(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+        cls.account_obj = cls.env["account.account"]
+        cls.move_obj = cls.env["account.move"]
+        cls.move_line_obj = cls.env["account.move.line"]
+        cls.company_id = cls.env.ref("base.main_company").id
+        cls.sale_journal = cls.env["account.journal"].create(
             {
                 "type": "sale",
                 "code": "SJXX",
                 "name": "Sale journal",
+                "company_id": cls.company_id,
             }
         )
-        liq_acc_type = self.env.ref("account.data_account_type_liquidity")
-        self.account1 = self.account_obj.create(
+        cls.account1 = cls.account_obj.create(
             {
                 "code": "124242",
                 "name": "Test 1",
-                "user_type_id": liq_acc_type.id,
+                "account_type": "asset_cash",
+                "company_id": cls.company_id,
             }
         )
-        self.account_type_custom = self.account_type_obj.create(
-            {
-                "name": "acc type test",
-                "type": "other",
-                "partner_policy": "optional",
-            }
-        )
-        self.account2 = self.account_obj.create(
+        cls.account2 = cls.account_obj.create(
             {
                 "code": "124243",
                 "name": "Test 2",
-                "user_type_id": self.account_type_custom.id,
+                "account_type": "asset_receivable",
+                "company_id": cls.company_id,
             }
         )
-        self.account3 = self.account_obj.create(
+        cls.account3 = cls.account_obj.create(
             {
                 "code": "124244",
                 "name": "Test 3",
-                "user_type_id": self.account_type_custom.id,
+                "account_type": "liability_payable",
+                "company_id": cls.company_id,
             }
         )
 
     def _create_move(self, with_partner, amount=100):
-        date = datetime.now()
         if with_partner:
             partner_id = self.env.ref("base.res_partner_1").id
         else:
             partner_id = False
         move_vals = {
+            "company_id": self.company_id,
             "journal_id": self.sale_journal.id,
-            "date": date,
             "line_ids": [
                 (
                     0,
@@ -101,45 +98,45 @@ class TestAccountPartnerRequired(common.TransactionCase):
         self._create_move(with_partner=True)
 
     def test_always_no_partner(self):
-        self.account_type_custom.partner_policy = "always"
+        self.account2.partner_policy = "always"
         with self.assertRaises(ValidationError):
             self._create_move(with_partner=False)
 
     def test_always_no_partner_0(self):
         # accept missing partner when debit=credit=0
-        self.account_type_custom.partner_policy = "always"
+        self.account2.partner_policy = "always"
         self._create_move(with_partner=False, amount=0)
 
     def test_always_with_partner(self):
-        self.account_type_custom.partner_policy = "always"
+        self.account2.partner_policy = "always"
         self._create_move(with_partner=True)
 
     def test_never_no_partner(self):
-        self.account_type_custom.partner_policy = "never"
+        self.account2.partner_policy = "never"
         self._create_move(with_partner=False)
 
     def test_never_with_partner(self):
-        self.account_type_custom.partner_policy = "never"
+        self.account2.partner_policy = "never"
         with self.assertRaises(ValidationError):
             self._create_move(with_partner=True)
 
     def test_never_with_partner_0(self):
-        self.account_type_custom.partner_policy = "never"
+        self.account2.partner_policy = "never"
         # accept partner when debit=credit=0
         self._create_move(with_partner=True, amount=0)
 
     def test_always_remove_partner(self):
         # remove partner when policy is always
-        self.account_type_custom.partner_policy = "always"
+        self.account2.partner_policy = "always"
         line = self._create_move(with_partner=True)
         with self.assertRaises(ValidationError):
             line.write({"partner_id": False})
 
     def test_change_account(self):
-        self.account_type_custom.partner_policy = "optional"
+        self.account2.partner_policy = "optional"
         line = self._create_move(with_partner=False)
         # change account to an account with policy always but missing partner
-        self.account_type_custom.partner_policy = "always"
+        self.account3.partner_policy = "always"
         with self.assertRaises(ValidationError):
             line.write({"account_id": self.account3.id})
         # change account to an account with policy always with partner
