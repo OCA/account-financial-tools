18e356487d31d9dd1227bb3e6070ea6d106a115e
Alexis de Lattre <alexis.delattre@akretion.com>
2022-12-28 20:35:47 +0100

    [MIG] account_partner_required to v16

--- account_partner_required/__manifest__.py
+++ account_partner_required/__manifest__.py
@@ -1,24 +1,21 @@
-# © 2014-2016 Acsone (http://acsone.eu).
-# © 2016 Akretion (http://www.akretion.com/)
+# Copyright 2014-2022 Acsone (http://acsone.eu).
+# Copyright 2016-2022 Akretion France (http://www.akretion.com/)
 # @author Stéphane Bidoul <stephane.bidoul@acsone.eu>
 # @author Alexis de Lattre <alexis.delattre@akretion.com>
-# © 2018 DynApps (https://odoo.dynapps.be/)
+# Copyright 2018-2022 DynApps (https://odoo.dynapps.be/)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Account partner required",
-    "version": "12.0.1.0.0",
+    "name": "Account Partner Required",
+    "version": "16.0.1.0.0",
     "category": "Accounting",
     "license": "AGPL-3",
-    "summary": "Adds an option 'partner policy' on account types",
+    "summary": "Adds an option 'partner policy' on accounts",
     "author": "ACSONE SA/NV,Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
     "website": "https://github.com/OCA/account-financial-tools",
-    "depends": [
-        "account_type_menu",
-    ],
-    "data": [
-        "views/account.xml",
-    ],
+    "depends": ["account"],
+    "data": ["views/account_account.xml"],
     "installable": True,
     "application": False,
 }
