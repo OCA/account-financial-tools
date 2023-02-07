9f257d895148454ab4e2e9d5e5eb308764311290
Núria Sancho <nuria.sancho@creublanca.es>
2023-02-07 15:03:13 +0100

    [14.0][FIX] account_loan, account_loan_line & account_loan_generate_entries: fix read
    account_loan 14.0.1.0.3

--- account_loan/model/account_loan.py
+++ account_loan/model/account_loan.py
@@ -442,15 +442,17 @@ class AccountLoan(models.Model):
 
     def view_account_moves(self):
         self.ensure_one()
-        action = self.env.ref("account.action_move_line_form")
-        result = action.read()[0]
+        result = self.env["ir.actions.act_window"]._for_xml_id(
+            "account.action_move_line_form"
+        )
         result["domain"] = [("loan_id", "=", self.id)]
         return result
 
     def view_account_invoices(self):
         self.ensure_one()
-        action = self.env.ref("account.action_move_out_invoice_type")
-        result = action.read()[0]
+        result = self.env["ir.actions.act_window"]._for_xml_id(
+            "account.action_move_out_invoice_type"
+        )
         result["domain"] = [("loan_id", "=", self.id), ("move_type", "=", "in_invoice")]
         return result
 
