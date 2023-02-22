c9ca0faad01f19b7d142dc09c05ae1e1d8b34c54
Alexis de Lattre <alexis.delattre@akretion.com>
2023-02-22 23:53:29 +0100

    [MIG] account_netting to v16
    Add prepare method for the account.move created by the wizard
    Add multi-company checks
    Improve error messages
    Improve usability: use monetary instead of float, display partner
    Use odoo methods to compare floats
    Update tests

--- account_netting/__manifest__.py
+++ account_netting/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Account netting",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "summary": "Compensate AR/AP accounts from the same partner",
     "category": "Accounting & Finance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
