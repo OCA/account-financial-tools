ed891e6f634abfd27c1a0a620b140dca0ad7c53b
Andrea Stirpe <a.stirpe@onestein.nl>
2023-03-01 13:40:12 +0100

    [15.0][FIX] account_cost_center: enable default cost_center

--- account_cost_center/__manifest__.py
+++ account_cost_center/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://github.com/OCA/account-financial-tools",
     "category": "Accounting",
     "version": "15.0.1.0.0",
-    "depends": ["account"],
+    "depends": ["account", "base_view_inheritance_extension"],
     "data": [
         "security/ir.model.access.csv",
         "security/account_cost_center_security.xml",
