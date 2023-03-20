ae2e0bf538590f37a0dd45233ca7c7987a12a467
Ruchir Shukla <ruchir@bizzappdev.com>
2023-03-20 15:59:20 +0530

    [MIG][15.0]account_fiscal_month:Migration to 15.0

--- account_fiscal_month/__manifest__.py
+++ account_fiscal_month/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Fiscal Month",
     "summary": """Provide a fiscal month date range type""",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
