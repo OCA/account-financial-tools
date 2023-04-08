8b2c3ff1e9bc224b019e87d7be09388f66cf5553
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-09 01:03:08 +0200

    [IMP] account_subsequence_fiscal_year: black, isort, prettier

--- account_subsequence_fiscal_year/models/ir_sequence.py
+++ account_subsequence_fiscal_year/models/ir_sequence.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import datetime
+
 from dateutil.relativedelta import relativedelta
 
 from odoo import _, fields, models
@@ -15,9 +16,11 @@ class IrSequence(models.Model):
     def _create_date_range_seq(self, date):
         AccountFiscalYear = self.env["account.fiscal.year"]
         IrSequenceDateRange = self.env["ir.sequence.date_range"]
-        if self._context.get("account_sequence", False)\
-                and self.company_id\
-                and self.company_id.account_subsequence_method:
+        if (
+            self._context.get("account_sequence", False)
+            and self.company_id
+            and self.company_id.account_subsequence_method
+        ):
 
             method = self.company_id.account_subsequence_method
             object_date = fields.Date.from_string(date)
@@ -39,21 +42,27 @@ class IrSequence(models.Model):
                 fiscal_years = AccountFiscalYear.search(
                     [("date_to", ">=", object_date.strftime("%Y-%m-%d"))],
                     order="date_from desc",
-                    limit=1
+                    limit=1,
                 )
                 if not fiscal_years or fiscal_years[0].date_from > object_date:
-                    raise ValidationError(_(
-                        "You can not post an accounting entry for the"
-                        " date %s because there is no fiscal year defined at"
-                        " this date.") % (date))
+                    raise ValidationError(
+                        _(
+                            "You can not post an accounting entry for the"
+                            " date %s because there is no fiscal year defined at"
+                            " this date."
+                        )
+                        % (date)
+                    )
                 date_from = fiscal_years[0].date_from
                 date_to = fiscal_years[0].date_to
 
             # Create and return new sequence
-            return IrSequenceDateRange.sudo().create({
-                'sequence_id': self.id,
-                'date_from': date_from,
-                'date_to': date_to,
-            })
+            return IrSequenceDateRange.sudo().create(
+                {
+                    "sequence_id": self.id,
+                    "date_from": date_from,
+                    "date_to": date_to,
+                }
+            )
 
         return super()._create_date_range_seq(date)
