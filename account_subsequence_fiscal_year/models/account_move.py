1a8ed6fc9b767881254f577bedd26d81cfd8c510
Sylvain LE GAL <sylvain.legal@grap.coop>
2023-04-09 01:34:32 +0200

    [MI] account_subsequence_fiscal_year from 12.0 to 16.0

--- account_subsequence_fiscal_year/models/account_move.py
+++ account_subsequence_fiscal_year/models/account_move.py
@@ -1,14 +1,13 @@
 # Copyright (C) 2020 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from odoo import api, models
+from odoo import models
 
 
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    @api.multi
-    def post(self, invoice=False):
-        return super(AccountMove, self.with_context(account_sequence=True)).post(
-            invoice=invoice
+    def _post(self, soft=True):
+        return super(AccountMove, self.with_context(account_sequence=True))._post(
+            soft=soft
         )
