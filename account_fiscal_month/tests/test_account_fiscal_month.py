169f3e45eb77d67d786585a2df52e960d19a861d
BT-anieto <alberto.nieto@braintec.com>
2023-08-04 08:14:41 +0200

    [MIG] account_fiscal_month: Migration to 16.0
    [UPD] Update account_fiscal_month.pot
    
    [UPD] README.rst
    
    Update translation files
    
    Updated by "Update PO files to match POT (msgmerge)" hook in Weblate.
    
    Translation: account-financial-tools-16.0/account-financial-tools-16.0-account_fiscal_month
    Translate-URL: https://translation.odoo-community.org/projects/account-financial-tools-16-0/account-financial-tools-16-0-account_fiscal_month/
    
    Translated using Weblate (Spanish)
    
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: account-financial-tools-16.0/account-financial-tools-16.0-account_fiscal_month
    Translate-URL: https://translation.odoo-community.org/projects/account-financial-tools-16-0/account-financial-tools-16-0-account_fiscal_month/es/

--- account_fiscal_month/tests/test_account_fiscal_month.py
+++ account_fiscal_month/tests/test_account_fiscal_month.py
@@ -10,45 +10,46 @@ from odoo.tools import mute_logger
 
 
 class TestAccountFiscalMonth(TransactionCase):
-    def setUp(self):
-        super(TestAccountFiscalMonth, self).setUp()
-        self.DateRangeObj = self.env["date.range"]
-        self.DateRangeType = self.env["date.range.type"]
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.DateRangeObj = cls.env["date.range"]
+        cls.DateRangeType = cls.env["date.range.type"]
 
-        self.company = self.env.ref("base.main_company")
+        cls.company = cls.env.ref("base.main_company")
 
-        self.date_range_type = self.DateRangeType.create(
+        cls.date_range_type = cls.DateRangeType.create(
             {"name": "Other Type", "allow_overlap": False}
         )
-        self.date_range_type_month = self.env.ref(
+        cls.date_range_type_month = cls.env.ref(
             "account_fiscal_month.date_range_fiscal_month"
         )
 
-        self.date_range_1 = self.DateRangeObj.create(
+        cls.date_range_1 = cls.DateRangeObj.create(
             {
                 "name": "Other",
                 "date_start": "2017-01-01",
                 "date_end": "2017-01-31",
-                "type_id": self.date_range_type.id,
-                "company_id": self.company.id,
+                "type_id": cls.date_range_type.id,
+                "company_id": cls.company.id,
             }
         )
 
-        self.date_range_january_2017 = self.DateRangeObj.create(
+        cls.date_range_january_2017 = cls.DateRangeObj.create(
             {
                 "name": "January 2017",
                 "date_start": "2017-01-01",
                 "date_end": "2017-01-31",
-                "type_id": self.date_range_type_month.id,
-                "company_id": self.company.id,
+                "type_id": cls.date_range_type_month.id,
+                "company_id": cls.company.id,
             }
         )
-        self.date_range_january_no_comp_2017 = self.DateRangeObj.create(
+        cls.date_range_january_no_comp_2017 = cls.DateRangeObj.create(
             {
                 "name": "January 2017",
                 "date_start": "2017-01-01",
                 "date_end": "2017-01-31",
-                "type_id": self.date_range_type_month.id,
+                "type_id": cls.date_range_type_month.id,
                 "company_id": False,
             }
         )
