c1b3bf20e231e068b4f17d1e34fe6519c6336d3e
AlexPForgeFlow <alex.paris@forgeflow.com>
2023-09-01 11:20:52 +0200

    [MIG] account_move_line_landed_cost_info: Migration to 16.0

--- account_move_line_landed_cost_info/__manifest__.py
+++ account_move_line_landed_cost_info/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Move Line Landed Cost Info",
     "summary": "Introduces the landed cost adjustment lines to the journal items",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
     "category": "Generic",
