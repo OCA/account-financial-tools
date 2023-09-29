c03c6fe0da52073ed6f455fddc48a637f66c088e
Simone Rubino <simone.rubino@aion-tech.it>
2023-09-29 14:31:00 +0200

    [MIG] account_lock_to_date: Migration to 16.0

--- account_lock_to_date/__manifest__.py
+++ account_lock_to_date/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2019 ForgeFlow S.L.
+# Copyright 2023 Simone Rubino - Aion Tech
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Account Lock To Date",
     "summary": """
         Allows to set an account lock date in the future.""",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
