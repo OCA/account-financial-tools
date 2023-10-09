e4b09cb88ac851efb5f120caaa08901cd3aa8460
Andrea Gidalti <andreag@vauxoo.com>
2023-10-09 19:42:17 +0000

    [MIG] account_move_print: Migration to 17.0
    - Update version.
    - Change use of t-esc to t-out since latter was deprecated since v15.0.

--- account_move_print/__manifest__.py
+++ account_move_print/__manifest__.py
@@ -2,8 +2,8 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Account Move Print",
-    "version": "16.0.1.0.0",
-    "category": "Accounting",
+    "version": "17.0.1.0.0",
+    "category": "Accounting/Accounting",
     "license": "LGPL-3",
     "summary": "Adds the option to print Journal Entries",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
