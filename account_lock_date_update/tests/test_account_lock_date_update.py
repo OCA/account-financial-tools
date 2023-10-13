557ab8ce3e132e0796ffd44304548cea2e6709fb
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-13 12:30:27 +0200

    [IMP] account_lock_date_update: test performance improvement
    - Include context keys for avoiding mail operations overhead.

--- account_lock_date_update/tests/test_account_lock_date_update.py
+++ account_lock_date_update/tests/test_account_lock_date_update.py
@@ -10,6 +10,16 @@ class TestAccountLockDateUpdate(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                mail_create_nolog=True,
+                mail_create_nosubscribe=True,
+                mail_notrack=True,
+                no_reset_password=True,
+                tracking_disable=True,
+            )
+        )
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.company = cls.env.ref("base.main_company")
         cls.demo_user = cls.env.ref("base.user_demo")
