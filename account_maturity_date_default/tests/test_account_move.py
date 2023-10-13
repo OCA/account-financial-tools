85a0cccd40339b2d09edcf548f68d140cdada619
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-13 11:26:01 +0200

    [IMP] account_maturity_date_default: test performance improvement

--- account_maturity_date_default/tests/test_account_move.py
+++ account_maturity_date_default/tests/test_account_move.py
@@ -9,6 +9,16 @@ class TestAccountMove(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                mail_create_nolog=True,
+                mail_create_nosubscribe=True,
+                mail_notrack=True,
+                no_reset_password=True,
+                tracking_disable=True,
+            )
+        )
         cls.account_tax = cls.env["account.tax"].create(
             {"name": "0%", "amount_type": "fixed", "type_tax_use": "sale", "amount": 0}
         )
