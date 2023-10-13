7c5506146d0194899ab749d4d383a10a3e838073
Alessandro Uffreduzzi <alessandro.uffreduzzi@pytech.it>
2023-10-13 17:29:47 +0200

    [MIG] account_fiscal_position_allowed_journal: Migration to 14.0

--- account_fiscal_position_allowed_journal/tests/test_account_fiscal_position_allowed_journal.py
+++ account_fiscal_position_allowed_journal/tests/test_account_fiscal_position_allowed_journal.py
@@ -1,14 +1,19 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from odoo import fields
 from odoo.exceptions import UserError
-from odoo.tests.common import SavepointCase
+from odoo.tests import tagged
+from odoo.tests.common import Form
+
+from odoo.addons.account.tests.common import AccountTestInvoicingCommon
 
 
-class TestAccountFiscalPositionAllowedJournal(SavepointCase):
+@tagged("post_install", "-at_install")
+class TestAccountFiscalPositionAllowedJournal(AccountTestInvoicingCommon):
     @classmethod
     def setUpClass(cls):
-        super(TestAccountFiscalPositionAllowedJournal, cls).setUpClass()
+        super().setUpClass()
 
         # MODELS
         cls.account_model = cls.env["account.account"]
@@ -18,35 +23,42 @@ class TestAccountFiscalPositionAllowedJournal(SavepointCase):
         cls.partner_model = cls.env["res.partner"]
 
         # INSTANCES
-        cls.account_account_01 = cls.account_model.search(
-            [("code", "ilike", "4%")], limit=1
+        cls.account_account_01 = cls.env["account.account"].create(
+            {
+                "user_type_id": cls.env.ref("account.data_account_type_expenses").id,
+                "code": "EXPTEST",
+                "name": "Test expense account",
+            }
         )
+
         cls.fiscal_position_01 = cls.fiscal_position_model.create(
             {"name": "Fiscal position 01"}
         )
-        cls.journal_01 = cls.journal_model.search([("type", "=", "sale")], limit=1)
-        cls.journal_02 = cls.journal_01.copy()
-        cls.partner_01 = cls.partner_model.search([], limit=1)
-        cls.invoice_01 = cls.invoice_model.create(
+        cls.journal_01 = cls.journal_model.create(
             {
-                "type": "out_invoice",
-                "partner_id": cls.partner_01.id,
-                "journal_id": cls.journal_01.id,
-                "fiscal_position_id": cls.fiscal_position_01.id,
-                "invoice_line_ids": [
-                    (
-                        0,
-                        None,
-                        {
-                            "name": "Invoice line 01",
-                            "price_unit": 1,
-                            "quantity": 1,
-                            "account_id": cls.account_account_01.id,
-                        },
-                    )
-                ],
+                "name": "Test journal",
+                "code": "TEST",
+                "type": "sale",
             }
         )
+        cls.journal_02 = cls.journal_01.copy()
+        cls.partner_01 = cls.partner_model.search([], limit=1)
+
+        move_form = Form(
+            cls.env["account.move"].with_context(
+                default_move_type="out_invoice", check_move_validity=False
+            )
+        )
+        move_form.invoice_date = fields.Date.context_today(cls.env.user)
+        move_form.partner_id = cls.partner_01
+        move_form.journal_id = cls.journal_01
+        move_form.fiscal_position_id = cls.fiscal_position_01
+        with move_form.invoice_line_ids.new() as line_form:
+            line_form.name = "Invoice line 01"
+            line_form.product_id = cls.env.ref("product.product_product_4")
+            line_form.price_unit = 1
+            line_form.quantity = 1
+        cls.invoice_01 = move_form.save()
 
     def test_01(self):
         """
@@ -59,7 +71,7 @@ class TestAccountFiscalPositionAllowedJournal(SavepointCase):
             - The invoice is validated
         """
         self.assertEqual(self.invoice_01.state, "draft")
-        self.invoice_01.post()
+        self.invoice_01.action_post()
         self.assertEqual(self.invoice_01.state, "posted")
 
     def test_02(self):
@@ -76,7 +88,7 @@ class TestAccountFiscalPositionAllowedJournal(SavepointCase):
         self.fiscal_position_01.allowed_journal_ids = [(6, 0, self.journal_02.ids)]
         self.assertEqual(self.invoice_01.state, "draft")
         with self.assertRaises(UserError):
-            self.invoice_01.post()
+            self.invoice_01.action_post()
         self.assertEqual(self.invoice_01.state, "draft")
 
     def test_03(self):
@@ -92,7 +104,7 @@ class TestAccountFiscalPositionAllowedJournal(SavepointCase):
         """
         self.fiscal_position_01.allowed_journal_ids = [(6, 0, self.journal_01.ids)]
         self.assertEqual(self.invoice_01.state, "draft")
-        self.invoice_01.post()
+        self.invoice_01.action_post()
         self.assertEqual(self.invoice_01.state, "posted")
 
     def test_04(self):
