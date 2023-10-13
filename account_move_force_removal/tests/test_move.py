3921373e10b6893d338db0f53b1030996cf3774d
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-13 11:27:21 +0200

    [IMP] account_move_force_removal: test performance improvement

--- account_move_force_removal/tests/test_move.py
+++ account_move_force_removal/tests/test_move.py
@@ -11,6 +11,16 @@ class TestMove(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                mail_create_nolog=True,
+                mail_create_nosubscribe=True,
+                mail_notrack=True,
+                no_reset_password=True,
+                tracking_disable=True,
+            )
+        )
         cls.partner = cls.env["res.partner"].create(
             {"name": "Test customer", "customer_rank": 1}
         )
