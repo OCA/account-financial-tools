aa79b8001aceae35646daa6d18fc64fe823cd046
Marina Alapont <marina.alapont@forgeflow.com>
2023-10-24 16:35:25 +0200

    [ADD] account_account_tag_code

---
+++ account_account_tag_code/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Account Tag Code",
+    "summary": "Add a code field to the accounts tags",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "depends": ["account"],
+    "data": ["views/account_account_tag_views.xml"],
+}
