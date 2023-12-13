5137267cf06e613698f8b240a92922a5c0cbdc09
Saran440 <saranl@ecosoft.co.th>
2023-12-13 13:49:52 +0700

    [IMP] account_payment_netting: pre-commit stuff

--- account_payment_netting/__manifest__.py
+++ account_payment_netting/__manifest__.py
@@ -2,22 +2,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Account Payment Netting',
-    'version': '12.0.1.0.1',
-    'summary': 'Net Payment on AR/AP invoice from the same partner',
-    'category': 'Accounting & Finance',
-    'author': 'Ecosoft, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/account-financial-tools',
-    'depends': [
-        'account',
+    "name": "Account Payment Netting",
+    "version": "12.0.1.0.1",
+    "summary": "Net Payment on AR/AP invoice from the same partner",
+    "category": "Accounting & Finance",
+    "author": "Ecosoft, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'views/account_invoice_view.xml',
-        'views/account_payment_view.xml',
+    "data": [
+        "views/account_invoice_view.xml",
+        "views/account_payment_view.xml",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['kittiu'],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["kittiu"],
 }
