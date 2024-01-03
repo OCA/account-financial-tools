c06d3ba1727bee9d552fd69f5cb3264c66c45e12
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-01-03 20:11:40 -0300

    [ADD] account_asset_management_fleet: add new module

---
+++ account_asset_management_fleet/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Asset Management Fleet",
+    "summary": """
+        Account Asset Management Fleet""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "depends": [
+        "fleet",
+        "account_asset_management",
+    ],
+    "data": [
+        "views/account_asset.xml",
+        "views/fleet_vehicle.xml",
+    ],
+    "demo": [],
+}
---
+++ account_asset_management_fleet/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+Enhance the capabilities of the Account Asset Management system by allowing the association of assets with vehicles.
