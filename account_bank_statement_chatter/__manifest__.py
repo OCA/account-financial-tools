aff8acce2046d0365fb0c85985885c1963842b29
Sergio Bustamante <sergio.bustamante@factorlibre.com>
2024-04-22 12:07:42 +0200

    [IMP] account_bank_statement_chatter: pre-commit stuff

--- account_bank_statement_chatter/__manifest__.py
+++ account_bank_statement_chatter/__manifest__.py
@@ -2,20 +2,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Chatter on bank statements',
-    'version': '12.0.1.0.0',
-    'category': 'Invoices & Payments',
-    'license': 'AGPL-3',
-    'author': 'Trey, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/account-financial-tools',
-    'depends': [
-        'account',
+    "name": "Chatter on bank statements",
+    "version": "12.0.1.0.0",
+    "category": "Invoices & Payments",
+    "license": "AGPL-3",
+    "author": "Trey, " "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-financial-tools",
+    "depends": [
+        "account",
     ],
-    'data': [
-        'views/account_bank_statement_views.xml',
+    "data": [
+        "views/account_bank_statement_views.xml",
     ],
-    'installable': True,
-    'development_status': 'Beta',
-    'maintainers': ['cubells'],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["cubells"],
 }
