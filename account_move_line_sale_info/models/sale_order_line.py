10577668ab8057371c9921d8bd9288c0e852bbce
andrea <ast@360erp.com>
2024-08-30 15:56:41 +0200

    [MIG] account_move_line_sale_info: migration to V17

--- account_move_line_sale_info/models/sale_order_line.py
+++ account_move_line_sale_info/models/sale_order_line.py
@@ -1,23 +1,23 @@
 # Copyright 2020-23 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import api, models
 
 
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    def name_get(self):
-        result = []
-        orig_name = dict(super(SaleOrderLine, self).name_get())
-        for line in self:
-            name = orig_name[line.id]
-            if self.env.context.get("so_line_info", False):
+    @api.depends()
+    @api.depends_context("so_line_info")
+    def _compute_display_name(self):
+        res = super()._compute_display_name()
+        if self.env.context.get("so_line_info", False):
+            for line in self.sudo():
                 name = "[{}] {} - ({})".format(
                     line.order_id.name, line.product_id.name, line.order_id.state
                 )
-            result.append((line.id, name))
-        return result
+                line.display_name = name
+        return res
 
     def _prepare_invoice_line(self, **optional_values):
         res = super()._prepare_invoice_line(**optional_values)
