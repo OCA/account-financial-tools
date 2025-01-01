1ea7cbd6c133b976edf6bf603d7ee54e7094d739
Bhavesh Heliconia <oca@heliconia.in>
2025-01-01 12:05:47 +0530

    [MIG] account_account_tag_code: Migration to 18.0

--- account_account_tag_code/__manifest__.py
+++ account_account_tag_code/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Account Tag Code",
     "summary": "Add a code field to the accounts tags",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ForgeFlow,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
