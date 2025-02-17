29de67aaec0d23b5786357685249d1248c959b12
Miquel Raïch <miquel.raich@forgeflow.com>
2025-02-17 11:11:30 +0100

    [FIX] account_move_line_purchase_info: name_get is deprecated
    Use _compute_display_name instead.

--- account_move_line_purchase_info/models/purchase_order_line.py
+++ account_move_line_purchase_info/models/purchase_order_line.py
@@ -1,18 +1,18 @@
 # Copyright 2019-2020 ForgeFlow S.L.
 #   (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import api, models
 
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    def name_get(self):
-        result = []
-        orig_name = dict(super().name_get())
+    @api.depends("name", "order_id.name", "order_id.state")
+    @api.depends_context("po_line_info")
+    def _compute_display_name(self):
+        if not self.env.context.get("po_line_info", False):
+            return super()._compute_display_name()
         for line in self:
-            name = orig_name[line.id]
-            if self.env.context.get("po_line_info", False):
-                name = f"[{line.order_id.name}] {name} ({line.order_id.state})"
-            result.append((line.id, name))
-        return result
+            line.display_name = (
+                f"[{line.order_id.name}] {line.name} ({line.order_id.state})"
+            )
