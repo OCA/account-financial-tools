daf47a7a3cb578b7d2301f1be1298d8fe3c01dae
Sébastien BEAU <sebastien.beau@akretion.com>
2025-06-20 00:12:28 +0200

    account_asset_management: fix reversal compatibility
    Some OCA module like account_invoice_refund_line_selection
    expect to have the keywork "active_id"
    as odoo native pass it, we have to pass it here

--- account_asset_management/wizard/wiz_asset_move_reverse.py
+++ account_asset_management/wizard/wiz_asset_move_reverse.py
@@ -40,7 +40,11 @@ class WizAssetMoveReverse(models.TransientModel):
         move = self.line_id.move_id
         move_reversal = (
             self.env["account.move.reversal"]
-            .with_context(active_model="account.move", active_ids=move.ids)
+            .with_context(
+                active_model="account.move",
+                active_ids=move.ids,
+                active_id=move.id,
+            )
             .create(
                 {
                     "date": self.date_reversal,
