5d3a85c1bc01e845bb217992ca2a25c890f5beb9
Hughes Damry <hughes@damry.org>
2025-06-27 15:49:50 +0200

    [FIX] account_asset_management: asset_report
    Co-authored-by: Pedro M. Baeza <pedro.baeza@tecnativa.com>

--- account_asset_management/report/account_asset_report_xls.py
+++ account_asset_management/report/account_asset_report_xls.py
@@ -719,6 +719,6 @@ class AssetReportXlsx(models.AbstractModel):
                     reason = self.env._("Undetermined error")
                 row_pos += 1
                 err_msg = self.env._("Assets to be corrected") + ": "
-                err_msg += "{}".format(", ".join(error_dict[k].mapped("display_name")))
+                err_msg += ", ".join(error_dict[k].mapped("display_name"))
                 err_msg += " - " + self.env._("Reason") + ": " + reason
                 ws.write_string(row_pos, 0, err_msg, FORMATS["format_left_bold"])
