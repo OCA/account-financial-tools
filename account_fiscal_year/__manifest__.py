4fe78b097777e62c5eeffa1a3531e97b4721ffd0
michelerusti <michele.rusticucci@agilebg.com>
2026-01-07 17:02:01 +0100

    Version bump

--- account_fiscal_year/__manifest__.py
+++ account_fiscal_year/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Account Fiscal Year",
     "summary": "Create Account Fiscal Year",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.20260107",
     "development_status": "Production/Stable",
     "category": "Accounting",
     "website": "https://github.com/OCA/account-financial-tools",
