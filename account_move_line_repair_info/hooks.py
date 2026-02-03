3752edcbd06cc238dfab87d9bf037f99c5b569a2
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2026-02-03 11:55:06 +0100

    [MIG] account_move_line_repair_info: Migration to v18

--- account_move_line_repair_info/hooks.py
+++ account_move_line_repair_info/hooks.py
@@ -2,9 +2,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-def post_init_hook(cr, registry):
+def post_init_hook(env):
     # Material consumed on repair
-    cr.execute(
+    env.cr.execute(
         """
     update account_move_line as aml set repair_order_id = q.order_id
     from (
@@ -13,8 +13,7 @@ def post_init_hook(cr, registry):
         left join account_move as am on am.id = aml.move_id
         left join stock_valuation_layer svl on svl.account_move_id = am.id
         left join stock_move as sm on sm.id = svl.stock_move_id
-        left join repair_line as rl on rl.move_id = sm.id
-        left join repair_order as ro on rl.repair_id = ro.id
+        left join repair_order as ro on sm.repair_id = ro.id
         where ro.id is not null
     ) as q
     where q.id = aml.id
@@ -22,7 +21,7 @@ def post_init_hook(cr, registry):
     )
 
     # Product Repaired
-    cr.execute(
+    env.cr.execute(
         """
     update account_move_line as aml set repair_order_id = q.order_id
     from (
@@ -32,38 +31,6 @@ def post_init_hook(cr, registry):
         left join stock_valuation_layer svl on svl.account_move_id = am.id
         left join stock_move as sm on sm.id = svl.stock_move_id
         left join repair_order as ro on sm.id = ro.move_id
-        where ro.id is not null
-    ) as q
-    where q.id = aml.id
-    """
-    )
-
-    # Invoice Lines for Products Consumed
-    cr.execute(
-        """
-    update account_move_line as aml set repair_order_id = q.order_id
-    from (
-        select aml.id, ro.id as order_id
-        from account_move_line as aml
-        left join account_move as am on am.id = aml.move_id
-        left join repair_line as rl on aml.id = rl.invoice_line_id
-        left join repair_order as ro on rl.repair_id = ro.id
-        where ro.id is not null
-    ) as q
-    where q.id = aml.id
-    """
-    )
-
-    # Invoice Lines for Fees
-    cr.execute(
-        """
-    update account_move_line as aml set repair_order_id = q.order_id
-    from (
-        select aml.id, ro.id as order_id
-        from account_move_line as aml
-        left join account_move as am on am.id = aml.move_id
-        left join repair_fee as rf on aml.id = rf.invoice_line_id
-        left join repair_order as ro on rf.repair_id = ro.id
         where ro.id is not null
     ) as q
     where q.id = aml.id
