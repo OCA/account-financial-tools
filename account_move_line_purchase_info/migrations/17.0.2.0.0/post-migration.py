QuiJoQuim/account-financial-tools_update_cash_basis_assets

[16.0][FIX] account_move_line_purchase_info: do not consider stock entry lines on revaluations Up to now, stock entries were considered in the revaluation process as they were stored on field invoice_lines. To avoid that we introduced a dedicated field to manage stock entries and avoid taking them into account in the revaluation odoo core process.

account_move_line_purchase_info/migrations/16.0.2.0.0/post-migration.py
@@ -1,0 +1,34 @@
+import logging
+
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    purchase_orders = env["purchase.order"].search([("state", "=", "purchase")])
+    for order in purchase_orders:
+        try:
+            _logger.info(f"Processing Purchase Order: {order.name} (ID: {order.id})")
+            valued_lines = order.order_line.invoice_lines.filtered(
+                lambda l: l.product_id
+                and l.product_id.cost_method != "standard"
+                and (
+                    not l.company_id.tax_lock_date
+                    or l.date > l.company_id.tax_lock_date
+                )
+            )
+            svls, _amls = valued_lines._apply_price_difference()
+
+            if svls:
+                svls._validate_accounting_entries()
+
+            bills = order.invoice_ids.filtered(lambda bill: bill.state == "posted")
+            bills._stock_account_anglo_saxon_reconcile_valuation()
+
+        except Exception as e:
+            _logger.error(
+                f"Error processing Purchase Order {order.name} (ID: {order.id}): {str(e)}",
+                exc_info=True,
+            )

OCA/account-financial-tools

account_move_line_purchase_info/migrations/17.0.2.0.0/post-migration.py
@@ -1,0 +1,34 @@
+import logging
+
+from openupgradelib import openupgrade
+
+_logger = logging.getLogger(__name__)
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    purchase_orders = env["purchase.order"].search([("state", "=", "purchase")])
+    for order in purchase_orders:
+        try:
+            _logger.info(f"Processing Purchase Order: {order.name} (ID: {order.id})")
+            valued_lines = order.order_line.invoice_lines.filtered(
+                lambda line: line.product_id
+                and line.product_id.cost_method != "standard"
+                and (
+                    not line.company_id.tax_lock_date
+                    or line.date > line.company_id.tax_lock_date
+                )
+            )
+            svls, _amls = valued_lines._apply_price_difference()
+
+            if svls:
+                svls._validate_accounting_entries()
+
+            bills = order.invoice_ids.filtered(lambda bill: bill.state == "posted")
+            bills._stock_account_anglo_saxon_reconcile_valuation()
+
+        except Exception as e:
+            _logger.error(
+                f"Error processing Purchase Order {order.name} (ID: {order.id}): {str(e)}",
+                exc_info=True,
+            )