a5adfdc35ee8e1305e266dbdaca9c8d7b57f020c
Damien Crier <damien.crier@foodles.co>
2026-02-06 15:20:16 +0100

    fix wip

--- account_invoice_constraint_chronology_in_date_range/tests/test_account_invoice_constraint_chronology_with_date_range.py
+++ account_invoice_constraint_chronology_in_date_range/tests/test_account_invoice_constraint_chronology_with_date_range.py
@@ -26,7 +26,7 @@ class TestAccountInvoiceConstraintChronologyWithDateRange(common.TransactionCase
                 "code": "sale.sequence",
                 "padding": 4,
                 "use_date_range": True,
-                "prefix": "INV/%(range_year)s/%(range_month)s/",
+                "prefix": "INV/%(range_year)s/%(range_day)s/",
                 "number_increment": 1,
                 "date_range_ids": [
                     (
