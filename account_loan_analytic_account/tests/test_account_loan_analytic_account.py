5c52439fd810803c1c3f68c89811d604db5acf17
Pierre Verkest <pierre@verkest.fr>
2026-02-26 12:16:09 +0100

    [REF] account_loan_analytic_account: take care account_loan_refactor

--- account_loan_analytic_account/tests/test_account_loan_analytic_account.py
+++ account_loan_analytic_account/tests/test_account_loan_analytic_account.py
@@ -1,44 +1,13 @@
 # Copyright 2024 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields
-from odoo.tests.common import TransactionCase
+from odoo.addons.account_loan.tests.common import LoanCommon
 
 
-class TestAccountLoanAnalyticAccount(TransactionCase):
+class TestAccountLoanAnalyticAccount(LoanCommon):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.company = cls.env.ref("base.main_company")
-        cls.company_02 = cls.env["res.company"].create({"name": "Auxiliar company"})
-        cls.journal = cls.env["account.journal"].create(
-            {
-                "company_id": cls.company.id,
-                "type": "purchase",
-                "name": "Debts",
-                "code": "DBT",
-            }
-        )
-        cls.loan_account = cls.create_account(
-            "DEP",
-            "depreciation",
-            "liability_current",
-        )
-        cls.payable_account = cls.create_account("PAY", "payable", "liability_payable")
-        cls.asset_account = cls.create_account("ASSET", "asset", "liability_payable")
-        cls.interests_account = cls.create_account("FEE", "Fees", "expense")
-        cls.lt_loan_account = cls.create_account(
-            "LTD",
-            "Long term depreciation",
-            "liability_non_current",
-        )
-        cls.partner = cls.env["res.partner"].create({"name": "Bank"})
-        cls.product = cls.env["product.product"].create(
-            {"name": "Payment", "type": "service"}
-        )
-        cls.interests_product = cls.env["product.product"].create(
-            {"name": "Bank fee", "type": "service"}
-        )
         cls.env.user.group_ids += cls.env.ref("analytic.group_analytic_accounting")
 
         cls.default_plan = cls.env["account.analytic.plan"].create({"name": "Default"})
@@ -61,45 +30,20 @@ class TestAccountLoanAnalyticAccount(TransactionCase):
             }
         )
 
-        cls.loan = cls.create_loan("fixed-annuity", 500000, 1, 60)
-
-    @classmethod
-    def create_account(cls, code, name, account_type):
-        return cls.env["account.account"].create(
-            {
-                "company_ids": [fields.Command.set([cls.company.id])],
-                "name": name,
-                "code": code,
-                "account_type": account_type,
-                "reconcile": True,
-            }
+    def setUp(self):
+        super().setUp()
+        self.loan = self.create_loan(
+            "fixed-annuity", 500000, 1, 60, compute_lines=False
         )
-
-    @classmethod
-    def create_loan(cls, type_loan, amount, rate, periods):
-        loan = cls.env["account.loan"].create(
+        self.loan.write(
             {
-                "journal_id": cls.journal.id,
-                "rate_type": "napr",
-                "loan_type": type_loan,
-                "loan_amount": amount,
-                "payment_on_first_period": True,
-                "rate": rate,
-                "periods": periods,
-                "leased_asset_account_id": cls.asset_account.id,
-                "short_term_loan_account_id": cls.loan_account.id,
-                "interest_expenses_account_id": cls.interests_account.id,
-                "product_id": cls.product.id,
-                "interests_product_id": cls.interests_product.id,
-                "partner_id": cls.partner.id,
                 "analytic_distribution": {
-                    cls.analytic_account_a.id: 50,
-                    cls.analytic_account_b.id: 50,
-                },
+                    self.analytic_account_a.id: 50,
+                    self.analytic_account_b.id: 50,
+                }
             }
         )
-        loan.compute_lines()
-        return loan
+        self.loan.compute_lines()
 
     def test_analytic_account_propagates_to_moves(self):
         post = (
