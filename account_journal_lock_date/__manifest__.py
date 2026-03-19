46a5955e566db3d8d03fad8d95b103deea20ebcd
azzougouagh <azzougouagh.aly@gmail.com>
2026-03-19 17:14:07 -0400

    [IMP] account_journal_lock_date: pre-commit stuff

--- account_journal_lock_date/__manifest__.py
+++ account_journal_lock_date/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Journal Lock Date",
     "summary": "Lock each journal independently",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
