6b626c2333d7d6f86a52a2860bdb8e1210ba33bd
Odoo Wired <odoowired@erp-m.eu>
2026-03-31 10:06:01 +0200

    [IMP] account_usability: ensure parent child rel

--- account_usability/models/account_account.py
+++ account_usability/models/account_account.py
@@ -1,4 +1,7 @@
+from collections import defaultdict
+
 from odoo import api, fields, models
+from odoo.tools import SQL
 
 
 class Account(models.Model):
@@ -7,52 +10,97 @@ class Account(models.Model):
     group_id = fields.Many2one(search="_search_group_id")
 
     def _search_group_id(self, operator, value):
-        if operator not in ("in", "="):
-            raise NotImplementedError
+        if operator in ("=", "in"):
+            # Browse groups because value can be an odoo.tools.query.Query
+            groups = self.env["account.group"].browse(value)
+            mapping = self._get_group_to_accounts_mapping()
+            account_ids = []
+            for gid in groups.ids:
+                account_ids.extend(mapping.get(gid, []))
+            return [("id", "in", account_ids)]
+        if operator in ("!=", "not in"):
+            groups = self.env["account.group"].browse(value)
+            mapping = self._get_group_to_accounts_mapping()
+            account_ids = []
+            for gid in groups.ids:
+                account_ids.extend(mapping.get(gid, []))
+            return [("id", "not in", account_ids)]
+        raise NotImplementedError(
+            f"Search on group_id with operator '{operator}' is not supported."
+        )
 
-        # Browse groups because value can be an odoo.tools.query.Query
-        groups = self.env["account.group"].browse(value)
+    def _get_group_to_accounts_mapping(self):
+        """Build a mapping of group_id -> [account_ids].
 
-        if not groups:
-            return [("id", "=", 0)]
-
-        query = """
-            SELECT
-                a.id
-            FROM
-                account_account a
-            JOIN
-                account_group g
-                ON g.code_prefix_start <= LEFT(
-                    (a.code_store::json ->> %(company_id)s),
-                    char_length(g.code_prefix_start)
-                )
-                AND g.code_prefix_end >= LEFT(
-                    (a.code_store::json ->> %(company_id)s),
-                    char_length(g.code_prefix_end)
-                )
-                AND g.company_id = %(company_id)s
-            WHERE g.id IN %(group_ids)s
+        Each account is assigned to its most specific matching group
+        (longest code prefix). The mapping is cached on self.env for
+        the duration of the current request to avoid repeated queries
+        when multiple search calls are made (e.g. MIS report rendering).
         """
-        self.env.cr.execute(
-            query,
+        cache_attr = "_group_to_accounts_cache"
+        root_company_id = self.env.company.root_id.id
+        cached = getattr(self.env.cr, cache_attr, None)
+        if cached and cached.get("company_id") == root_company_id:
+            return cached["mapping"]
+        company_key = str(root_company_id)
+        results = self.env.execute_query(
+            SQL(
+                """
+                SELECT DISTINCT ON (aa.id)
+                       aa.id AS account_id,
+                       agroup.id AS group_id
+                FROM account_account aa
+                LEFT JOIN account_group agroup
+                    ON agroup.code_prefix_start <= LEFT(
+                        aa.code_store->>%(company_key)s,
+                        char_length(agroup.code_prefix_start)
+                    )
+                    AND agroup.code_prefix_end >= LEFT(
+                        aa.code_store->>%(company_key)s,
+                        char_length(agroup.code_prefix_end)
+                    )
+                    AND agroup.company_id = %(root_company_id)s
+                WHERE aa.code_store->>%(company_key)s IS NOT NULL
+                ORDER BY aa.id,
+                         char_length(agroup.code_prefix_start) DESC,
+                         agroup.id
+                """,
+                company_key=company_key,
+                root_company_id=root_company_id,
+            )
+        )
+        mapping = defaultdict(list)
+        for account_id, group_id in results:
+            if group_id:
+                mapping[group_id].append(account_id)
+        setattr(
+            self.env.cr,
+            cache_attr,
             {
-                "group_ids": tuple(groups.ids),
-                "company_id": str(self.env.company.root_id.id),
+                "company_id": root_company_id,
+                "mapping": mapping,
             },
         )
-        account_ids = [row[0] for row in self.env.cr.fetchall()]
-        return [("id", "in", account_ids)]
+        return mapping
+
+    @api.model
+    def _invalidate_group_to_accounts_cache(self):
+        cache_attr = "_group_to_accounts_cache"
+        if hasattr(self.env.cr, cache_attr):
+            delattr(self.env.cr, cache_attr)
 
     @api.model_create_multi
     def create(self, vals_list):
         res = super().create(vals_list)
-        res.mapped("group_id").invalidate_recordset()
+        self._invalidate_group_to_accounts_cache()
+        self.invalidate_model(["group_id"])
+        self.env["account.group"].invalidate_model(["account_ids"])
         return res
 
     def write(self, vals):
-        groups = self.mapped("group_id")
         res = super().write(vals)
         if "code" in vals:
-            (self.mapped("group_id") | groups).invalidate_recordset()
+            self._invalidate_group_to_accounts_cache()
+            self.invalidate_model(["group_id"])
+            self.env["account.group"].invalidate_model(["account_ids"])
         return res
--- account_usability/models/account_group.py
+++ account_usability/models/account_group.py
@@ -1,7 +1,7 @@
 # Copyright 2018 FOREST AND BIOMASS ROMANIA SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class AccountGroup(models.Model):
@@ -12,3 +12,20 @@ class AccountGroup(models.Model):
         inverse_name="group_id",
         string="Accounts",
     )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        res = super().create(vals_list)
+        self.env["account.account"]._invalidate_group_to_accounts_cache()
+        return res
+
+    def write(self, vals):
+        res = super().write(vals)
+        if "code_prefix_start" in vals or "code_prefix_end" in vals:
+            self.env["account.account"]._invalidate_group_to_accounts_cache()
+        return res
+
+    def unlink(self):
+        res = super().unlink()
+        self.env["account.account"]._invalidate_group_to_accounts_cache()
+        return res
