6fc491aadf9ae1df0d0377a4fac3934632bb4608
Bhavesh Heliconia <oca@heliconia.in>
2026-04-22 13:52:37 +0530

    [MIG] account_bank_statement_chatter: Migration to 18.0

--- account_bank_statement_chatter/__manifest__.py
+++ account_bank_statement_chatter/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Chatter on bank statements",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Invoices & Payments",
     "license": "AGPL-3",
     "author": "Trey, Odoo Community Association (OCA)",
