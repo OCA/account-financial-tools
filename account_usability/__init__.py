77a423891390e67cf17d6a50e2d298422a4f03bd
Michael Tietz <mtietz@mt-software.de>
2026-05-06 08:34:59 +0200

    [FIX] account_usability: Ensure translations getting used Add menu_finance as xmlid of this record to get it listed in i18n/account_usability.pot Ensure translation is getting loaded separately to not interfere with same translation terms of other addons like account

--- account_usability/__init__.py
+++ account_usability/__init__.py
@@ -1,1 +1,3 @@
+from .hooks import pre_init_hook, post_init_hook
 from . import models
+from . import wizards
--- account_usability/__manifest__.py
+++ account_usability/__manifest__.py
@@ -1,10 +1,11 @@
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# Copyright 2026 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account - Missing Menus & Saxon Accounting",
-    "version": "18.0.1.1.1",
+    "version": "18.0.1.1.2",
     "category": "Accounting",
     "license": "AGPL-3",
     "summary": "Adds missing menu entries for Account module and"
@@ -13,6 +14,7 @@
     "maintainers": ["legalsylvain"],
     "website": "https://github.com/OCA/account-financial-tools",
     "depends": ["account"],
+    "external_dependencies": {"python": ["openupgradelib"]},
     "data": [
         "data/ir_module_category.xml",
         "security/res_groups.xml",
@@ -25,4 +27,6 @@
     ],
     "demo": ["demo/res_groups.xml"],
     "installable": True,
+    "pre_init_hook": "pre_init_hook",
+    "post_init_hook": "post_init_hook",
 }
