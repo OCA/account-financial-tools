e8a340379551d144b7bd7bb899839bdcdd86edf3
Don Kendall <dkendall@ledoweb.com>
2026-05-19 09:50:05 -0400

    [IMP] account_asset_low_value: pre-commit auto fixes

--- account_asset_low_value/__manifest__.py
+++ account_asset_low_value/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Assets Management - Low Value Asset",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
