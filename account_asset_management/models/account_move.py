fb1900f33208f8205169cf7f419143130aa9c7fc
Florian da Costa <florian.dacosta@akretion.com>
2026-07-04 20:38:54 +0200

    FIX] account_asset_management: default account on invoice line     Only set the default account for supplier invoice

--- account_asset_management/models/account_move.py
+++ account_asset_management/models/account_move.py
@@ -197,7 +197,8 @@ class AccountMoveLine(models.Model):
     @api.onchange("asset_profile_id")
     def _onchange_asset_profile_id(self):
         if (
-            self.asset_profile_id.account_asset_id
+            self.move_id.move_type == "in_invoice"
+            and self.asset_profile_id.account_asset_id
             and self.asset_profile_id.account_asset_id != self.account_id
         ):
             self.account_id = self.asset_profile_id.account_asset_id
