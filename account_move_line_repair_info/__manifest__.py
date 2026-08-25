db8b4199b27485d443a73bf224a6371e2f6611b1
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2026-08-25 12:36:37 +0530

    [IMP] account_move_line_repair_info: pre-commit auto fixes

--- account_move_line_repair_info/__manifest__.py
+++ account_move_line_repair_info/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Account Move Line Repair Info",
     "summary": "Introduces the repair order to the journal items",
     "version": "18.0.1.0.0",
-    "author": "ForgeFlow S.L., " "Odoo Community Association (OCA)",
+    "author": "ForgeFlow S.L., Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/account-financial-tools",
     "category": "Generic",
     "depends": [
